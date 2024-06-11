5052e3402d1275d18ada1d095fabf64333ea7561
Radek Scheibinger <scheibinger@users.noreply.github.com>
2024-06-11 12:25:33 +0200

    Improve migrations script (#11)
    * Add more checks to prevent overriding existing .env files
    * More verbose output

--- scripts/migrations/migrate_to_monorepo.sh
+++ scripts/migrations/migrate_to_monorepo.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 CHAINLINK_CODE_DIR="${1}"
 
 help() {
   echo "missing argument: $0 <absolute path to CHAINLINK_CODE_DIR>"
-  echo "CHAINLINK_CODE_DIR is a directory where you checkout clone chainlink repositories locally"
+  echo "CHAINLINK_CODE_DIR is a directory where you clone/checkout chainlink repositories"
+  echo "example $0 ~/code"
 }
 
 if [ $# -ne 1 ]; then
@@ -17,21 +18,37 @@ fi
 pushd "$CHAINLINK_CODE_DIR"
 
 if [  ! -d "${CHAINLINK_CODE_DIR}/crib" ]; then
-  echo "crib repo is already cloned"
+  echo "cloning crib repo"
   git clone org-25111032@github.com:smartcontractkit/crib.git
 fi
 
-# Move .env files
-if [ -f "${CHAINLINK_CODE_DIR}/chainlink/crib/.env" ]; then
-  old_file="${CHAINLINK_CODE_DIR}/chainlink/crib/.env"
-  new_file="${CHAINLINK_CODE_DIR}/crib/core/.env"
-  echo -e "# The path to a directory with chainlink repositories\nCHAINLINK_CODE_DIR=\"${CHAINLINK_CODE_DIR}\"\n\n$(cat "$old_file")" > "$new_file"
-fi
+# Copy .env files
 
-if [ -f "${CHAINLINK_CODE_DIR}/ccip/crib/.env" ]; then
-  old_file="${CHAINLINK_CODE_DIR}/ccip/crib/.env"
-  new_file="${CHAINLINK_CODE_DIR}/crib/ccip/.env"
-  echo -e "# The path to a directory with chainlink repositories\nCHAINLINK_CODE_DIR=\"${CHAINLINK_CODE_DIR}\"\n\n$(cat "$old_file")" > "$new_file"
-fi
+update_dot_env() {
+  local old_file=$1
+  local new_file=$2
+
+  echo "Updating $old_file and copying to crib repo"
+  if [ -f "$new_file" ]; then
+    echo "$new_file already exists, skipping"
+  elif [ -f "$old_file" ]; then
+    echo "copying and updating ccip/crib/.env"
+    echo -e "# The path to a directory with chainlink repositories\nCHAINLINK_CODE_DIR=\"${CHAINLINK_CODE_DIR}\"\n\n$(cat "$old_file")" > "$new_file"
+  else
+    echo "couldn't find $old_file, skipping"
+  fi
+}
+
+# Copy env file for chainlink repo
+old_file="${CHAINLINK_CODE_DIR}/chainlink/crib/.env"
+new_file="${CHAINLINK_CODE_DIR}/crib/core/.env"
+
+update_dot_env "$old_file" "$new_file"
+
+# Copy .env for ccip
+old_file="${CHAINLINK_CODE_DIR}/ccip/crib/.env"
+new_file="${CHAINLINK_CODE_DIR}/crib/ccip/.env"
+
+update_dot_env "$old_file" "$new_file"
 
 popd
