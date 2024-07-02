03596718e051df0b77d00d6777bf678d88a4ce84
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-07-02 13:05:28 +0300

    fix comment

--- commit/plugin_e2e_test.go
+++ commit/plugin_e2e_test.go
@@ -324,7 +324,7 @@ func setupAllNodesReadAllChains(ctx context.Context, t *testing.T, lggr logger.L
 		n.ccipReader.On("NextSeqNum", ctx, []cciptypes.ChainSelector{chainA, chainB}).
 			Return([]cciptypes.SeqNum{10, 20}, nil)
 
-		// all nodes observe the same sequence numbers 10 for chainA and 20 for chainB
+		// transmission phase root staleness check passes
 		n.ccipReader.On("NextSeqNum", ctx, []cciptypes.ChainSelector{chainB}).
 			Return([]cciptypes.SeqNum{20}, nil)
 	}
