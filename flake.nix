6671983f5f925477026f4b288cd9a5633a9b973a
Njegos <njegos.railic@smartcontract.com>
2024-07-03 12:49:19 +0200

    Adding 1Pass integration

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ ]; };
+        pkgs = import nixpkgs { inherit system; overlays = [ ]; config.allowUnfree = true; };
       in
       rec {
         devShell = pkgs.callPackage ./shell.nix {
