a4b3893b8c736e165c0017c74aa11151e8499763
dimitris <dimitrios.kouveris@smartcontract.com>
2024-07-04 17:38:28 +0300

    Commit plugin dynamic reader sync (#18)
    * plugin syncs reader
    
    * code review comms

--- commit/plugin.go
+++ commit/plugin.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"sync"
 	"time"
 
 	mapset "github.com/deckarep/golang-set/v2"
@@ -36,7 +37,9 @@ type Plugin struct {
 	msgHasher         cciptypes.MessageHasher
 	lggr              logger.Logger
 
-	homeChain reader.HomeChain
+	homeChain        reader.HomeChain
+	bgSyncCancelFunc context.CancelFunc
+	bgSyncWG         *sync.WaitGroup
 }
 
 func NewPlugin(
@@ -51,7 +54,7 @@ func NewPlugin(
 	lggr logger.Logger,
 	homeChain reader.HomeChain,
 ) *Plugin {
-	return &Plugin{
+	p := &Plugin{
 		nodeID:            nodeID,
 		oracleIDToP2pID:   oracleIDToP2pID,
 		cfg:               cfg,
@@ -62,6 +65,21 @@ func NewPlugin(
 		lggr:              lggr,
 		homeChain:         homeChain,
 	}
+
+	bgSyncCtx, bgSyncCf := context.WithCancel(context.Background())
+	p.bgSyncCancelFunc = bgSyncCf
+	p.bgSyncWG = &sync.WaitGroup{}
+	p.bgSyncWG.Add(1)
+	backgroundReaderSync(
+		bgSyncCtx,
+		p.bgSyncWG,
+		lggr,
+		ccipReader,
+		syncTimeout(cfg.SyncTimeout),
+		time.NewTicker(syncFrequency(p.cfg.SyncFrequency)).C,
+	)
+
+	return p
 }
 
 // Query phase is not used.
@@ -345,6 +363,9 @@ func (p *Plugin) Close() error {
 	if err := p.ccipReader.Close(ctx); err != nil {
 		return fmt.Errorf("close ccip reader: %w", err)
 	}
+
+	p.bgSyncCancelFunc()
+	p.bgSyncWG.Wait()
 	return nil
 }
 
@@ -382,6 +403,20 @@ func (p *Plugin) supportsDestChain() (bool, error) {
 		return false, fmt.Errorf("get chain config: %w", err)
 	}
 	return destChainConfig.SupportedNodes.Contains(p.oracleIDToP2pID[p.nodeID]), nil
+}
+
+func syncFrequency(configuredValue time.Duration) time.Duration {
+	if configuredValue.Milliseconds() == 0 {
+		return 10 * time.Second
+	}
+	return configuredValue
+}
+
+func syncTimeout(configuredValue time.Duration) time.Duration {
+	if configuredValue.Milliseconds() == 0 {
+		return 3 * time.Second
+	}
+	return configuredValue
 }
 
 // Interface compatibility checks.
--- commit/plugin_functions_test.go
+++ commit/plugin_functions_test.go
@@ -2,21 +2,23 @@ package commit
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 	"slices"
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 
 	mapset "github.com/deckarep/golang-set/v2"
 	libocrtypes "github.com/smartcontractkit/libocr/ragep2p/types"
 
-	"github.com/smartcontractkit/chainlink-ccip/internal/libs/slicelib"
-	"github.com/smartcontractkit/chainlink-ccip/internal/mocks"
-
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+
+	"github.com/smartcontractkit/chainlink-ccip/internal/libs/slicelib"
+	"github.com/smartcontractkit/chainlink-ccip/internal/mocks"
 
 	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 
@@ -1179,6 +1181,50 @@ func Test_gasPricesConsensus(t *testing.T) {
 			assert.Equal(t, tc.expPrices, prices)
 		})
 	}
+}
+
+func Test_backgroundReaderSync(t *testing.T) {
+	ctx, cf := context.WithCancel(context.Background())
+	lggr := logger.Test(t)
+	reader := mocks.NewCCIPReader()
+	syncTimeout := 50 * time.Millisecond
+	ticker := make(chan time.Time)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	// start background syncing
+	backgroundReaderSync(ctx, wg, lggr, reader, syncTimeout, ticker)
+
+	// send a tick to trigger the first sync that errors
+	reader.On("Sync", mock.Anything).Return(false, fmt.Errorf("some err")).Once()
+	ticker <- time.Now()
+
+	// send a tick to trigger the second sync that succeeds without changes
+	reader.On("Sync", mock.Anything).Return(false, nil).Once()
+	ticker <- time.Now()
+
+	// make sync hang to see the context timeout
+	reader.On("Sync", mock.Anything).Run(func(args mock.Arguments) {
+		ctx := args.Get(0).(context.Context)
+		for { // simulate endless work until context times out
+			select {
+			case <-ctx.Done():
+				t.Log("context cancelled as expected")
+				return
+			default:
+				time.Sleep(time.Millisecond) // sleep to not block the CPU
+			}
+		}
+	}).Return(false, nil).Once()
+	ticker <- time.Now()
+
+	// send a tick to trigger the fourth sync that succeeds with changes
+	reader.On("Sync", mock.Anything).Return(true, nil).Once()
+	ticker <- time.Now()
+
+	cf()      // trigger bg sync to stop
+	wg.Wait() // wait for it to stop
+	reader.AssertExpectations(t)
 }
 
 func Test_validateMerkleRootsState(t *testing.T) {
