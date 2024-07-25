71b84c17c8f4b990745ef907a9c3c7bcc6289fd2
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-07-25 13:15:38 +0300

    fix comment

--- internal/libs/typconv/address.go
+++ internal/libs/typconv/address.go
@@ -4,7 +4,7 @@ import (
 	"encoding/hex"
 )
 
-// HexEncode converts a byte slice to an EVM string representation of an address
+// HexEncode converts a byte slice to a hex representation
 func HexEncode(addr []byte) string {
 	return "0x" + hex.EncodeToString(addr)
 }
