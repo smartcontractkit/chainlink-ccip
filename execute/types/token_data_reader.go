de4cd6eb4a7c1bfda7921021d77c2bd02c64a4fd
Will Winder <wwinder.unh@gmail.com>
2024-07-30 11:55:27 -0400

    Remove unused go:generate comments.

--- execute/types/token_data_reader.go
+++ execute/types/token_data_reader.go
@@ -8,8 +8,6 @@ import (
 
 // TokenDataReader is an interface for reading extra token data from an async process.
 // TODO: Build a token data reading process.
-//
-//go:generate mockery --name TokenDataReader --output ../../internal/mocks --case=underscore
 type TokenDataReader interface {
 	ReadTokenData(ctx context.Context, srcChain cciptypes.ChainSelector, num cciptypes.SeqNum) ([][]byte, error)
 }
