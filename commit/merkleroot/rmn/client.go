12b090da9b677213287550d0b175978704c4f6a9
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-09-05 16:59:36 +0300

    add comment to iface method

--- commit/merkleroot/rmn/client.go
+++ commit/merkleroot/rmn/client.go
@@ -11,6 +11,9 @@ var ErrTimeout = errors.New("signature computation timeout")
 // Client contains the methods required by the plugin to interact with the RMN nodes.
 type Client interface {
 	// ComputeReportSignatures computes and returns the signatures for the provided lane updates.
+	//
+	// This method abstracts away the RMN specific requests (ObservationRequest, ReportSignaturesRequest) and all the
+	// necessary steps to compute the signatures, like retrying and caching which are handled by the implementation.
 	ComputeReportSignatures(
 		ctx context.Context,
 		destChain DestChainInfo,
