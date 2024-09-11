753a2c5dda0fc104f16fbbe35d8706a6e8e3d53c
Will Winder <wwinder.unh@gmail.com>
2024-09-11 15:19:02 -0400

    Add fChain consensus...

--- shared/discovery/processor.go
+++ shared/discovery/processor.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
-	"github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
+	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 
 	"github.com/smartcontractkit/chainlink-ccip/pkg/consts"
 	"github.com/smartcontractkit/chainlink-ccip/pkg/reader"
@@ -17,16 +17,20 @@ import (
 var _ shared.PluginProcessor[Query, Observation, Outcome] = &ContractDiscoveryProcessor{}
 
 // Outcome isn't needed for this processor.
-type Outcome []byte
+type Outcome struct {
+	// TODO: some sort of request flag to avoid including this every time.
+	// Request bool
+}
 
 // Query isn't needed for this processor.
 type Query []byte
 
 // Observation of contract addresses.
 type Observation struct {
-	OnRamp map[ccipocr3.ChainSelector][]byte
+	FChain map[cciptypes.ChainSelector]int
+	OnRamp map[cciptypes.ChainSelector][]byte
 
-	// TODO: some sort of request flag.
+	// TODO: some sort of request flag to avoid including this every time.
 	// Request bool
 }
 
@@ -35,18 +39,19 @@ type ContractDiscoveryProcessor struct {
 	lggr      logger.Logger
 	reader    reader.CCIPReader
 	homechain reader.HomeChain
-	dest      ccipocr3.ChainSelector
+	dest      cciptypes.ChainSelector
+	bigF      int
 }
 
 // Query is not needed for this processor.
-func (cdp *ContractDiscoveryProcessor) Query(ctx context.Context, prevOutcome Outcome) (Query, error) {
+func (cdp *ContractDiscoveryProcessor) Query(_ context.Context, _ Outcome) (Query, error) {
 	return nil, nil
 }
 
 // Observation reads contract addresses from the destination chain.
 // In the future this should be extended to omit observations unless one of the nodes requests addresses.
 func (cdp *ContractDiscoveryProcessor) Observation(
-	ctx context.Context, prevOutcome Outcome, query Query,
+	ctx context.Context, _ Outcome, _ Query,
 ) (Observation, error) {
 	contracts, err := cdp.reader.DiscoverContracts(ctx, cdp.dest)
 	if err != nil {
@@ -57,36 +62,53 @@ func (cdp *ContractDiscoveryProcessor) Observation(
 		return Observation{}, fmt.Errorf("unable to discover contracts: %w", err)
 	}
 
+	fChain, err := cdp.homechain.GetFChain()
+	if err != nil {
+		return Observation{}, fmt.Errorf("unable to get fchain: %w", err)
+	}
+
 	return Observation{
+		FChain: fChain,
 		OnRamp: contracts[consts.ContractNameOnRamp],
 	}, nil
 }
 
 func (cdp *ContractDiscoveryProcessor) ValidateObservation(
-	prevOutcome Outcome, query Query, ao shared.AttributedObservation[Observation],
+	_ Outcome, _ Query, ao shared.AttributedObservation[Observation],
 ) error {
+	// TODO:
 	return nil
 }
 
 // Outcome
 func (cdp *ContractDiscoveryProcessor) Outcome(
-	prevOutcome Outcome, query Query, aos []shared.AttributedObservation[Observation],
+	_ Outcome, _ Query, aos []shared.AttributedObservation[Observation],
 ) (Outcome, error) {
 	// come to consensus on the onramp addresses and update the chainreader.
 
-	fChain, err := cdp.homechain.GetFChain()
-	if err != nil {
-		return Outcome{}, fmt.Errorf("unable to get fchain: %w", err)
+	// fChain consensus - uses the role DON F value because all nodes can observe the home chain.
+	fChainObs := make(map[cciptypes.ChainSelector][]int)
+	for _, ao := range aos {
+		for chainSel, f := range ao.Observation.FChain {
+			fChainObs[chainSel] = append(fChainObs[chainSel], f)
+		}
+	}
+	fMin := make(map[cciptypes.ChainSelector]int)
+	for chain := range fChainObs {
+		fMin[chain] = cdp.bigF
 	}
 
-	onrampAddrs := make(map[ccipocr3.ChainSelector][][]byte)
+	fChain := shared.GetConsensusMap(cdp.lggr, "fChain", fChainObs, fMin)
+
+	// onramp address consensus
+	onrampAddrs := make(map[cciptypes.ChainSelector][][]byte)
 	for _, ao := range aos {
 		for chain, addr := range ao.Observation.OnRamp {
 			onrampAddrs[chain] = append(onrampAddrs[chain], addr)
 		}
 	}
 
-	contracts := make(map[string]map[ccipocr3.ChainSelector][]byte)
+	contracts := make(map[string]map[cciptypes.ChainSelector][]byte)
 	contracts[consts.ContractNameOnRamp] = shared.GetConsensusMap(cdp.lggr, "onramp", onrampAddrs, fChain)
 	if err := cdp.reader.Sync(context.Background(), contracts); err != nil {
 		return Outcome{}, fmt.Errorf("unable to sync contracts: %w", err)
