9186497c13d5338dcb3741c1ae05def643a93dc7
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-09-16 16:09:50 +0200

    CCIP-2219 USDC Support - Stubbing interfaces and new flow  (#117)

---
+++ execute/exectypes/observation_test.go
@@ -0,0 +1,124 @@
+package exectypes
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_MessageTokensData(t *testing.T) {
+	tests := []struct {
+		name         string
+		msgTokenData MessageTokenData
+		ready        bool
+		error        error
+	}{
+		{
+			name:         "empty MessageTokenData is always ready - message doesnt carry tokens",
+			msgTokenData: MessageTokenData{},
+			ready:        true,
+			error:        nil,
+		},
+		{
+			name: "MessageTokenData is ready - all tokens are ready",
+			msgTokenData: MessageTokenData{
+				TokenData: []TokenData{
+					{
+						Ready: true,
+						Data:  []byte{123},
+					},
+					{
+						Ready: true,
+						Data:  []byte{234},
+					},
+				},
+			},
+			ready: true,
+			error: nil,
+		},
+		{
+			name: "MessageTokenData is not ready - one token is not ready",
+			msgTokenData: MessageTokenData{
+				TokenData: []TokenData{
+					{
+						Ready: true,
+						Data:  []byte{123},
+					},
+					{
+						Ready: false,
+						Data:  nil,
+						Error: fmt.Errorf("some error"),
+					},
+				},
+			},
+			ready: false,
+			error: fmt.Errorf("some error"),
+		},
+		{
+			name: "MessageTokenData is not ready - all tokens are not ready, first error is returned",
+			msgTokenData: MessageTokenData{
+				TokenData: []TokenData{
+					{
+						Ready: false,
+						Data:  nil,
+						Error: fmt.Errorf("error1"),
+					},
+					{
+						Ready: false,
+						Data:  nil,
+						Error: fmt.Errorf("error2"),
+					},
+				},
+			},
+			ready: false,
+			error: fmt.Errorf("error1"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.ready, tt.msgTokenData.IsReady())
+			assert.Equal(t, tt.error, tt.msgTokenData.Error())
+		})
+	}
+}
+
+func Test_MessageTokensData_ToByteSlice(t *testing.T) {
+	tests := []struct {
+		name         string
+		msgTokenData MessageTokenData
+		expected     [][]byte
+	}{
+		{
+			name:         "empty MessageTokenData is always ready - message doesnt carry tokens",
+			msgTokenData: MessageTokenData{},
+			expected:     [][]byte{},
+		},
+		{
+			name: "MessageTokenData is ready - all tokens are ready",
+			msgTokenData: MessageTokenData{
+				TokenData: []TokenData{
+					{
+						Ready: true,
+						Data:  []byte{123},
+					},
+					{
+						Ready: true,
+						Data:  []byte{234},
+					},
+				},
+			},
+			expected: [][]byte{
+				{123},
+				{234},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, tt.msgTokenData.ToByteSlice())
+		})
+	}
+}
--- pluginconfig/execute.go
+++ pluginconfig/execute.go
@@ -52,6 +52,9 @@ type ExecuteOffchainConfig struct {
 
 	// BatchingStrategyID is the strategy to use for batching messages.
 	BatchingStrategyID uint32 `json:"batchingStrategyID"`
+
+	// TokenDataObserver registers different strategies for processing token data.
+	TokenDataObserver []TokenDataObserverConfig `json:"tokenDataObservers"`
 }
 
 func (e ExecuteOffchainConfig) Validate() error {
@@ -78,6 +81,128 @@ func (e ExecuteOffchainConfig) Validate() error {
 		return errors.New("MessageVisibilityInterval not set")
 	}
 
+	set := make(map[string]struct{})
+	for _, ob := range e.TokenDataObserver {
+		if err := ob.Validate(); err != nil {
+			return err
+		}
+
+		key := ob.Type + ob.Version
+		if _, exists := set[key]; exists {
+			return errors.New("duplicate token data observer type and version")
+		}
+		set[key] = struct{}{}
+	}
+	return nil
+}
+
+// TokenDataObserverConfig is the base struct for token data observers. Every token data observer
+// has to define its type and version. The type and version is used to determine which observer's
+// implementation to use. Whenever you want to add a new observer type, you need to add a new struct and embed that
+// in the TokenDataObserverConfig similarly to how USDCCCTPObserverConfig is embedded in the TokenDataObserverConfig.
+// There are two additional checks for the TokenDataObserverConfig to enforce that it's semantically (Validate)
+// and syntactically correct (WellFormed).
+type TokenDataObserverConfig struct {
+	// Type is the type of the token data observer. You can think of different token data observers as different
+	// strategies for processing token data. For example, you can have a token data observer for USDC tokens using CCTP
+	// and different one for processing LINK token.
+	Type string `json:"type"`
+	// Version is the version of the TokenObserverConfig and the matching Observer implementation for that config.
+	// This is used to determine which version of the observer to use. Right now, we only have one version
+	// of the observer, but in the future, we might have multiple versions.
+	// This is a precautionary measure to ensure that we can upgrade the observer without breaking the existing ones.
+	// Example would be CCTPv1 using AttestationAPI and CCTPv2 using a different API or completely
+	// different strategy which requires different configuration and implementation during Observation phase.
+	// [
+	//  {
+	//    "type": "usdc-cctp",
+	//    "version": "1.0",
+	//    "attestationAPI": "http://circle.com/attestation",
+	//    "attestationAPITimeout": "1s",
+	//    "attestationAPIIntervalMilliseconds": "500ms"
+	//  },
+	//  {
+	//    "type": "usdc-cctp",
+	//    "version": "2.0",
+	//    "customCirlceAPI": "http://cirle.com/gohere",
+	//    "yetAnotherAPI": "http://cirle.com/anotherone",
+	//    "customCircleAPITimeout": "1s",
+	//    "yetAnotherAPITimeout": "500ms"
+	//  }
+	//]
+	// Having version in that JSON isn't expensive, but it could reduce the risk of breaking the observers in the future.
+	Version string `json:"version"`
+
+	*USDCCCTPObserverConfig
+}
+
+// WellFormed checks that the observer's config is syntactically correct - proper struct is initialized based on type
+func (t TokenDataObserverConfig) WellFormed() error {
+	switch t.Type {
+	case USDCCCTPHandlerType:
+		if t.USDCCCTPObserverConfig == nil {
+			return errors.New("USDCCCTPObserverConfig is empty")
+		}
+	default:
+		return errors.New("unknown token data observer type")
+	}
+	return nil
+}
+
+// Validate checks that the observer's config is semantically correct - fields are set correctly
+// depending on the config's type
+func (t TokenDataObserverConfig) Validate() error {
+	switch t.Type {
+	case USDCCCTPHandlerType:
+		if err := t.USDCCCTPObserverConfig.Validate(); err != nil {
+			return err
+		}
+	default:
+		return errors.New("unknown token data observer type " + t.Type)
+	}
+	return nil
+}
+
+const USDCCCTPHandlerType = "usdc-cctp"
+
+type USDCCCTPObserverConfig struct {
+	Tokens                 map[int]USDCCCTPTokenConfig `json:"tokens"`
+	AttestationAPI         string                      `json:"attestationAPI"`
+	AttestationAPITimeout  commonconfig.Duration       `json:"attestationAPITimeout"`
+	AttestationAPIInterval commonconfig.Duration       `json:"attestationAPIInterval"`
+}
+
+func (p USDCCCTPObserverConfig) Validate() error {
+	if p.AttestationAPI == "" {
+		return errors.New("AttestationAPI not set")
+	}
+	if len(p.Tokens) == 0 {
+		return errors.New("Tokens not set")
+	}
+	for _, token := range p.Tokens {
+		if err := token.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// nolint:lll // CCTP link
+type USDCCCTPTokenConfig struct {
+	// SourceTokenAddress is the address of the USDC ECR-20 token on the source chain
+	SourceTokenAddress string `json:"sourceTokenAddress"`
+	// SourceMessageTransmitterAddr is the address of the CCTP MessageTransmitter address on the source chain
+	// https://github.com/circlefin/evm-cctp-contracts/blob/adb2a382b09ea574f4d18d8af5b6706e8ed9b8f2/src/MessageTransmitter.sol
+	SourceMessageTransmitterAddr string `json:"sourceMessageTransmitterAddress"`
+}
+
+func (t USDCCCTPTokenConfig) Validate() error {
+	if t.SourceTokenAddress == "" {
+		return errors.New("SourceTokenAddress not set")
+	}
+	if t.SourceMessageTransmitterAddr == "" {
+		return errors.New("SourceMessageTransmitterAddress not set")
+	}
 	return nil
 }
 
@@ -92,5 +217,12 @@ func DecodeExecuteOffchainConfig(encodedExecuteOffchainConfig []byte) (ExecuteOf
 	if err := json.Unmarshal(encodedExecuteOffchainConfig, &e); err != nil {
 		return e, err
 	}
+
+	for _, ob := range e.TokenDataObserver {
+		if err := ob.WellFormed(); err != nil {
+			return e, err
+		}
+	}
+
 	return e, nil
 }
