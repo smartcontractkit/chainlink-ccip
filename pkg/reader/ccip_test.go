78864b3061ed7e19121228582f6ae2115463ec02
Will Winder <wwinder.unh@gmail.com>
2024-09-17 14:43:21 -0400

    Fix lint warnings.

--- pkg/reader/ccip_test.go
+++ pkg/reader/ccip_test.go
@@ -43,7 +43,13 @@ func TestCCIPChainReader_getSourceChainsConfig(t *testing.T) {
 		mock.Anything,
 		mock.Anything,
 		mock.Anything,
-	).Run(func(ctx context.Context, readIdentifier string, confidenceLevel primitives.ConfidenceLevel, params interface{}, returnVal interface{}) {
+	).Run(func(
+		ctx context.Context,
+		readIdentifier string,
+		confidenceLevel primitives.ConfidenceLevel,
+		params interface{},
+		returnVal interface{},
+	) {
 		sourceChain := params.(map[string]any)["sourceChainSelector"].(cciptypes.ChainSelector)
 		v := returnVal.(*sourceChainConfig)
 		v.OnRamp = []byte(fmt.Sprintf("onramp-%d", sourceChain))
@@ -74,117 +80,6 @@ func TestCCIPChainReader_getSourceChainsConfig(t *testing.T) {
 	assert.Equal(t, []byte("onramp-1"), cfgs[chainA].OnRamp)
 	assert.Equal(t, []byte("onramp-2"), cfgs[chainB].OnRamp)
 }
-
-/*
-
-func TestCCIPChainReader_getSourceChainsConfig(t *testing.T) {
-	sourceCRs := make(map[cciptypes.ChainSelector]*chainreadermocks.MockContractReader)
-	for _, chain := range []cciptypes.ChainSelector{chainA, chainB} {
-		sourceCRs[chain] = chainreadermocks.NewMockContractReader(t)
-	}
-
-	destCR := chainreadermocks.NewMockContractReader(t)
-
-	destCR.EXPECT().GetLatestValue(
-		mock.Anything,
-		mock.Anything,
-		mock.Anything,
-		mock.Anything,
-		mock.Anything,
-	).Run(func(ctx context.Context, readIdentifier string, confidenceLevel primitives.ConfidenceLevel, params interface{}, returnVal interface{}) {
-		sourceChain := params.(map[string]any)["sourceChainSelector"].(cciptypes.ChainSelector)
-		v := returnVal.(*sourceChainConfig)
-		v.OnRamp = []byte(fmt.Sprintf("onramp-%d", sourceChain))
-	})
-
-	sourceCRs[chainA].EXPECT().Bind(mock.Anything, mock.Anything).Return(nil)
-	sourceCRs[chainB].EXPECT().Bind(mock.Anything, mock.Anything).Return(nil)
-	destCR.EXPECT().Bind(mock.Anything, mock.Anything).Return(nil)
-
-	offrampAddress := []byte{0x3}
-	ccipReader := newCCIPChainReaderInternal(
-		logger.Test(t),
-		map[cciptypes.ChainSelector]types.ContractReader{
-			chainA: sourceCRs[chainA],
-			chainB: sourceCRs[chainB],
-			chainC: destCR,
-		}, nil, chainC, offrampAddress,
-	)
-
-	require.NoError(t, ccipReader.contractReaders[chainA].Bind(
-		context.Background(), []types.BoundContract{{Name: "OnRamp", Address: "0x1"}}))
-	require.NoError(t, ccipReader.contractReaders[chainB].Bind(
-		context.Background(), []types.BoundContract{{Name: "OnRamp", Address: "0x2"}}))
-	require.NoError(t, ccipReader.contractReaders[chainC].Bind(
-		context.Background(), []types.BoundContract{{Name: "OffRamp",
-			Address: typeconv.AddressBytesToString(offrampAddress, 111_111)}}))
-
-	ctx := context.Background()
-	cfgs, err := ccipReader.getSourceChainsConfig(ctx, []cciptypes.ChainSelector{chainA, chainB})
-	assert.NoError(t, err)
-	assert.Len(t, cfgs, 2)
-	assert.Equal(t, []byte("onramp-1"), cfgs[chainA].OnRamp)
-	assert.Equal(t, []byte("onramp-2"), cfgs[chainB].OnRamp)
-}
-
-func TestCCIPChainReader_GetContractAddress(t *testing.T) {
-	ecr := contractreader2.NewMockExtended(t)
-
-	ccipReader := ccipChainReader{
-		contractReaders: map[cciptypes.ChainSelector]contractreader.Extended{
-			chainA: ecr,
-		},
-	}
-
-	someAddr := "0x1234567890123456789012345678901234567890"
-	someAddrBytes, err := typeconv.AddressStringToBytes(someAddr, uint64(chainA))
-	require.NoError(t, err)
-
-	t.Run("happy path", func(t *testing.T) {
-		ecr.EXPECT().GetBindings(consts.ContractNameOnRamp).Return([]contractreader.ExtendedBoundContract{
-			{
-				BoundAt: time.Now().UTC(),
-				Binding: types.BoundContract{Address: someAddr, Name: consts.ContractNameOnRamp},
-			},
-		}).Once()
-		addr, err := ccipReader.GetContractAddress(consts.ContractNameOnRamp, chainA)
-		assert.NoError(t, err)
-		assert.Equal(t, someAddrBytes, addr)
-	})
-
-	t.Run("multiple bindings leads to error", func(t *testing.T) {
-		ecr.EXPECT().GetBindings(consts.ContractNameOnRamp).Return([]contractreader.ExtendedBoundContract{
-			{
-				BoundAt: time.Now().UTC(),
-				Binding: types.BoundContract{Address: someAddr, Name: consts.ContractNameOnRamp},
-			},
-			{
-				BoundAt: time.Now().UTC(),
-				Binding: types.BoundContract{Address: someAddr, Name: consts.ContractNameOnRamp},
-			},
-		}).Once()
-		_, err := ccipReader.GetContractAddress(consts.ContractNameOnRamp, chainA)
-		assert.Error(t, err)
-	})
-
-	t.Run("no binding leads to error", func(t *testing.T) {
-		ecr.EXPECT().GetBindings(consts.ContractNameOnRamp).Return([]contractreader.ExtendedBoundContract{}).Once()
-		_, err := ccipReader.GetContractAddress(consts.ContractNameOnRamp, chainA)
-		assert.Error(t, err)
-	})
-
-	t.Run("invalid address leads to error", func(t *testing.T) {
-		ecr.EXPECT().GetBindings(consts.ContractNameOnRamp).Return([]contractreader.ExtendedBoundContract{
-			{
-				BoundAt: time.Now().UTC(),
-				Binding: types.BoundContract{Address: "some wrong address fmt", Name: consts.ContractNameOnRamp},
-			},
-		}).Once()
-		_, err := ccipReader.GetContractAddress(consts.ContractNameOnRamp, chainA)
-		assert.Error(t, err)
-	})
-}
-*/
 
 func TestCCIPChainReader_GetContractAddress(t *testing.T) {
 	ecr := contractreader2.NewMockExtended(t)
