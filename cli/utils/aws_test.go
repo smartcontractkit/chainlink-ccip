b60aed8637ece1a5f07068489e30a28b3510623f
Rafael Felix Correa <rafael.felix@gmail.com>
2024-09-18 19:34:19 +0200

    CRIB-405: added logic to setup an AWS Profile in the CLI

---
+++ cli/utils/aws_test.go
@@ -0,0 +1,188 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/ini.v1"
+)
+
+// AwsProfile represent an AWS profile inside ~/.aws/config
+type AwsProfile struct {
+	name    string
+	entries map[string]string
+}
+
+// MockAwsConfigFile is a helper function that returns the path to a tempfile
+// containing the desired content and permissions
+func MockAwsConfigFile(content []byte, perm fs.FileMode) *os.File {
+	tempFile, err := os.CreateTemp("", "config")
+	if err != nil {
+		log.Fatalf("Failed to create temp file: %v", err)
+	}
+
+	if err := os.WriteFile(tempFile.Name(), content, perm); err != nil {
+		log.Fatal(err)
+	}
+
+	return tempFile
+}
+
+// LoadTestAwsConfig is a convenience wrapper for config.LoadDefaultConfig from AWS Go SDK
+func LoadTestAwsConfig(awsConfigFile string, profileName string) (aws.Config, error) {
+	os.Unsetenv("AWS_DEFAULT_REGION")
+	return config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithSharedConfigFiles([]string{awsConfigFile}),
+		config.WithSharedConfigProfile(profileName))
+}
+
+// InMemIniWithAwsProfiles takes a list of AwsProfile and returns an in-memory *ini.File instance
+func InMemIniWithAwsProfiles(awsProfiles []AwsProfile) (*ini.File, error) {
+	inMemIni := ini.Empty()
+
+	for _, awsProfile := range awsProfiles {
+		section, err := inMemIni.NewSection(fmt.Sprintf("profile %s", awsProfile.name))
+		if err != nil {
+			return inMemIni, err
+		}
+
+		for k, v := range awsProfile.entries {
+			section.NewKey(k, v)
+		}
+	}
+
+	return inMemIni, nil
+}
+
+// AssertEqualIniSection compares two ini.Section objects based on its
+// keys and values only, as direct equality doesn't work well when
+// working with temporary files
+func AssertEqualIniSections(t *testing.T, a *ini.Section, b *ini.Section) {
+
+	// strictly comparing ini.File, ini.Section or ini.Key instances
+	// fails because each of them carry a pointer to its respective parent
+	// which contains the dataSources attribute, which carries the path
+	// for the file, which is incomparable when dealing with temp files
+	// see: https://github.com/go-ini/ini/blob/b2f570e5b5b844226bbefe6fb521d891f529a951/file.go#L31
+	for _, k := range a.Keys() {
+		assert.Equal(t, k.Value(), b.Key(k.Name()).Value())
+	}
+	assert.Equal(t, len(a.Keys()), len(b.Keys()))
+}
+
+func TestSetupAwsProfileNonExisting(t *testing.T) {
+	mockedAwsConfig := MockAwsConfigFile([]byte(""), 0666)
+	defer os.Remove(mockedAwsConfig.Name())
+
+	require.NoError(t, SetupAwsProfile(mockedAwsConfig.Name(), "test-profile", "12345678909", "ap-southeast-1", "test-role-name", "https://sso.start.url"))
+
+	// inspect the new config with the AWS SDK
+	cfg, err := LoadTestAwsConfig(mockedAwsConfig.Name(), "test-profile")
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+	assert.Equal(t, "ap-southeast-1", cfg.Region)
+
+	// aws.Config doesn't directly expose sso_ prefixed profile parameters
+	// so our only choice is to inspect the file and assert the content is what we expect
+	want, err := InMemIniWithAwsProfiles([]AwsProfile{
+		{
+			name: "test-profile",
+			entries: map[string]string{
+				"region":         "ap-southeast-1",
+				"sso_start_url":  "https://sso.start.url",
+				"sso_region":     "ap-southeast-1",
+				"sso_account_id": "12345678909",
+				"sso_role_name":  "test-role-name",
+			},
+		},
+	})
+	require.NoError(t, err)
+	require.NotNil(t, want)
+
+	got, err := ini.Load(mockedAwsConfig.Name())
+	require.NoError(t, err)
+	require.NotNil(t, got)
+
+	wantSectionName := "profile test-profile"
+	AssertEqualIniSections(t, want.Section(wantSectionName), got.Section(wantSectionName))
+}
+
+func TestSetupAwsProfileExistsButDiverges(t *testing.T) {
+	mockedAwsConfig := MockAwsConfigFile([]byte(`[default]
+region = us-west-2
+output = text
+
+[profile some-other-profile]
+region = ap-southeast-1
+output = text
+
+[profile test-profile]
+region = us-east-1
+output = json
+sso_start_url = https://some.outdated.url
+sso_region = us-east-1
+sso_account_id = 123
+sso_role_name = outdated-role`), 0666)
+	defer os.Remove(mockedAwsConfig.Name())
+
+	require.NoError(t, SetupAwsProfile(mockedAwsConfig.Name(), "test-profile", "12345678909", "ap-southeast-1", "test-role-name", "https://sso.start.url"))
+
+	// inspect the new config with the AWS SDK
+	cfg, err := LoadTestAwsConfig(mockedAwsConfig.Name(), "test-profile")
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+	assert.Equal(t, "ap-southeast-1", cfg.Region)
+
+	// aws.Config doesn't directly expose sso_ prefixed profile parameters
+	// so our only choice is to inspect the file and assert the content is what we expect
+	wantAwsProfiles := []AwsProfile{
+		{
+			name: "some-other-profile",
+			entries: map[string]string{
+				"region": "ap-southeast-1",
+				"output": "text",
+			},
+		},
+		{
+			name: "test-profile",
+			entries: map[string]string{
+				"region":         "ap-southeast-1",
+				"sso_start_url":  "https://sso.start.url",
+				"sso_region":     "ap-southeast-1",
+				"sso_account_id": "12345678909",
+				"sso_role_name":  "test-role-name",
+			},
+		},
+	}
+
+	want, err := InMemIniWithAwsProfiles(wantAwsProfiles)
+	require.NoError(t, err)
+	require.NotNil(t, want)
+
+	got, err := ini.Load(mockedAwsConfig.Name())
+	require.NoError(t, err)
+	require.NotNil(t, got)
+
+	for _, wantAwsProfile := range wantAwsProfiles {
+		wantSectionName := fmt.Sprintf("profile %s", wantAwsProfile.name)
+		AssertEqualIniSections(t, want.Section(wantSectionName), got.Section(wantSectionName))
+	}
+
+	// test if the default section is also untouched
+	want = ini.Empty()
+	section, err := want.NewSection("default")
+	require.NoError(t, err)
+	section.NewKey("region", "us-west-2")
+	section.NewKey("output", "text")
+
+	AssertEqualIniSections(t, want.Section("default"), got.Section("default"))
+}
