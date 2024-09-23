53adf7d1c9e12123f280775912bbe08fe3aeb04e
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-09-23 17:41:16 +0200

    Fixes

--- internal/reader/contractreader/contractreader_facade.go
+++ internal/reader/contractreader/contractreader_facade.go
@@ -3,6 +3,7 @@ package contractreader
 import (
 	"context"
 
+	"github.com/smartcontractkit/chainlink-common/pkg/services"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query/primitives"
@@ -12,15 +13,11 @@ import (
 //
 //nolint:lll // don't read this interface.
 type ContractReaderFacade interface {
+	services.Service
 	GetLatestValue(ctx context.Context, readIdentifier string, confidenceLevel primitives.ConfidenceLevel, params, returnVal any) error
 	BatchGetLatestValues(ctx context.Context, request types.BatchGetLatestValuesRequest) (types.BatchGetLatestValuesResult, error)
 	Bind(ctx context.Context, bindings []types.BoundContract) error
 	Unbind(ctx context.Context, bindings []types.BoundContract) error
 	QueryKey(ctx context.Context, contract types.BoundContract, filter query.KeyFilter, limitAndSort query.LimitAndSort, sequenceDataType any) ([]types.Sequence, error)
 	//mustEmbedUnimplementedContractReaderServer()
-	Name() string
-	Start(context.Context) error
-	Close() error
-	Ready() error
-	HealthReport() map[string]error
 }
