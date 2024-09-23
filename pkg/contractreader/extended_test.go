93520ee801116a1043c9cd05e41e50a2e0ff5f84
Will Winder <wwinder.unh@gmail.com>
2024-09-23 06:21:26 -0400

    Add ErrTooManyBindings and ErrNoBindings. (#148)
    * Add ErrTooManyBindings and ErrNoBindings.
    
    * Add a test.
    
    ---------
    
    Co-authored-by: Makram <makramkd@users.noreply.github.com>

--- pkg/contractreader/extended_test.go
+++ pkg/contractreader/extended_test.go
@@ -3,6 +3,7 @@ package contractreader
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,4 +46,63 @@ func TestExtendedContractReader(t *testing.T) {
 	assert.Len(t, bindings, 2)
 	assert.Equal(t, "0x123", bindings[0].Binding.Address)
 	assert.Equal(t, "0x124", bindings[1].Binding.Address)
+}
+
+func TestGetOneBinding(t *testing.T) {
+	tests := []struct {
+		name          string
+		bindings      []types.BoundContract
+		expectedError error
+	}{
+		{
+			name:          "no bindings",
+			bindings:      []types.BoundContract{},
+			expectedError: ErrNoBindings,
+		},
+		{
+			name: "one binding",
+			bindings: []types.BoundContract{
+				{Name: "testContract", Address: "0x123"},
+			},
+			expectedError: nil,
+		},
+		{
+			name: "multiple bindings",
+			bindings: []types.BoundContract{
+				{Name: "testContract", Address: "0x123"},
+				{Name: "testContract", Address: "0x124"},
+			},
+			expectedError: ErrTooManyBindings,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			cr := chainreadermocks.NewMockContractReaderFacade(t)
+			contractName := "testContract"
+
+			var extendedBindings []ExtendedBoundContract
+			for _, binding := range tt.bindings {
+				extendedBindings = append(extendedBindings, ExtendedBoundContract{
+					Binding: binding,
+				})
+			}
+			extendedReader := &extendedContractReader{
+				ContractReaderFacade: cr,
+				contractBindingsByName: map[string][]ExtendedBoundContract{
+					contractName: extendedBindings,
+				},
+				mu: &sync.RWMutex{},
+			}
+
+			_, err := extendedReader.getOneBinding(contractName)
+			if tt.expectedError != nil {
+				assert.ErrorIs(t, err, tt.expectedError)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
