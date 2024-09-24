1c60b4dca29054d9f856e33c62f4bff698a2962f
Will Winder <wwinder.unh@gmail.com>
2024-09-24 12:11:12 -0400

    Remove temporary call to DiscoverContracts.

--- pkg/reader/ccip.go
+++ pkg/reader/ccip.go
@@ -504,15 +504,6 @@ func (r *ccipChainReader) DiscoverContracts(
 
 // Sync goes through the input contracts and binds them to the contract reader.
 func (r *ccipChainReader) Sync(ctx context.Context, contracts ContractAddresses) error {
-	if len(contracts) == 0 {
-		// TODO: stop calling DiscoverContracts here once the contracts are passed in via observations.
-		var err error
-		contracts, err = r.DiscoverContracts(ctx, r.destChain)
-		if err != nil {
-			return fmt.Errorf("sync: %w", err)
-		}
-	}
-
 	var errs []error
 	for contractName, chainSelToAddress := range contracts {
 		for chainSel, address := range chainSelToAddress {
