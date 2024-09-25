c112c234ff879a273e463d733d8a16bbaed60a3c
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-09-25 11:11:47 +0200

    Rate limiter tests from chainlink repo

--- execute/tokendata/usdc/http_test.go
+++ execute/tokendata/usdc/http_test.go
@@ -1,15 +1,19 @@
 package usdc
 
 import (
+	"context"
 	"encoding/hex"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -254,6 +258,112 @@ func Test_HTTPClient_CoolDownWithRetryHeader(t *testing.T) {
 	require.EqualError(t, err, ErrUnknownResponse.Error())
 
 	require.Equal(t, requestCount, 3)
+}
+
+func Test_HTTPClient_RateLimiting_Parallel(t *testing.T) {
+	testCases := []struct {
+		name         string
+		requests     uint64
+		rateConfig   time.Duration
+		testDuration time.Duration
+		timeout      time.Duration
+		err          string
+	}{
+		{
+			name:         "rate limited with default config",
+			requests:     5,
+			rateConfig:   100 * time.Millisecond,
+			testDuration: 4 * 100 * time.Millisecond,
+		},
+		{
+			name:         "rate limited with config",
+			requests:     10,
+			rateConfig:   50 * time.Millisecond,
+			testDuration: 9 * 50 * time.Millisecond,
+		},
+		{
+			name:         "timeout after first request",
+			requests:     5,
+			rateConfig:   100 * time.Millisecond,
+			testDuration: 1 * time.Millisecond,
+			timeout:      1 * time.Millisecond,
+			err:          "token data API is being rate limited",
+		},
+		{
+			name:         "timeout after second request",
+			requests:     5,
+			rateConfig:   100 * time.Millisecond,
+			testDuration: 100 * time.Millisecond,
+			timeout:      150 * time.Millisecond,
+			err:          "token data API is being rate limited",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_, err := w.Write(validAttestationResponse)
+				require.NoError(t, err)
+			}))
+			defer ts.Close()
+
+			attestationURI, err := url.ParseRequestURI(ts.URL)
+			require.NoError(t, err)
+
+			client := NewHTTPClient(attestationURI.String(), tc.rateConfig, longTimeout)
+
+			ctx := context.Background()
+			if tc.timeout > 0 {
+				var cf context.CancelFunc
+				ctx, cf = context.WithTimeout(ctx, tc.timeout)
+				defer cf()
+			}
+
+			trigger := make(chan struct{})
+			errorChan := make(chan error, tc.requests)
+			wg := sync.WaitGroup{}
+			for i := 0; i < int(tc.requests); i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+
+					<-trigger
+					_, _, err := client.Get(ctx, [32]byte{0xA})
+					if err != nil {
+						errorChan <- err
+					}
+				}()
+			}
+
+			// Start the test
+			start := time.Now()
+			close(trigger)
+
+			// Wait for requests to complete
+			wg.Wait()
+			finish := time.Now()
+			close(errorChan)
+
+			// Collect errors
+			errorFound := false
+			for err := range errorChan {
+				if tc.err != "" && strings.Contains(err.Error(), tc.err) {
+					errorFound = true
+				} else if err != nil && !strings.Contains(err.Error(), "token data API timed out") {
+					// Anything else is unexpected.
+					require.Fail(t, "unexpected error", err)
+				}
+			}
+
+			if tc.err != "" {
+				assert.True(t, errorFound)
+			}
+			assert.WithinDuration(t, start.Add(tc.testDuration), finish, 50*time.Millisecond)
+		})
+	}
 }
 
 func Test_httpResponse(t *testing.T) {
