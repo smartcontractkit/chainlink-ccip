8e091a3073f99bdbf61206e3e84a624b382235e5
asoliman <abdelrahman.soliman@smartcontract.com>
2024-09-27 18:40:47 +0400

    Remove validation logic for further debugging
    Signed-off-by: asoliman <abdelrahman.soliman@smartcontract.com>

--- commit/chainfee/validate_observation.go
+++ commit/chainfee/validate_observation.go
@@ -7,8 +7,6 @@ import (
 	"github.com/smartcontractkit/chainlink-ccip/internal/plugincommon"
 
 	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
-
-	"golang.org/x/exp/maps"
 )
 
 func (p *processor) ValidateObservation(
@@ -16,44 +14,44 @@ func (p *processor) ValidateObservation(
 	query Query,
 	ao plugincommon.AttributedObservation[Observation],
 ) error {
-	obs := ao.Observation
-
-	if err := validateFChain(obs.FChain); err != nil {
-		return fmt.Errorf("failed to validate FChain: %w", err)
-	}
-
-	observerSupportedChains, err := p.chainSupport.SupportedChains(ao.OracleID)
-	if err != nil {
-		return fmt.Errorf("failed to get supported chains: %w", err)
-	}
-
-	observedChains := append(maps.Keys(obs.FeeComponents), maps.Keys(obs.NativeTokenPrices)...)
-
-	for _, chain := range observedChains {
-		if !observerSupportedChains.Contains(chain) {
-			return fmt.Errorf("chain %d is not supported by observer", chain)
-		}
-	}
-
-	for _, feeComponent := range obs.FeeComponents {
-		err := validateBigInt(feeComponent.ExecutionFee, "execution fee")
-		if err != nil {
-			return err
-		}
-
-		err = validateBigInt(feeComponent.DataAvailabilityFee, "data availability fee")
-		if err != nil {
-			return err
-		}
-
-	}
-
-	for _, token := range obs.NativeTokenPrices {
-		err := validateBigInt(token.Int, "native token price")
-		if err != nil {
-			return err
-		}
-	}
+	//obs := ao.Observation
+	//
+	//if err := validateFChain(obs.FChain); err != nil {
+	//	return fmt.Errorf("failed to validate FChain: %w", err)
+	//}
+	//
+	//observerSupportedChains, err := p.chainSupport.SupportedChains(ao.OracleID)
+	//if err != nil {
+	//	return fmt.Errorf("failed to get supported chains: %w", err)
+	//}
+	//
+	//observedChains := append(maps.Keys(obs.FeeComponents), maps.Keys(obs.NativeTokenPrices)...)
+	//
+	//for _, chain := range observedChains {
+	//	if !observerSupportedChains.Contains(chain) {
+	//		return fmt.Errorf("chain %d is not supported by observer", chain)
+	//	}
+	//}
+	//
+	//for _, feeComponent := range obs.FeeComponents {
+	//	err := validateBigInt(feeComponent.ExecutionFee, "execution fee")
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	err = validateBigInt(feeComponent.DataAvailabilityFee, "data availability fee")
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//}
+	//
+	//for _, token := range obs.NativeTokenPrices {
+	//	err := validateBigInt(token.Int, "native token price")
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
 	return nil
 }
 
