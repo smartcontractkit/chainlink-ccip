2c4e00c89c1d4b11b7072e3e6ea955c11a309ac1
nogo <0xnogo@gmail.com>
2024-09-27 17:38:08 +0400

    more logs

--- commit/plugin.go
+++ commit/plugin.go
@@ -239,7 +239,7 @@ func (p *Plugin) ObserveFChain() map[cciptypes.ChainSelector]int {
 func (p *Plugin) Outcome(
 	outCtx ocr3types.OutcomeContext, q types.Query, aos []types.AttributedObservation,
 ) (ocr3types.Outcome, error) {
-	p.lggr.Debugw("Commit plugin performing outcome",
+	p.lggr.Infow("Commit plugin performing outcome",
 		"outctx", outCtx,
 		"query", q,
 		"attributedObservations", aos)
