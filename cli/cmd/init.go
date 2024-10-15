3b3c168d6af0ccd2f6b62aaaca76eba65c76bf46
Rafael Felix Correa <rafael.felix@gmail.com>
2024-10-15 14:29:39 +0200

    ecr.GetAuthorizationToken doesn't automatically return cross-account registries
    according to https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/ecr#GetAuthorizationTokenInput we shouldn't need to specify the registry URL in order to use the ecr authorization token to login to a registry. Unfortunately this explanation doesn't apply for cross-account registries, which is the case for our helm charts

--- cli/cmd/init.go
+++ cli/cmd/init.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net/url"
 	"os"
 	"path/filepath"
 	"slices"
@@ -201,6 +202,12 @@ var initCmd = &cobra.Command{
 			return
 		}
 
+		parsedHelmRegistryURI, err := url.Parse(viper.GetString("CHAINLINK_HELM_REGISTRY_URI"))
+		if err != nil {
+			logger.Error("failed to parse CHAINLINK_HELM_REGISTRY_URI", slog.Any("error", err))
+			os.Exit(1)
+		}
+
 		ecrClient := wrappers.NewECRClientWrapper(awsSdkConfig)
 		ecrAuthToken, err := utils.GetDecodedECRAuthorizationToken(ecrClient)
 		if err != nil {
@@ -228,11 +235,11 @@ var initCmd = &cobra.Command{
 					logger.Error("failed to initialize Helm Registry Client", slog.Any("error", err))
 					os.Exit(1)
 				}
-				if err := utils.HelmRegistryLogin(helmRegistryClient, authData.Username, authData.Password, authData.RegistryURL); err != nil {
+				if err := utils.HelmRegistryLogin(helmRegistryClient, authData.Username, authData.Password, parsedHelmRegistryURI.Host); err != nil {
 					logger.Error("failed to helm registry login", slog.Any("error", err))
 					os.Exit(1)
 				}
-				logger.Info("Helm registry login successful", "registry", authData.RegistryURL)
+				logger.Info("Helm registry login successful", "registry", parsedHelmRegistryURI.Host)
 			}
 		}
 
