48e21db03a5a67dcb055a70539bc7d642bd1743a
Rafael Felix Correa <rafael.felix@gmail.com>
2024-10-17 16:20:13 +0200

    CRIB-481: moving devspace deploy hooks to the CLI

--- cli/cmd/devspace.go
+++ cli/cmd/devspace.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/go-git/go-git/v5"
@@ -12,6 +13,10 @@ import (
 	"github.com/smartcontractkit/crib/cli/wrappers"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // devspaceCmd represents the devspace command
@@ -123,6 +128,142 @@ var refreshEcrCredentialsCmd = &cobra.Command{
 	},
 }
 
+// ensureNamespaceCmd represents the devspace ensure-namespace subcommand
+var ensureNamespaceCmd = &cobra.Command{
+	Use:   "ensure-namespace",
+	Short: "Ensure the k8s namespace exists and - when PROVIDER=aws - the kyverno-generated power user rolebinding is in place",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := utils.IsValidCribNamespace(viper.GetString("DEVSPACE_NAMESPACE"), viper.GetBool("CRIB_IGNORE_NAMESPACE_PREFIX")); err != nil {
+			logger.Error("invalid namespace for CRIB", slog.Any("error", err))
+			os.Exit(1)
+		}
+
+		kubeconfig, err := clientcmd.BuildConfigFromFlags("", viper.GetString("KUBECONFIG"))
+		if err != nil {
+			logger.Error("failed to initialize kubeconfig", slog.Any("error", err))
+			os.Exit(1)
+		}
+
+		kubeClientset, err := kubernetes.NewForConfig(kubeconfig)
+		if err != nil {
+			logger.Error("failed to initialize kube clientset", slog.Any("error", err))
+			os.Exit(1)
+		}
+
+		if err := utils.CheckK8sAccess(kubeClientset.CoreV1()); err != nil {
+			msg := "k8s access not working."
+			if !viper.GetBool("CRIB_CI_ENV") {
+				msg = fmt.Sprintf("%s Make sure you're connected to the VPN and try again.", msg)
+			}
+			logger.Error(
+				msg,
+				slog.String("kubeconfig", viper.GetString("KUBECONFIG")),
+				slog.String("kubecontext", viper.GetString("CRIB_EKS_ALIAS_NAME")),
+				slog.Any("error", err),
+			)
+			os.Exit(1)
+		}
+
+		logger.Info(
+			"k8s access working",
+			slog.String("kubeconfig", viper.GetString("KUBECONFIG")),
+			slog.String("kubecontext", viper.GetString("CRIB_EKS_ALIAS_NAME")),
+		)
+
+		created, err := utils.EnsureNamespaceExists(context.TODO(), kubeClientset.CoreV1().Namespaces(), viper.GetString("DEVSPACE_NAMESPACE"))
+		if err != nil {
+			logger.Error("failed to ensure namespace existence", slog.String("name", viper.GetString("DEVSPACE_NAMESPACE")), slog.Bool("already_exists", !created), slog.Any("error", err))
+			os.Exit(1)
+		}
+		logger.Debug("k8s namespace in place", slog.String("name", viper.GetString("DEVSPACE_NAMESPACE")), slog.Bool("already_exists", !created))
+
+		if viper.GetString("PROVIDER") == "aws" {
+			dynamicClient, err := dynamic.NewForConfig(kubeconfig)
+			if err != nil {
+				logger.Error("failed to initialize kube dynamic client", slog.Any("error", err))
+				os.Exit(1)
+			}
+
+			rolebindingClient := dynamicClient.Resource(schema.GroupVersionResource{
+				Group:    "rbac.authorization.k8s.io",
+				Version:  "v1",
+				Resource: "rolebindings",
+			}).Namespace(viper.GetString("DEVSPACE_NAMESPACE"))
+
+			// Use the WaitForResource function to check if a RoleBinding exists
+			roleBindingName := fmt.Sprintf("%s-crib-poweruser", viper.GetString("DEVSPACE_NAMESPACE"))
+			timeout := 20 * time.Second
+			if err := utils.WaitForResource(context.TODO(), rolebindingClient, roleBindingName, 2*time.Second, timeout); err != nil {
+				logger.Error("timed out waiting for role binding to be created", slog.String("role_binding_name", roleBindingName), slog.Duration("timeout", timeout), slog.String("namespace", viper.GetString("DEVSPACE_NAMESPACE")), slog.Any("error", err))
+				os.Exit(1)
+			}
+			logger.Info("role binding found", slog.String("role_binding_name", roleBindingName), slog.String("namespace", viper.GetString("DEVSPACE_NAMESPACE")))
+		}
+		logger.Info("k8s namespace ready", slog.String("name", viper.GetString("DEVSPACE_NAMESPACE")))
+	},
+}
+
+// checkEnvVarsCmd represents the devspace check-env-vars subcommand
+var checkEnvVarsCmd = &cobra.Command{
+	Use:       "check-env-vars [core|ccip]",
+	Short:     "Ensure all required environment variables are set for the given product (core|ccip). core is the default product",
+	Args:      cobra.OnlyValidArgs,
+	ValidArgs: []string{"", "core", "ccip"},
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if len(args) > 1 {
+			logger.Error("invalid number of arguments", slog.Int("expected", 1), slog.Int("received", len(args)))
+			os.Exit(1)
+		}
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		logger.Debug("Running with the following parameters", "config", viper.AllSettings())
+
+		product := "core"
+		if len(args) == 1 && args[0] != "" {
+			product = args[0]
+		}
+
+		logger.Info("checking required env vars", slog.String("product", product))
+		requiredEnvVars := []string{
+			"IS_CRIB",
+			"CHAINLINK_CODE_DIR",
+			"DEVSPACE_IMAGE",
+			"DEVSPACE_INGRESS_CIDRS",
+			"DEVSPACE_INGRESS_BASE_DOMAIN",
+			"DEVSPACE_INGRESS_CERT_ARN",
+			"DEVSPACE_K8S_POD_WAIT_TIMEOUT",
+		}
+
+		switch product {
+		case "core":
+			requiredEnvVars = append(requiredEnvVars, "CHAINLINK_CLUSTER_HELM_CHART_URI")
+		case "ccip":
+			requiredEnvVars = append(requiredEnvVars, "CHAINLINK_HELM_REGISTRY_URI")
+		}
+
+		if viper.GetString("DEVSPACE_PROFILE") == "keystone" {
+			requiredEnvVars = append(requiredEnvVars, "KEYSTONE_ETH_WS_URL", "KEYSTONE_ETH_HTTP_URL", "KEYSTONE_ACCOUNT_KEY")
+		}
+
+		missingEnvVars := []string{}
+		for _, name := range requiredEnvVars {
+			value := os.Getenv(name)
+			logger.Debug("reading env var", slog.String("name", name), slog.String("value", value))
+			if value == "" {
+				missingEnvVars = append(missingEnvVars, name)
+			}
+		}
+
+		missingEnvVarsCount := len(missingEnvVars)
+		if missingEnvVarsCount > 0 {
+			logger.Error("missing required environment variables, make sure they're all added ot the config file and try again (check '.env.example' for reference)", slog.Int("count", missingEnvVarsCount), slog.Any("env_vars", missingEnvVars), slog.String("config_file", viper.ConfigFileUsed()), slog.String("product", product))
+			os.Exit(1)
+		}
+
+		logger.Info("all required environment variables are set", slog.String("product", product), slog.String("config_file", viper.ConfigFileUsed()))
+	},
+}
+
 //nolint:gochecknoinits
 func init() {
 	rootCmd.AddCommand(devspaceCmd)
@@ -135,4 +276,7 @@ func init() {
 	refreshEcrCredentialsCmd.MarkFlagsOneRequired("docker", "helm")
 
 	_ = viper.BindPFlags(refreshEcrCredentialsCmd.Flags())
+
+	devspaceCmd.AddCommand(ensureNamespaceCmd)
+	devspaceCmd.AddCommand(checkEnvVarsCmd)
 }
