2cb58cb894280e5171142ce6af776fcbb2a7a7b8
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-10-17 11:52:49 +0300

    fix stream name

--- commit/merkleroot/rmn/peerclient.go
+++ commit/merkleroot/rmn/peerclient.go
@@ -7,6 +7,7 @@ import (
 	"crypto/sha256"
 	"encoding/binary"
 	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/smartcontractkit/libocr/commontypes"
@@ -93,6 +94,8 @@ func (r *peerClient) InitConnection(
 
 	h := sha256.Sum256(append(commitConfigDigest[:], rmnHomeConfigDigest[:]...))
 	r.genericEndpointConfigDigest = writePrefix(ocr2types.ConfigDigestPrefixCCIPMultiRoleRMNCombo, h)
+	r.lggr.Infow("Creating new peer group",
+		"genericEndpointConfigDigest", r.genericEndpointConfigDigest.String())
 
 	peerGroup, err := r.peerGroupFactory.NewPeerGroup(
 		[32]byte(r.genericEndpointConfigDigest),
@@ -149,7 +152,8 @@ func (r *peerClient) getOrCreateRageP2PStream(rmnNode rmntypes.HomeNodeInfo) (St
 	}
 
 	// todo: versioning for stream names e.g. for 'v1_7'
-	streamName := fmt.Sprintf("ccip-rmn/v1_6/%x", r.genericEndpointConfigDigest)
+	streamName := fmt.Sprintf("ccip-rmn/v1_6/%s",
+		strings.TrimPrefix(r.genericEndpointConfigDigest.String(), "0x"))
 	r.lggr.Infow("Creating new stream", "streamName", streamName)
 
 	var err error
