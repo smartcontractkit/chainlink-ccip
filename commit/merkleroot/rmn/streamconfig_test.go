6d86812d0db9ab3831a26aa7227c5504fce53b2e
dimitris <dimitrios.kouveris@smartcontract.com>
2024-10-31 11:56:46 +0200

    Adjust stream config based on response types sizes (#283)
    * adjust stream config
    * dynamic round interval and more tests

--- commit/merkleroot/rmn/streamconfig_test.go
+++ commit/merkleroot/rmn/streamconfig_test.go
@@ -2,47 +2,80 @@ package rmn
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 )
 
+//nolint:dupl
 func Test_newStreamConfig(t *testing.T) {
+	const kB = 1024
 	lggr := logger.Test(t)
-
 	streamName := "myCoolStream"
 
-	cfg := newStreamConfig(lggr, streamName)
+	t.Run("one second rounds", func(t *testing.T) {
+		cfg := newStreamConfig(lggr, streamName, time.Second)
+		assert.Equal(t, streamName, cfg.StreamName)
+		assert.Equal(t, 1, cfg.OutgoingBufferSize)
+		assert.Equal(t, 1, cfg.IncomingBufferSize)
+		// message length
+		assert.Greater(t, cfg.MaxMessageLength, 52*kB)
+		assert.Less(t, cfg.MaxMessageLength, 53*kB)
+		// message rate
+		assert.Greater(t, cfg.MessagesLimit.Rate, 3.5)
+		assert.Less(t, cfg.MessagesLimit.Rate, 3.6)
+		// message capacity
+		assert.Equal(t, 15, int(cfg.MessagesLimit.Capacity))
+		// bytes rate
+		assert.Greater(t, cfg.BytesLimit.Rate, float64(63*kB))
+		assert.Less(t, cfg.BytesLimit.Rate, float64(64*kB))
+		// bytes capacity
+		assert.Greater(t, int(cfg.BytesLimit.Capacity), 263*kB)
+		assert.Less(t, int(cfg.BytesLimit.Capacity), 265*kB)
+	})
 
-	assert.Equal(t, streamName, cfg.StreamName)
+	t.Run("500ms rounds", func(t *testing.T) {
+		cfg := newStreamConfig(lggr, streamName, 500*time.Millisecond)
+		assert.Equal(t, streamName, cfg.StreamName)
+		assert.Equal(t, 1, cfg.OutgoingBufferSize)
+		assert.Equal(t, 1, cfg.IncomingBufferSize)
+		// message length
+		assert.Greater(t, cfg.MaxMessageLength, 52*kB)
+		assert.Less(t, cfg.MaxMessageLength, 53*kB)
+		// message rate
+		assert.Greater(t, cfg.MessagesLimit.Rate, 7.1)
+		assert.Less(t, cfg.MessagesLimit.Rate, 7.2)
+		// message capacity
+		assert.Equal(t, 30, int(cfg.MessagesLimit.Capacity))
+		// bytes rate
+		assert.Greater(t, cfg.BytesLimit.Rate, float64(126*kB))
+		assert.Less(t, cfg.BytesLimit.Rate, float64(128*kB))
+		// bytes capacity
+		assert.Greater(t, int(cfg.BytesLimit.Capacity), 263*kB)
+		assert.Less(t, int(cfg.BytesLimit.Capacity), 265*kB)
+	})
 
-	assert.Equal(t, 1, cfg.OutgoingBufferSize)
+	t.Run("4s rounds", func(t *testing.T) {
+		cfg := newStreamConfig(lggr, streamName, 4*time.Second)
+		assert.Equal(t, streamName, cfg.StreamName)
+		assert.Equal(t, 1, cfg.OutgoingBufferSize)
+		assert.Equal(t, 1, cfg.IncomingBufferSize)
+		// message length
+		assert.Greater(t, cfg.MaxMessageLength, 52*kB)
+		assert.Less(t, cfg.MaxMessageLength, 53*kB)
+		// message rate
+		assert.Greater(t, cfg.MessagesLimit.Rate, 0.8)
+		assert.Less(t, cfg.MessagesLimit.Rate, 0.9)
+		// message capacity
+		assert.Equal(t, 3, int(cfg.MessagesLimit.Capacity))
+		// bytes rate
+		assert.Greater(t, cfg.BytesLimit.Rate, float64(15*kB))
+		assert.Less(t, cfg.BytesLimit.Rate, float64(17*kB))
+		// bytes capacity
+		assert.Greater(t, int(cfg.BytesLimit.Capacity), 263*kB)
+		assert.Less(t, int(cfg.BytesLimit.Capacity), 265*kB)
+	})
 
-	assert.Equal(t, 1, cfg.IncomingBufferSize)
-
-	// message length
-	assert.Greater(t, cfg.MaxMessageLength, 25*megaByte)
-	assert.Less(t, cfg.MaxMessageLength, 27*megaByte)
-
-	// message rate
-	assert.Greater(t, cfg.MessagesLimit.Rate, 3.4)
-	assert.Less(t, cfg.MessagesLimit.Rate, 3.8)
-
-	// message capacity
-	assert.Equal(t, 9, int(cfg.MessagesLimit.Capacity))
-
-	// bytes rate
-	assert.Greater(t, cfg.BytesLimit.Rate, float64(30*megaByte))
-	assert.Less(t, cfg.BytesLimit.Rate, float64(33*megaByte))
-
-	// bytes capacity
-	assert.Greater(t, int(cfg.BytesLimit.Capacity), 77*megaByte)
-	assert.Less(t, int(cfg.BytesLimit.Capacity), 80*megaByte)
 }
-
-const (
-	byt      = 1
-	kiloByte = 1024 * byt
-	megaByte = 1024 * kiloByte
-)
