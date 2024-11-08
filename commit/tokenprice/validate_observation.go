5bcc6b8ab6c95ca6ca2b5b3874a35e4a4dd5f7df
0xAustinWang <austiwang+eth@gmail.com>
2024-11-08 16:24:57 +0900

    add fchain validation for fee token observation

---
+++ commit/tokenprice/validate_observation.go
@@ -0,0 +1,51 @@
+package tokenprice
+
+import (
+	"fmt"
+	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/smartcontractkit/chainlink-ccip/internal/plugincommon"
+	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
+)
+
+func (p *processor) ValidateObservation(
+	prevOutcome Outcome,
+	query Query,
+	ao plugincommon.AttributedObservation[Observation],
+) error {
+	obs := ao.Observation
+
+	if err := validateFChain(obs.FChain); err != nil {
+		return fmt.Errorf("failed to validate FChain: %w", err)
+	}
+
+	if err := validateObservedTokenPrices(obs.FeedTokenPrices); err != nil {
+		return fmt.Errorf("failed to validate observed token prices: %w", err)
+	}
+
+	return nil
+}
+
+func validateFChain(fChain map[cciptypes.ChainSelector]int) error {
+	for chainSelector, f := range fChain {
+		if f < 0 {
+			return fmt.Errorf("fChain for chain %d is not positive: %d", chainSelector, f)
+		}
+	}
+
+	return nil
+}
+
+func validateObservedTokenPrices(tokenPrices []cciptypes.TokenPrice) error {
+	tokensWithPrice := mapset.NewSet[cciptypes.UnknownEncodedAddress]()
+	for _, t := range tokenPrices {
+		if tokensWithPrice.Contains(t.TokenID) {
+			return fmt.Errorf("duplicate token price for token: %s", t.TokenID)
+		}
+		tokensWithPrice.Add(t.TokenID)
+
+		if t.Price.IsEmpty() {
+			return fmt.Errorf("token price of token %v must not be empty", t.TokenID)
+		}
+	}
+	return nil
+}
--- commit/tokenprice/validate_observation_test.go
+++ commit/tokenprice/validate_observation_test.go
@@ -1,10 +1,9 @@
 package tokenprice
 
 import (
+	"github.com/stretchr/testify/assert"
 	"math/big"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 )
@@ -62,5 +61,42 @@ func Test_validateObservedTokenPrices(t *testing.T) {
 			assert.NoError(t, err)
 		})
 
+	}
+}
+
+func Test_validateFChain(t *testing.T) {
+	testCases := []struct {
+		name   string
+		fChain map[cciptypes.ChainSelector]int
+		expErr bool
+	}{
+		{
+			name: "FChain contains negative values",
+			fChain: map[cciptypes.ChainSelector]int{
+				1: 11,
+				2: -4,
+			},
+			expErr: true,
+		},
+		{
+			name: "FChain valid",
+			fChain: map[cciptypes.ChainSelector]int{
+				12: 6,
+				7:  9,
+			},
+			expErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := validateFChain(tc.fChain)
+
+			if tc.expErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+		})
 	}
 }
