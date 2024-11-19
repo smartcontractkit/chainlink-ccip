d012636823e5fedc424db833c87e2ce83742d101
0xAustinWang <austiwang+eth@gmail.com>
2024-11-19 18:30:38 +0900

    test different changes

--- commit/report.go
+++ commit/report.go
@@ -57,6 +57,8 @@ func (p *Plugin) Reports(
 		rep     cciptypes.CommitPluginReport
 		repInfo ReportInfo
 	)
+
+	// MerkleRoots and RMNSignatures will be empty arrays if there is nothing to report
 	rep = cciptypes.CommitPluginReport{
 		MerkleRoots: outcome.MerkleRootOutcome.RootsToReport,
 		PriceUpdates: cciptypes.PriceUpdates{
@@ -64,6 +66,11 @@ func (p *Plugin) Reports(
 			GasPriceUpdates:   outcome.ChainFeeOutcome.GasPrices,
 		},
 		RMNSignatures: outcome.MerkleRootOutcome.RMNReportSignatures,
+	}
+
+	if outcome.MerkleRootOutcome.OutcomeType == merkleroot.ReportEmpty {
+		rep.MerkleRoots = []cciptypes.MerkleRootChain{}
+		rep.RMNSignatures = []cciptypes.RMNECDSASignature{}
 	}
 
 	if outcome.MerkleRootOutcome.OutcomeType == merkleroot.ReportGenerated {
