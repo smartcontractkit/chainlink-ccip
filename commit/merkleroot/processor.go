1ed1594ef23faf242ccdfe6d0b590bdf6b256a2e
dimitris <dimitrios.kouveris@smartcontract.com>
2024-12-10 10:51:16 +0200

    Commit Plugin - Merkle root processor cleanup (#349)
    ## **Refactor Merkle Root Processor: Code Cleanup and Enhancements**
    
    ### Description
    This PR focuses on refactoring the Merkle Root Processor to improve code quality, maintainability, and clarity. Key changes include:
    
    - **Documentation Improvements:** Added or updated inline comments to enhance code readability and clarity.
    - **Code Style Consistency:** Standardized code style across the processor, including naming conventions and formatting.
    - **Receiver Name Uniformity:** Resolved inconsistencies in receiver names for better readability.
    - **Function Renaming:** Renamed specific functions for improved descriptive clarity.
    - **Log Message Cleanup:** Refined log messages for better debugging and operational clarity.
    - **Encapsulation Improvements:** Unexported methods and fields that are not intended for external use.
    - **Constructor Implementation:** Introduced constructors for struct initialization to prevent the creation of structs with incomplete or invalid fields.
    
    These changes aim to reduce technical debt, align the implementation with best practices, and enhance future maintainability of the processor.

--- commit/merkleroot/processor.go
+++ commit/merkleroot/processor.go
@@ -52,27 +52,26 @@ func NewProcessor(
 	rmnCrypto cciptypes.RMNCrypto,
 	rmnHomeReader readerpkg.RMNHome,
 ) *Processor {
-	observer := observerImpl{
-		lggr,
-		homeChain,
-		oracleID,
-		chainSupport,
-		ccipReader,
-		msgHasher,
-	}
 	return &Processor{
 		oracleID:        oracleID,
 		oracleIDToP2pID: oracleIDToP2pID,
 		offchainCfg:     offchainCfg,
 		destChain:       destChain,
 		lggr:            lggr,
-		observer:        observer,
-		ccipReader:      ccipReader,
-		reportingCfg:    reportingCfg,
-		chainSupport:    chainSupport,
-		rmnController:   rmnController,
-		rmnCrypto:       rmnCrypto,
-		rmnHomeReader:   rmnHomeReader,
+		observer: newObserverImpl(
+			lggr,
+			homeChain,
+			oracleID,
+			chainSupport,
+			ccipReader,
+			msgHasher,
+		),
+		ccipReader:    ccipReader,
+		reportingCfg:  reportingCfg,
+		chainSupport:  chainSupport,
+		rmnController: rmnController,
+		rmnCrypto:     rmnCrypto,
+		rmnHomeReader: rmnHomeReader,
 	}
 }
 
