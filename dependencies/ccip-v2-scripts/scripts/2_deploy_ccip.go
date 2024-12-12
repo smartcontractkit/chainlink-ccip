100296ebdbd05797728c7f4f28aa28a1f6aff805
Radek Scheibinger <scheibinger@users.noreply.github.com>
2024-12-12 11:17:21 +0100

    Return updated addresses (#329)
    * Return updated addresses
    
    * fix linter

--- dependencies/ccip-v2-scripts/scripts/2_deploy_ccip.go
+++ dependencies/ccip-v2-scripts/scripts/2_deploy_ccip.go
@@ -19,24 +19,23 @@ func DeployCCIPAndAddLanes(logger *zap.SugaredLogger, env config.DevspaceEnv, ou
 
 	reader := ccip.NewOutputReader(outputDir)
 	addressBook := reader.ReadAddressBook()
-	addresses, err := addressBook.Addresses()
-	if err != nil {
-		panic(err)
-	}
+
 	ccipLogger, _ := v2logger.NewLogger()
 	ctx := context.Background()
 
 	output, err := crib.DeployCCIPAndAddLanes(ctx, ccipLogger, envConfig, alphaChainSel, betaChainSel, addressBook)
 	if err != nil {
-		logger.Error("Deployment failed due to error", "error", err.Error())
+		logger.Error("deployment failed due to error", "error", err.Error())
 		panic(err)
 	}
 
 	envState := model.NewEnvState(logger, env)
+
+	addresses, err := output.AddressBook.Addresses()
 	if err != nil {
+		logger.Error("failed to get addresses from address book", "error", err.Error())
 		panic(err)
 	}
-
 	envState.SaveAddressBook(addresses)
 	envState.SaveNodeDetails(ccip.NodesDetails{
 		NodeIDs: output.NodeIDs,
