c58941621603fbea0a2eb8a59a67ae96394d1406
Abdelrahman Soliman (Boda) <2677789+asoliman92@users.noreply.github.com>
2024-12-13 19:42:51 +0200

    Reduce commit report size in observation phase in exec plugin (#348)
    * Hash messages earlier in the observation and use hashes directly in ConstructMerkleTree function
    * Truncate observations' reports on messages basis

--- execute/outcome.go
+++ execute/outcome.go
@@ -71,7 +71,7 @@ func (p *Plugin) Outcome(
 	case exectypes.GetCommitReports:
 		outcome = p.getCommitReportsOutcome(observation)
 	case exectypes.GetMessages:
-		outcome = p.getMessagesOutcome(observation, previousOutcome)
+		outcome = p.getMessagesOutcome(observation)
 	case exectypes.Filter:
 		outcome, err = p.getFilterOutcome(ctx, observation, previousOutcome)
 	default:
@@ -120,33 +120,47 @@ func (p *Plugin) getCommitReportsOutcome(observation exectypes.Observation) exec
 
 func (p *Plugin) getMessagesOutcome(
 	observation exectypes.Observation,
-	previousOutcome exectypes.Outcome,
 ) exectypes.Outcome {
-	commitReports := previousOutcome.PendingCommitReports
+	commitReports := make([]exectypes.CommitData, 0)
 	costlyMessagesSet := mapset.NewSet[cciptypes.Bytes32]()
 	for _, msgID := range observation.CostlyMessages {
 		costlyMessagesSet.Add(msgID)
 	}
 
+	// First ensure that all observed messages has hashes and token data.
+	if err := validateHashesExist(observation.Messages, observation.Hashes); err != nil {
+		p.lggr.Errorw("validate hashes exist: %w", err)
+		return exectypes.Outcome{}
+	}
+	if err := validateTokenDataObservations(observation.Messages, observation.TokenData); err != nil {
+		p.lggr.Errorw("validate token data observations: %w", err)
+		return exectypes.Outcome{}
+	}
+
+	reports := observation.CommitReports.Flatten()
 	// add messages to their commitReports.
-	for i, report := range commitReports {
+	for i, report := range reports {
 		report.Messages = nil
 		report.CostlyMessages = nil
 		for j := report.SequenceNumberRange.Start(); j <= report.SequenceNumberRange.End(); j++ {
 			if msg, ok := observation.Messages[report.SourceChain][j]; ok {
+				// Always add the message and hash, even if it wont be executed.
+				// This slice must have an entry for each message in the commit range.
 				report.Messages = append(report.Messages, msg)
+
+				report.Hashes = append(report.Hashes, observation.Hashes[report.SourceChain][j])
+
 				if costlyMessagesSet.Contains(msg.Header.MessageID) {
 					report.CostlyMessages = append(report.CostlyMessages, msg.Header.MessageID)
 				}
-			}
-
-			if tokenData, ok := observation.TokenData[report.SourceChain][j]; ok {
-				report.MessageTokenData = append(report.MessageTokenData, tokenData)
+				report.MessageTokenData = append(report.MessageTokenData, observation.TokenData[report.SourceChain][j])
 			}
 		}
-		commitReports[i].Messages = report.Messages
-		commitReports[i].MessageTokenData = report.MessageTokenData
-		commitReports[i].CostlyMessages = report.CostlyMessages
+		if len(report.Messages) == 0 {
+			// If there are no messages, remove the commit report.
+			commitReports = append(commitReports[:i], commitReports[i+1:]...)
+		}
+		commitReports = append(commitReports, report)
 	}
 
 	// Must use 'NewOutcome' rather than direct struct initialization to ensure the outcome is sorted.
