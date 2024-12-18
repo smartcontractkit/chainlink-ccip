7622cb0f85fc3126a7dee5d628fd01b642998090
toblich <toblich@users.noreply.github.com>
2024-12-18 17:56:02 -0300

    First implementation to transfer fees

--- chains/solana/gobindings/ccip_router/instructions.go
+++ chains/solana/gobindings/ccip_router/instructions.go
@@ -276,6 +276,16 @@ var (
 	// The fee amount in u64.
 	Instruction_GetFee = ag_binary.TypeID([8]byte{115, 195, 235, 161, 25, 219, 60, 29})
 
+	// Transfers the accumulated billed fees in a particular token to an arbitrary token account.
+	// Only the CCIP Admin can extract billed fees.
+	//
+	// # Arguments
+	//
+	// * `ctx` - The context containing the accounts required for the transfer of billed fees.
+	// * `transfer_all` - A flag indicating whether to transfer all the accumulated fees in that token or not.
+	// * `desired_amount` - The amount to transfer. If `transfer_all` is true, this value must be 0.
+	Instruction_TransferBilledFunds = ag_binary.TypeID([8]byte{72, 138, 134, 233, 194, 10, 23, 127})
+
 	// ON RAMP FLOW
 	// Sends a message to the destination chain.
 	//
@@ -403,6 +413,8 @@ func InstructionIDToName(id ag_binary.TypeID) string {
 		return "RemoveBillingTokenConfig"
 	case Instruction_GetFee:
 		return "GetFee"
+	case Instruction_TransferBilledFunds:
+		return "TransferBilledFunds"
 	case Instruction_CcipSend:
 		return "CcipSend"
 	case Instruction_Commit:
@@ -499,6 +511,9 @@ var InstructionImplDef = ag_binary.NewVariantDefinition(
 		},
 		{
 			"get_fee", (*GetFee)(nil),
+		},
+		{
+			"transfer_billed_funds", (*TransferBilledFunds)(nil),
 		},
 		{
 			"ccip_send", (*CcipSend)(nil),
