3cf73aeaa7621e3312c7a27a96b85fb576827bf7
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-12-18 11:52:26 +0200

    fix imports

--- execute/plugin_functions_test.go
+++ execute/plugin_functions_test.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	mapset "github.com/deckarep/golang-set/v2"
-	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
 	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/assert"
