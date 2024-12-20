c773fbc2a34626dafb9fcc5637bf6a1f4089174c
toblich <toblich@users.noreply.github.com>
2024-12-20 16:44:10 -0300

    Better type for TokenProgram when it can be 2022 or pre-2022

--- chains/solana/contracts/programs/ccip-router/src/context.rs
+++ chains/solana/contracts/programs/ccip-router/src/context.rs
@@ -346,13 +346,7 @@ pub struct AddBillingTokenConfig<'info> {
     )]
     pub billing_token_config: Account<'info, BillingTokenConfigWrapper>,
 
-    /// CHECK: This is the token program OR the token-2022 program. Given that there are 2 options, this can't have the
-    /// type of a specific program (which would enforce its ID). Thus, it's an UncheckedAccount
-    /// with a constraint enforcing that it is one of the two allowed programs.
-    #[account(
-        constraint = TokenInterface::ids().contains(&token_program.key()) @ CcipRouterError::InvalidInputs,
-    )]
-    pub token_program: UncheckedAccount<'info>,
+    pub token_program: Interface<'info, TokenInterface>,
 
     #[account(
         owner = token_program.key() @ CcipRouterError::InvalidInputs,
@@ -425,13 +419,7 @@ pub struct RemoveBillingTokenConfig<'info> {
     )]
     pub billing_token_config: Account<'info, BillingTokenConfigWrapper>,
 
-    /// CHECK: This is the token program OR the token-2022 program. Given that there are 2 options, this can't have the
-    /// type of a specific program (which would enforce its ID). Thus, it's an UncheckedAccount
-    /// with a constraint enforcing that it is one of the two allowed programs.
-    #[account(
-        constraint = TokenInterface::ids().contains(&token_program.key()) @ CcipRouterError::InvalidInputs,
-    )]
-    pub token_program: UncheckedAccount<'info>,
+    pub token_program: Interface<'info, TokenInterface>,
 
     #[account(
         owner = token_program.key() @ CcipRouterError::InvalidInputs,
@@ -497,13 +485,7 @@ pub struct CcipSend<'info> {
     // billing token //
     ///////////////////
     // TODO improve all usages of CcipRouterError::InvalidInputs to be more specific
-    /// CHECK: This is the token program OR the token-2022 program. Given that there are 2 options, this can't have the
-    /// type of a specific program (which would enforce its ID). Thus, it's an UncheckedAccount
-    /// with a constraint enforcing that it is one of the two allowed programs.
-    #[account(
-        constraint = TokenInterface::ids().contains(&fee_token_program.key()) @ CcipRouterError::InvalidInputs,
-    )]
-    pub fee_token_program: UncheckedAccount<'info>,
+    pub fee_token_program: Interface<'info, TokenInterface>,
 
     #[account(
         owner = fee_token_program.key() @ CcipRouterError::InvalidInputs,
