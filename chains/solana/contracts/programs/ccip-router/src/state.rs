6d06ece7bc00c911827a5c1faefeb9f8279fc88e
Tobías Lichtig <toblich@users.noreply.github.com>
2025-01-13 14:55:40 -0300

    Refactor: Moving logic out of `state.rs` [NONEVM-1114] (#437)

--- chains/solana/contracts/programs/ccip-router/src/state.rs
+++ chains/solana/contracts/programs/ccip-router/src/state.rs
@@ -1,11 +1,6 @@
 use anchor_lang::prelude::*;
 use ethnum::U256;
 
-use crate::{
-    valid_version, CcipRouterError, FEE_BILLING_TOKEN_CONFIG, MAX_TOKEN_AND_CHAIN_CONFIG_V,
-    TOKEN_POOL_BILLING_SEED,
-};
-
 // zero_copy is used to prevent hitting stack/heap memory limits
 #[account(zero_copy)]
 #[derive(InitSpace, AnchorSerialize, AnchorDeserialize)]
@@ -154,55 +149,6 @@ pub struct CommitReport {
     pub execution_states: u128,
 }
 
-impl CommitReport {
-    pub fn set_state(&mut self, sequence_number: u64, execution_state: MessageExecutionState) {
-        let packed = &mut self.execution_states;
-        let dif = sequence_number.checked_sub(self.min_msg_nr);
-        assert!(dif.is_some(), "Sequence number out of bounds");
-        let i = dif.unwrap();
-        assert!(i < 64, "Sequence number out of bounds");
-
-        // Clear the 2 bits at position 'i'
-        *packed &= !(0b11 << (i * 2));
-        // Set the new value in the cleared bits
-        *packed |= (execution_state as u128) << (i * 2);
-    }
-
-    pub fn get_state(&self, sequence_number: u64) -> MessageExecutionState {
-        let packed = self.execution_states;
-        let dif = sequence_number.checked_sub(self.min_msg_nr);
-        assert!(dif.is_some(), "Sequence number out of bounds");
-        let i = dif.unwrap();
-        assert!(i < 64, "Sequence number out of bounds");
-
-        let mask = 0b11 << (i * 2);
-        let state = (packed & mask) >> (i * 2);
-        MessageExecutionState::try_from(state).unwrap()
-    }
-}
-
-#[derive(Clone, AnchorSerialize, AnchorDeserialize, Debug, PartialEq)]
-pub enum MessageExecutionState {
-    Untouched = 0,
-    InProgress = 1, // Not used in Solana, but used in EVM
-    Success = 2,
-    Failure = 3,
-}
-
-impl TryFrom<u128> for MessageExecutionState {
-    type Error = &'static str;
-
-    fn try_from(value: u128) -> std::result::Result<MessageExecutionState, &'static str> {
-        match value {
-            0 => Ok(MessageExecutionState::Untouched),
-            1 => Ok(MessageExecutionState::InProgress),
-            2 => Ok(MessageExecutionState::Success),
-            3 => Ok(MessageExecutionState::Failure),
-            _ => Err("Invalid ExecutionState"),
-        }
-    }
-}
-
 #[account]
 #[derive(InitSpace, Debug)]
 pub struct PerChainPerTokenConfig {
@@ -211,30 +157,6 @@ pub struct PerChainPerTokenConfig {
     pub mint: Pubkey,        // token on solana
 
     pub billing: TokenBilling, // EVM: configurable in router only by ccip admins
-}
-
-impl PerChainPerTokenConfig {
-    pub fn validated_try_from<'info>(
-        account: &'info AccountInfo<'info>,
-        token: Pubkey,
-        dest_chain_selector: u64,
-    ) -> Result<Self> {
-        let (expected, _) = Pubkey::find_program_address(
-            &[
-                TOKEN_POOL_BILLING_SEED,
-                dest_chain_selector.to_le_bytes().as_ref(),
-                token.key().as_ref(),
-            ],
-            &crate::ID,
-        );
-        require_keys_eq!(account.key(), expected, CcipRouterError::InvalidInputs);
-        let account = Account::<PerChainPerTokenConfig>::try_from(account)?;
-        require!(
-            valid_version(account.version, MAX_TOKEN_AND_CHAIN_CONFIG_V),
-            CcipRouterError::InvalidInputs
-        );
-        Ok(account.into_inner())
-    }
 }
 
 #[derive(InitSpace, Debug, Clone, AnchorSerialize, AnchorDeserialize)]
@@ -268,29 +190,6 @@ pub struct BillingTokenConfig {
     pub usd_per_token: TimestampedPackedU224,
     // billing configs
     pub premium_multiplier_wei_per_eth: u64,
-}
-
-impl BillingTokenConfig {
-    // Returns Ok(None) when parsing the ZERO address, which is a valid input from users
-    // specifying a token that has no Billing config.
-    pub fn validated_try_from<'info>(
-        account: &'info AccountInfo<'info>,
-        token: Pubkey,
-    ) -> Result<Option<Self>> {
-        if account.key() == Pubkey::default() {
-            return Ok(None);
-        }
-
-        let (expected, _) =
-            Pubkey::find_program_address(&[FEE_BILLING_TOKEN_CONFIG, token.as_ref()], &crate::ID);
-        require_keys_eq!(account.key(), expected, CcipRouterError::InvalidInputs);
-        let account = Account::<BillingTokenConfigWrapper>::try_from(account)?;
-        require!(
-            valid_version(account.version, 1),
-            CcipRouterError::InvalidInputs
-        );
-        Ok(Some(account.into_inner().config))
-    }
 }
 
 #[account]
@@ -344,105 +243,32 @@ impl UnpackedDoubleU224 {
     }
 }
 
+#[derive(Clone, AnchorSerialize, AnchorDeserialize, Debug, PartialEq)]
+pub enum MessageExecutionState {
+    Untouched = 0,
+    InProgress = 1, // Not used in Solana, but used in EVM
+    Success = 2,
+    Failure = 3,
+}
+
+impl TryFrom<u128> for MessageExecutionState {
+    type Error = &'static str;
+
+    fn try_from(value: u128) -> std::result::Result<MessageExecutionState, &'static str> {
+        match value {
+            0 => Ok(MessageExecutionState::Untouched),
+            1 => Ok(MessageExecutionState::InProgress),
+            2 => Ok(MessageExecutionState::Success),
+            3 => Ok(MessageExecutionState::Failure),
+            _ => Err("Invalid ExecutionState"),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::convert::TryFrom;
-
-    #[test]
-    fn test_set_state() {
-        let mut commit_report = CommitReport {
-            version: 1,
-            chain_selector: 0,
-            merkle_root: [0; 32],
-            timestamp: 0,
-            min_msg_nr: 0,
-            max_msg_nr: 64,
-            execution_states: 0,
-        };
-
-        commit_report.set_state(0, MessageExecutionState::Success);
-        assert_eq!(commit_report.get_state(0), MessageExecutionState::Success);
-
-        commit_report.set_state(1, MessageExecutionState::Failure);
-        assert_eq!(commit_report.get_state(1), MessageExecutionState::Failure);
-
-        commit_report.set_state(2, MessageExecutionState::Untouched);
-        assert_eq!(commit_report.get_state(2), MessageExecutionState::Untouched);
-
-        commit_report.set_state(3, MessageExecutionState::InProgress);
-        assert_eq!(
-            commit_report.get_state(3),
-            MessageExecutionState::InProgress
-        );
-    }
-
-    #[test]
-    #[should_panic(expected = "Sequence number out of bounds")]
-    fn test_set_state_out_of_bounds() {
-        let mut commit_report = CommitReport {
-            version: 1,
-            chain_selector: 1,
-            merkle_root: [0; 32],
-            timestamp: 1,
-            min_msg_nr: 1500,
-            max_msg_nr: 1530,
-            execution_states: 0,
-        };
-
-        commit_report.set_state(65, MessageExecutionState::Success);
-    }
-
-    #[test]
-    fn test_get_state() {
-        let mut commit_report = CommitReport {
-            version: 1,
-            chain_selector: 1,
-            merkle_root: [0; 32],
-            timestamp: 1,
-            min_msg_nr: 1500,
-            max_msg_nr: 1530,
-            execution_states: 0,
-        };
-
-        commit_report.set_state(1501, MessageExecutionState::Success);
-        commit_report.set_state(1505, MessageExecutionState::Failure);
-        commit_report.set_state(1520, MessageExecutionState::Untouched);
-        commit_report.set_state(1523, MessageExecutionState::InProgress);
-
-        assert_eq!(
-            commit_report.get_state(1501),
-            MessageExecutionState::Success
-        );
-        assert_eq!(
-            commit_report.get_state(1505),
-            MessageExecutionState::Failure
-        );
-        assert_eq!(
-            commit_report.get_state(1520),
-            MessageExecutionState::Untouched
-        );
-        assert_eq!(
-            commit_report.get_state(1523),
-            MessageExecutionState::InProgress
-        );
-    }
-
-    #[test]
-    #[should_panic(expected = "Sequence number out of bounds")]
-    fn test_get_state_out_of_bounds() {
-        let commit_report = CommitReport {
-            version: 1,
-            chain_selector: 1,
-            merkle_root: [0; 32],
-            timestamp: 1,
-            min_msg_nr: 1500,
-            max_msg_nr: 1530,
-            execution_states: 0,
-        };
-
-        commit_report.get_state(65);
-    }
 
     #[test]
     fn test_execution_state_try_from() {
