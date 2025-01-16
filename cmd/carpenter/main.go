5327e27515275e4c17ada3b7154c436a096dd5d5
Will Winder <wwinder.unh@gmail.com>
2025-01-16 14:54:47 -0500

    Add cli handling so file is configurable.

--- cmd/carpenter/main.go
+++ cmd/carpenter/main.go
@@ -1,85 +1,16 @@
 package main
 
 import (
-	"bufio"
+	"context"
 	"fmt"
 	"os"
-	"time"
-
-	"github.com/charmbracelet/lipgloss"
 
 	_ "github.com/smartcontractkit/chainlink-ccip/cmd/carpenter/internal/filter"
-	"github.com/smartcontractkit/chainlink-ccip/cmd/carpenter/internal/parse"
-	"github.com/smartcontractkit/chainlink-ccip/cmd/carpenter/internal/stream"
 )
 
-func maybeExit(err error, s string, a ...interface{}) {
-	if err == nil {
-		return
-	}
-	fmt.Fprintf(os.Stderr, s+"\n", a...)
-	os.Exit(1)
-}
-
-// renderData
-/*
-
-2024-12-04T20:15:35Z | 1.1.1 |       Commit(MerkleRoot) | <processor details>
-                       | | |           |     |-- processor
-                       | | |           |-- OCR Plugin
-                       | | |-- sequence number
-                       | |-- DON ID
-                       -- oracleID
-
-*/
-func renderData(data *parse.Data) {
-	// simple color selection algorithm
-	withColor := func(in interface{}, i int) string {
-		color := fmt.Sprintf("%d", i%7+1)
-		str := fmt.Sprintf("%v", in)
-
-		return lipgloss.NewStyle().Foreground(lipgloss.Color(color)).Render(str)
-	}
-
-	var timeStyle = lipgloss.NewStyle().Width(10).Height(1).MaxHeight(1).
-		Align(lipgloss.Center)
-	var uidStyle = lipgloss.NewStyle().Width(15).Height(1).MaxHeight(1).
-		Align(lipgloss.Left).PaddingLeft(1).Bold(true)
-	var messageStyle = lipgloss.NewStyle().Width(50).Height(1).MaxHeight(1).
-		Align(lipgloss.Left).PaddingLeft(1)
-
-	uid := fmt.Sprintf("%s.%s.%s",
-		withColor(data.OracleID, data.OracleID),
-		withColor(data.DONID, data.DONID),
-		withColor(data.SequenceNumber, data.SequenceNumber),
-	)
-
-	fmt.Printf("%s|%s|%s\n",
-		timeStyle.Render(data.Timestamp.Format(time.TimeOnly)),
-		uidStyle.Render(uid),
-		messageStyle.Render(data.Message),
-	)
-}
-
 func main() {
-	inputStream, err := stream.InitializeInputStream(stream.InputOptions{})
-	maybeExit(err, "failed to initialize input stream: %s", err)
-
-	scanner := bufio.NewScanner(inputStream)
-	for scanner.Scan() {
-		line := scanner.Text()
-		data, err := parse.Filter(line)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Unable to get data: %s", err)
-			continue
-		}
-		if data == nil {
-			// no data to display.
-			continue
-		}
-
-		renderData(data)
-		// TODO: render data.
-		fmt.Println(data)
+	err := makeCommand().Run(context.Background(), os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Problem running command: %s\n", err.Error())
 	}
 }
--- go.mod
+++ go.mod
@@ -54,6 +54,7 @@ require (
 	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/urfave/cli/v3 v3.0.0-beta1 // indirect
 	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
 	go.opentelemetry.io/otel v1.30.0 // indirect
 	go.opentelemetry.io/otel/metric v1.30.0 // indirect
