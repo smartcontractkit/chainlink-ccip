179d1c343747cda792d8101bc0f45420772e9ebe
Will Winder <wwinder.unh@gmail.com>
2025-01-17 11:41:46 -0500

    WIP

--- execute/report/txm_checker.go
+++ execute/report/txm_checker.go
@@ -2,6 +2,7 @@ package report
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
@@ -25,10 +26,43 @@ func getStatusGetters(writers map[ccipocr3.ChainSelector]types.ContractWriter) m
 	return statusers
 }
 
-func NewTXMCheck(txm map[ccipocr3.ChainSelector]StatusGetter) Check {
-	return func(lggr logger.Logger, msg ccipocr3.Message, idx int, report exectypes.CommitData) (messageStatus, error) {
-		var cw types.ContractWriter
-		cw.GetTransactionStatus()
+// NewTXMCheck creates a new check that queries the TXM for the status of a transaction.
+//
+// Algorithm ported from OCR2 version: chainlink/core/services/relay/evm/statuschecker/txm_status_checker.go#L31
+func NewTXMCheck(statusGetter StatusGetter, maxAttempts uint64) Check {
+	return func(
+		ctx context.Context,
+		lggr logger.Logger,
+		msg ccipocr3.Message,
+		idx int,
+		report exectypes.CommitData,
+	) (messageStatus, error) {
+
+		allStatuses := make([]types.TransactionStatus, 0)
+
+		var counter uint64
+		for {
+			transactionID := fmt.Sprintf("%s-%d", msg.Header.MessageID.String(), counter)
+			status, err := statusGetter.GetTransactionStatus(ctx, transactionID)
+			if err != nil && status == types.Unknown {
+				// If the status is unknown and err not nil, it means the transaction was not found
+				break
+			}
+			allStatuses = append(allStatuses, status)
+			counter++
+
+			// Break the loop if the cap is reached
+			if counter >= maxAttempts {
+				lggr.Infow(
+					"skipping message due to txm max attempts reached",
+					"message",
+					msg,
+					"maxAttempts",
+					maxAttempts,
+				)
+				return TXMCheckError, nil
+			}
+		}
 		return None, nil
 	}
 }
