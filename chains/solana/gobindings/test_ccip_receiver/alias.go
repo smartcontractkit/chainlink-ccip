82938f4801c9d6e590b791310a0a6494e8d6f0f3
aalu1418 <50029043+aalu1418@users.noreply.github.com>
2025-01-30 14:24:21 -0700

    lint fix

--- chains/solana/gobindings/test_ccip_receiver/alias.go
+++ chains/solana/gobindings/test_ccip_receiver/alias.go
@@ -1,11 +1,11 @@
-package test_ccip_receiver
+package test_ccip_receiver // revive:disable-line:var-naming
 
 import (
-	ccip_receiver "github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/example_ccip_receiver"
+	ccipReceiver "github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/example_ccip_receiver"
 )
 
 // DO NOT DELETE - imported custom types are are not-automatically resolved by `anchor-go` but are declared in the anchor idl
 // this files aliases types from other modules to ensure the go modules compile
 
-type BaseState = ccip_receiver.BaseState
-type Any2SVMMessage = ccip_receiver.Any2SVMMessage
+type BaseState = ccipReceiver.BaseState
+type Any2SVMMessage = ccipReceiver.Any2SVMMessage
