fe429267b71335f6bd05371fb5092e3664d31efb
Tobías Lichtig <toblich@users.noreply.github.com>
2025-02-05 18:25:48 -0300

    Refactor: Splitting the offramp [NONEVM-1200] (#553)

--- chains/solana/contracts/programs/ccip-router/src/instructions/v1/mod.rs
+++ chains/solana/contracts/programs/ccip-router/src/instructions/v1/mod.rs
@@ -2,17 +2,12 @@
 // Public modules //
 ////////////////////
 pub mod admin;
-pub mod offramp;
 pub mod onramp;
 pub mod token_admin_registry;
 
 /////////////////////
 // Private modules //
 /////////////////////
-mod config;
 mod fees;
-mod merkle;
 mod messages;
-mod ocr3base;
-mod ocr3impl;
 mod pools;
