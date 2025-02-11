4193d6a77d1eb8ce03e57ef1b7841706caea943d
Makram Kamaleddine <makramkd@users.noreply.github.com>
2025-02-11 14:03:04 +0200

    pkg/reader: fix Errorw call

--- pkg/reader/ccip.go
+++ pkg/reader/ccip.go
@@ -180,7 +180,7 @@ func (r *ccipChainReader) CommitReportsGTETimestamp(
 				cciptypes.ChainSelector(mr.SourceChainSelector),
 			)
 			if err != nil {
-				r.lggr.Errorw("get onRamp address for selector %d: %w", mr.SourceChainSelector, err)
+				r.lggr.Errorw("get onRamp address for selector", "sourceChain", mr.SourceChainSelector, "err", err)
 				continue
 			}
 
