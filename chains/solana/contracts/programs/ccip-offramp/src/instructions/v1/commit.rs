c12933330b97eae74e0e5119505907aad0bfd8d1
PabloMansanet <pablo.mansanet@smartcontract.com>
2025-02-14 10:06:29 +0100

    Deduplicate price update logic

--- chains/solana/contracts/programs/ccip-offramp/src/instructions/v1/commit.rs
+++ chains/solana/contracts/programs/ccip-offramp/src/instructions/v1/commit.rs
@@ -58,92 +58,26 @@ pub fn commit<'info>(
             CcipOfframpError::InvalidInputsNumberOfAccounts
         );
     } else {
-        // There are price updates in the report.
-        // Remaining accounts represent:
-        // - The state account to store the price sequence updates
-        // - the accounts to update BillingTokenConfig for token prices
-        // - the accounts to update DestChain for gas prices
-        // They must be in order:
-        // 1. state_account
-        // 2. fee quoter token_accounts[]
-        // 3. fee quoter gas_accounts[]
-        // matching the order of the price updates in the CommitInput.
-        // They must also all be writable so they can be updated.
-        let minimum_remaining_accounts = 1
-            + report.price_updates.token_price_updates.len()
-            + report.price_updates.gas_price_updates.len();
-        require_eq!(
-            ctx.remaining_accounts.len(),
-            minimum_remaining_accounts,
-            CcipOfframpError::InvalidInputsNumberOfAccounts
-        );
+        let cpi_seeds = &[seed::FEE_BILLING_SIGNER, &[ctx.bumps.fee_billing_signer]];
+        let cpi_signer = &[&cpi_seeds[..]];
+        let cpi_program = ctx.accounts.fee_quoter.to_account_info();
+        let cpi_accounts = fee_quoter::cpi::accounts::UpdatePrices {
+            config: ctx.accounts.fee_quoter_config.to_account_info(),
+            authority: ctx.accounts.fee_billing_signer.to_account_info(),
+            allowed_price_updater: ctx
+                .accounts
+                .fee_quoter_allowed_price_updater
+                .to_account_info(),
+        };
 
-        let ocr_sequence_number = report_context.sequence_number();
-
-        // The Global state PDA is sent as a remaining_account as it is optional to avoid having the lock when not modifying it, so all validations need to be done manually
-        let (expected_state_key, _) = Pubkey::find_program_address(&[seed::STATE], &crate::ID);
-        require_keys_eq!(
-            ctx.remaining_accounts[0].key(),
-            expected_state_key,
-            CcipOfframpError::InvalidInputsGlobalStateAccount
-        );
-        require!(
-            ctx.remaining_accounts[0].is_writable,
-            CcipOfframpError::InvalidInputsMissingWritable
-        );
-
-        let mut global_state: Account<GlobalState> = Account::try_from(&ctx.remaining_accounts[0])?;
-
-        if global_state.latest_price_sequence_number < ocr_sequence_number {
-            // Update the persisted sequence number
-            global_state.latest_price_sequence_number = ocr_sequence_number;
-            global_state.exit(&crate::ID)?; // as it is manually loaded, it also has to be manually written back
-
-            let cpi_seeds = &[seed::FEE_BILLING_SIGNER, &[ctx.bumps.fee_billing_signer]];
-            let cpi_signer = &[&cpi_seeds[..]];
-            let cpi_program = ctx.accounts.fee_quoter.to_account_info();
-            let cpi_accounts = fee_quoter::cpi::accounts::UpdatePrices {
-                config: ctx.accounts.fee_quoter_config.to_account_info(),
-                authority: ctx.accounts.fee_billing_signer.to_account_info(),
-                allowed_price_updater: ctx
-                    .accounts
-                    .fee_quoter_allowed_price_updater
-                    .to_account_info(),
-            };
-            let cpi_remaining_accounts = ctx.remaining_accounts[1..].to_vec();
-            let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, cpi_signer)
-                .with_remaining_accounts(cpi_remaining_accounts);
-
-            let token_price_updates = report
-                .price_updates
-                .token_price_updates
-                .iter()
-                .map(|u| fee_quoter::context::TokenPriceUpdate {
-                    source_token: u.source_token,
-                    usd_per_token: u.usd_per_token,
-                })
-                .collect();
-            let gas_price_update = report
-                .price_updates
-                .gas_price_updates
-                .iter()
-                .map(|u| fee_quoter::context::GasPriceUpdate {
-                    dest_chain_selector: u.dest_chain_selector,
-                    usd_per_unit_gas: u.usd_per_unit_gas,
-                })
-                .collect();
-
-            fee_quoter::cpi::update_prices(cpi_ctx, token_price_updates, gas_price_update)?;
-        } else {
-            // TODO check if this is really necessary. EVM has this validation checking that the
-            // array of merkle roots in the report is not empty. But here, considering we only have 1 root per report,
-            // this check is just validating that the root is not zeroed
-            // (which should never happen anyway, so it may be redundant).
-            require!(
-                report.merkle_root.as_ref().unwrap().source_chain_selector > 0,
-                CcipOfframpError::StaleCommitReport
-            );
-        }
+        helpers::update_prices(
+            &report,
+            &report_context,
+            ctx.remaining_accounts,
+            cpi_signer,
+            cpi_program,
+            cpi_accounts,
+        )?;
     }
 
     // The Commit Report Account stores the information of 1 Commit Report:
@@ -237,82 +171,26 @@ pub fn commit_price_only<'info>(
         CcipOfframpError::MissingExpectedPriceUpdates
     );
 
-    // Remaining accounts represent:
-    // - The state account to store the price sequence updates
-    // - the accounts to update BillingTokenConfig for token prices
-    // - the accounts to update DestChain for gas prices
-    // They must be in order:
-    // 1. state_account
-    // 2. fee quoter token_accounts[]
-    // 3. fee quoter gas_accounts[]
-    // matching the order of the price updates in the CommitInput.
-    // They must also all be writable so they can be updated.
-    let minimum_remaining_accounts = 1
-        + report.price_updates.token_price_updates.len()
-        + report.price_updates.gas_price_updates.len();
-    require_eq!(
-        ctx.remaining_accounts.len(),
-        minimum_remaining_accounts,
-        CcipOfframpError::InvalidInputsNumberOfAccounts
-    );
+    let cpi_seeds = &[seed::FEE_BILLING_SIGNER, &[ctx.bumps.fee_billing_signer]];
+    let cpi_signer = &[&cpi_seeds[..]];
+    let cpi_program = ctx.accounts.fee_quoter.to_account_info();
+    let cpi_accounts = fee_quoter::cpi::accounts::UpdatePrices {
+        config: ctx.accounts.fee_quoter_config.to_account_info(),
+        authority: ctx.accounts.fee_billing_signer.to_account_info(),
+        allowed_price_updater: ctx
+            .accounts
+            .fee_quoter_allowed_price_updater
+            .to_account_info(),
+    };
 
-    let ocr_sequence_number = report_context.sequence_number();
-
-    // The Global state PDA is sent as a remaining_account as it is optional to avoid having the lock when not modifying it, so all validations need to be done manually
-    let (expected_state_key, _) = Pubkey::find_program_address(&[seed::STATE], &crate::ID);
-    require_keys_eq!(
-        ctx.remaining_accounts[0].key(),
-        expected_state_key,
-        CcipOfframpError::InvalidInputsGlobalStateAccount
-    );
-    require!(
-        ctx.remaining_accounts[0].is_writable,
-        CcipOfframpError::InvalidInputsMissingWritable
-    );
-
-    let mut global_state: Account<GlobalState> = Account::try_from(&ctx.remaining_accounts[0])?;
-
-    if global_state.latest_price_sequence_number < ocr_sequence_number {
-        // Update the persisted sequence number
-        global_state.latest_price_sequence_number = ocr_sequence_number;
-        global_state.exit(&crate::ID)?; // as it is manually loaded, it also has to be manually written back
-
-        let cpi_seeds = &[seed::FEE_BILLING_SIGNER, &[ctx.bumps.fee_billing_signer]];
-        let cpi_signer = &[&cpi_seeds[..]];
-        let cpi_program = ctx.accounts.fee_quoter.to_account_info();
-        let cpi_accounts = fee_quoter::cpi::accounts::UpdatePrices {
-            config: ctx.accounts.fee_quoter_config.to_account_info(),
-            authority: ctx.accounts.fee_billing_signer.to_account_info(),
-            allowed_price_updater: ctx
-                .accounts
-                .fee_quoter_allowed_price_updater
-                .to_account_info(),
-        };
-        let cpi_remaining_accounts = ctx.remaining_accounts[1..].to_vec();
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, cpi_signer)
-            .with_remaining_accounts(cpi_remaining_accounts);
-
-        let token_price_updates = report
-            .price_updates
-            .token_price_updates
-            .iter()
-            .map(|u| fee_quoter::context::TokenPriceUpdate {
-                source_token: u.source_token,
-                usd_per_token: u.usd_per_token,
-            })
-            .collect();
-        let gas_price_update = report
-            .price_updates
-            .gas_price_updates
-            .iter()
-            .map(|u| fee_quoter::context::GasPriceUpdate {
-                dest_chain_selector: u.dest_chain_selector,
-                usd_per_unit_gas: u.usd_per_unit_gas,
-            })
-            .collect();
-
-        fee_quoter::cpi::update_prices(cpi_ctx, token_price_updates, gas_price_update)?;
-    }
+    helpers::update_prices(
+        &report,
+        &report_context,
+        ctx.remaining_accounts,
+        cpi_signer,
+        cpi_program,
+        cpi_accounts,
+    )?;
 
     emit!(PriceOnlyCommitReportAccepted {
         price_updates: report.price_updates.clone(),
@@ -329,4 +207,89 @@ pub fn commit_price_only<'info>(
     )?;
 
     Ok(())
+}
+
+mod helpers {
+
+    use fee_quoter::cpi::accounts::UpdatePrices;
+
+    use super::*;
+    pub fn update_prices<'info>(
+        report: &CommitInput,
+        report_context: &ReportContext,
+        remaining_accounts: &'info [AccountInfo<'info>],
+        cpi_signer: &[&[&[u8]]; 1],
+        cpi_program: AccountInfo<'info>,
+        cpi_accounts: UpdatePrices<'info>,
+    ) -> Result<()> {
+        // Remaining accounts represent:
+        // - The state account to store the price sequence updates
+        // - the accounts to update BillingTokenConfig for token prices
+        // - the accounts to update DestChain for gas prices
+        // They must be in order:
+        // 1. state_account
+        // 2. fee quoter token_accounts[]
+        // 3. fee quoter gas_accounts[]
+        // matching the order of the price updates in the CommitInput.
+        // They must also all be writable so they can be updated.
+
+        let minimum_remaining_accounts = 1
+            + report.price_updates.token_price_updates.len()
+            + report.price_updates.gas_price_updates.len();
+        require_eq!(
+            remaining_accounts.len(),
+            minimum_remaining_accounts,
+            CcipOfframpError::InvalidInputsNumberOfAccounts
+        );
+
+        let ocr_sequence_number = report_context.sequence_number();
+
+        // The Global state PDA is sent as a remaining_account as it is optional to avoid having the lock when not modifying it, so all validations need to be done manually
+        let (expected_state_key, _) = Pubkey::find_program_address(&[seed::STATE], &crate::ID);
+        require_keys_eq!(
+            remaining_accounts[0].key(),
+            expected_state_key,
+            CcipOfframpError::InvalidInputsGlobalStateAccount
+        );
+        require!(
+            remaining_accounts[0].is_writable,
+            CcipOfframpError::InvalidInputsMissingWritable
+        );
+
+        let mut global_state: Account<'info, GlobalState> =
+            Account::try_from(&remaining_accounts[0])?;
+
+        if global_state.latest_price_sequence_number < ocr_sequence_number {
+            // Update the persisted sequence number
+            global_state.latest_price_sequence_number = ocr_sequence_number;
+            global_state.exit(&crate::ID)?; // as it is manually loaded, it also has to be manually written back
+
+            let cpi_remaining_accounts = remaining_accounts[1..].to_vec();
+            let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, cpi_signer)
+                .with_remaining_accounts(cpi_remaining_accounts);
+
+            let token_price_updates = report
+                .price_updates
+                .token_price_updates
+                .iter()
+                .map(|u| fee_quoter::context::TokenPriceUpdate {
+                    source_token: u.source_token,
+                    usd_per_token: u.usd_per_token,
+                })
+                .collect();
+            let gas_price_update = report
+                .price_updates
+                .gas_price_updates
+                .iter()
+                .map(|u| fee_quoter::context::GasPriceUpdate {
+                    dest_chain_selector: u.dest_chain_selector,
+                    usd_per_unit_gas: u.usd_per_unit_gas,
+                })
+                .collect();
+
+            fee_quoter::cpi::update_prices(cpi_ctx, token_price_updates, gas_price_update)?;
+        }
+
+        Ok(())
+    }
 }
