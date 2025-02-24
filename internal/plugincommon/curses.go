928eca4835100453769b34ed9f2f27bf5d6f9f24
Mateusz Sekara <mateusz.sekara@gmail.com>
2025-02-24 15:13:04 +0100

    Don't require source chain selectors while fetching the curse information

--- internal/plugincommon/curses.go
+++ internal/plugincommon/curses.go
@@ -7,7 +7,6 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 
 	"github.com/smartcontractkit/chainlink-ccip/pkg/reader"
-	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 )
 
@@ -20,8 +19,7 @@ func IsReportCursed(
 	ctx context.Context,
 	lggr logger.Logger,
 	ccipReader reader.CCIPReader,
-	destChain ccipocr3.ChainSelector,
-	reportSourceChains []ccipocr3.ChainSelector,
+	reportSourceChains []cciptypes.ChainSelector,
 ) (bool, error) {
 	// No error is returned in case there are other things for the report.
 	if len(reportSourceChains) == 0 {
