c13a422fc64576d02f0616c7019d74a267055e1a
PabloMansanet <pablo.mansanet@smartcontract.com>
2025-02-24 11:23:19 +0100

    Add custom allocator to example sender

--- chains/solana/contracts/programs/example-ccip-sender/src/lib.rs
+++ chains/solana/contracts/programs/example-ccip-sender/src/lib.rs
@@ -8,6 +8,15 @@ use solana_program::{
 
 declare_id!("CcipSender111111111111111111111111111111111");
 
+#[cfg(target_os = "solana")]
+#[global_allocator]
+static ALLOC: smalloc::Smalloc<
+    { solana_program::entrypoint::HEAP_START_ADDRESS as usize },
+    { solana_program::entrypoint::HEAP_LENGTH as usize },
+    16,
+    1024,
+> = smalloc::Smalloc::new();
+
 pub mod context;
 use context::*;
 
