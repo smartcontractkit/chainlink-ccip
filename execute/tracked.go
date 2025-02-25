smartcontractkit/chainlink-ccip

commit 49447ff49fc6a6c6596701da19145d33d3459451

CCIP-5262 Improvements to prometheus metrics  (#632)
* exposing source as chainID instead of selector
* better suited buckets for measuring latencies
* tracking latencies per exec state

execute/tracked.go
@@ -1,0 +1,105 @@
+package execute
+
+import (
+	"context"
+	"time"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
+	"github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+
+	"github.com/smartcontractkit/chainlink-ccip/execute/exectypes"
+	"github.com/smartcontractkit/chainlink-ccip/execute/metrics"
+	"github.com/smartcontractkit/chainlink-ccip/internal/plugincommon"
+	"github.com/smartcontractkit/chainlink-ccip/pkg/ocrtypecodec"
+)
+
+// TrackedPlugin tracks latency of the basic ReportingPlugin's methods. The special ingredient (compared to OCR3)
+// metrics is additional tracking of the state machine state. Please see metrics.Reporter for more details and the
+// exact prometheus metrics that are exposed.
+type TrackedPlugin struct {
+	ocr3types.ReportingPlugin[[]byte]
+	lggr     logger.Logger
+	reporter metrics.Reporter
+	codec    *ocrtypecodec.ExecCodecJSON
+}
+
+func NewTrackedPlugin(
+	plugin ocr3types.ReportingPlugin[[]byte],
+	lggr logger.Logger,
+	reporter metrics.Reporter,
+	codec *ocrtypecodec.ExecCodecJSON,
+) ocr3types.ReportingPlugin[[]byte] {
+	return &TrackedPlugin{
+		ReportingPlugin: plugin,
+		lggr:            lggr,
+		reporter:        reporter,
+		codec:           codec,
+	}
+}
+
+func (p *TrackedPlugin) Query(
+	ctx context.Context, outctx ocr3types.OutcomeContext,
+) (types.Query, error) {
+	return withTrackedMethod(
+		p,
+		plugincommon.QueryMethod,
+		outctx,
+		func() (types.Query, error) { return p.ReportingPlugin.Query(ctx, outctx) },
+	)
+}
+
+func (p *TrackedPlugin) Observation(
+	ctx context.Context, outctx ocr3types.OutcomeContext, query types.Query,
+) (types.Observation, error) {
+	return withTrackedMethod(
+		p,
+		plugincommon.ObservationMethod,
+		outctx,
+		func() (types.Observation, error) { return p.ReportingPlugin.Observation(ctx, outctx, query) },
+	)
+}
+
+func (p *TrackedPlugin) Outcome(
+	ctx context.Context, outctx ocr3types.OutcomeContext, query types.Query, aos []types.AttributedObservation,
+) (ocr3types.Outcome, error) {
+	return withTrackedMethod(
+		p,
+		plugincommon.OutcomeMethod,
+		outctx,
+		func() (ocr3types.Outcome, error) { return p.ReportingPlugin.Outcome(ctx, outctx, query, aos) },
+	)
+}
+
+func withTrackedMethod[T any](
+	p *TrackedPlugin,
+	method plugincommon.MethodType,
+	outctx ocr3types.OutcomeContext,
+	f func() (T, error),
+) (T, error) {
+	queryStarted := time.Now()
+	resp, err := f()
+
+	latency := time.Since(queryStarted)
+	state := currentState(p, outctx)
+
+	p.reporter.TrackLatency(state, method, latency, err)
+	p.lggr.Debugw("tracking exec latency",
+		"state",
+		"method", method,
+		"latency", latency,
+	)
+
+	return resp, err
+}
+
+func currentState(p *TrackedPlugin, outctx ocr3types.OutcomeContext) exectypes.PluginState {
+	// It's already decoded by the plugin, but we can't easily access it on the interface level.
+	// Assuming for now that CPU consumption for this extra decoding is negligible.
+	out, err := p.codec.DecodeOutcome(outctx.PreviousOutcome)
+	if err != nil {
+		p.lggr.Errorw("unable to get state", "error", err)
+		return exectypes.Unknown
+	}
+	return out.State.Next()
+}

commit/metrics/prom.go
@@ -17,7 +17,7 @@ import (
 )
 
 var (
-	RequestLatencyBucketsMilliseconds = []float64{
+	rmnLatencyBucketsMilliseconds = []float64{
 		5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000,
 	}
 	promProcessorOutputCounter = promauto.NewCounterVec(
@@ -29,9 +29,21 @@ var (
 	)
 	promProcessorLatencyHistogram = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "ccip_commit_processor_latency_ms",
-			Help:    "This metric tracks the client-observed latency of a single processor method",
-			Buckets: RequestLatencyBucketsMilliseconds,
+			Name: "ccip_commit_processor_latency",
+			Help: "This metric tracks the client-observed latency of a single processor method",
+			Buckets: []float64{
+				float64(50 * time.Millisecond),
+				float64(100 * time.Millisecond),
+				float64(200 * time.Millisecond),
+				float64(500 * time.Millisecond),
+				float64(700 * time.Millisecond),
+				float64(time.Second),
+				float64(2 * time.Second),
+				float64(5 * time.Second),
+				float64(7 * time.Second),
+				float64(10 * time.Second),
+				float64(20 * time.Second),
+			},
 		},
 		[]string{"chainID", "processor", "method"},
 	)
@@ -47,13 +59,13 @@ var (
 			Name: "ccip_commit_max_sequence_number",
 			Help: "This metric tracks the max sequence number observed by the commit processor",
 		},
-		[]string{"chainID", "sourceChainSelector", "method"},
+		[]string{"chainID", "sourceChain", "method"},
 	)
 	promMerkleProcessorRmnReportLatency = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "ccip_commit_merkle_processor_rmn_report_latency_ms",
 			Help:    "This metric tracks the client-observed latency of building an full RMN report with signatures",
-			Buckets: RequestLatencyBucketsMilliseconds,
+			Buckets: rmnLatencyBucketsMilliseconds,
 		},
 		[]string{"chainID", "success"},
 	)
@@ -61,7 +73,7 @@ var (
 		prometheus.HistogramOpts{
 			Name:    "ccip_commit_rmn_controller_rmn_request_latency_ms",
 			Help:    "This metric tracks the client-observed latency of a single RMN request",
-			Buckets: RequestLatencyBucketsMilliseconds,
+			Buckets: rmnLatencyBucketsMilliseconds,
 		},
 		[]string{"method", "nodeID", "error"},
 	)
@@ -127,13 +139,21 @@ func (p *PromReporter) trackMaxSequenceNumber(
 		return
 	}
 
+	sourceChain, err := sel.GetChainIDFromSelector(uint64(sourceChainSelector))
+	if err != nil {
+		p.lggr.Errorw("failed to get chain ID from selector", "err", err)
+		return
+	}
+
 	p.sequenceNumbers.
-		WithLabelValues(p.chainID, sourceChainSelector.String(), method).
+		WithLabelValues(p.chainID, sourceChain, method).
 		Set(float64(maxSeqNr))
 
-	p.lggr.Debugw("latest max seq num",
+	p.lggr.Debugw(
+		"exec latest max seq num",
 		"method", method,
-		"sourceChainSelector", sourceChainSelector,
+		"sourceChain", sourceChain,
+		"destChain", p.chainID,
 		"maxSeqNr", maxSeqNr,
 	)
 }
@@ -163,7 +183,7 @@ func (p *PromReporter) TrackProcessorLatency(
 
 	p.processorLatencyHistogram.
 		WithLabelValues(p.chainID, processor, method).
-		Observe(float64(latency.Milliseconds()))
+		Observe(float64(latency))
 }
 
 func (p *PromReporter) TrackProcessorOutput(

commit/metrics/prom_test.go
@@ -416,18 +416,24 @@ func Test_LatencyAndErrors(t *testing.T) {
 }
 
 func Test_SequenceNumbers(t *testing.T) {
+	chain1 := "2337"
+	selector1 := cciptypes.ChainSelector(12922642891491394802)
+	chain2 := "3337"
+	selector2 := cciptypes.ChainSelector(4793464827907405086)
+	selector3 := cciptypes.ChainSelector(909606746561742123)
+
 	tt := []struct {
 		name   string
 		obs    committypes.Observation
 		out    committypes.Outcome
 		method plugincommon.MethodType
-		exp    map[cciptypes.ChainSelector]cciptypes.SeqNum
+		exp    map[string]cciptypes.SeqNum
 	}{
 		{
 			name:   "empty observation should not report anything",
 			obs:    committypes.Observation{},
 			method: plugincommon.ObservationMethod,
-			exp:    map[cciptypes.ChainSelector]cciptypes.SeqNum{},
+			exp:    map[string]cciptypes.SeqNum{},
 		},
 		{
 			name: "single chain observation with seq nr",
@@ -435,15 +441,15 @@ func Test_SequenceNumbers(t *testing.T) {
 				MerkleRootObs: merkleroot.Observation{
 					MerkleRoots: []cciptypes.MerkleRootChain{
 						{
-							ChainSel:     cciptypes.ChainSelector(123),
+							ChainSel:     selector1,
 							SeqNumsRange: cciptypes.NewSeqNumRange(1, 2),
 						},
 					},
 				},
 			},
 			method: plugincommon.ObservationMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 2,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 2,
 			},
 		},
 		{
@@ -452,24 +458,24 @@ func Test_SequenceNumbers(t *testing.T) {
 				MerkleRootObs: merkleroot.Observation{
 					MerkleRoots: []cciptypes.MerkleRootChain{
 						{
-							ChainSel:     cciptypes.ChainSelector(123),
+							ChainSel:     selector1,
 							SeqNumsRange: cciptypes.NewSeqNumRange(1, 2),
 						},
 						{
-							ChainSel:     cciptypes.ChainSelector(456),
+							ChainSel:     selector2,
 							SeqNumsRange: cciptypes.NewSeqNumRange(3, 4),
 						},
 						{
-							ChainSel:     cciptypes.ChainSelector(789),
+							ChainSel:     selector3,
 							SeqNumsRange: cciptypes.NewSeqNumRange(0, 0),
 						},
 					},
 				},
 			},
 			method: plugincommon.ObservationMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 2,
-				456: 4,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 2,
+				chain2: 4,
 			},
 		},
 		{
@@ -478,15 +484,15 @@ func Test_SequenceNumbers(t *testing.T) {
 				MerkleRootOutcome: merkleroot.Outcome{
 					RootsToReport: []cciptypes.MerkleRootChain{
 						{
-							ChainSel:     cciptypes.ChainSelector(123),
+							ChainSel:     selector1,
 							SeqNumsRange: cciptypes.NewSeqNumRange(1, 2),
 						},
 					},
 				},
 			},
 			method: plugincommon.OutcomeMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 2,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 2,
 			},
 		},
 		{
@@ -495,20 +501,20 @@ func Test_SequenceNumbers(t *testing.T) {
 				MerkleRootOutcome: merkleroot.Outcome{
 					RootsToReport: []cciptypes.MerkleRootChain{
 						{
-							ChainSel:     cciptypes.ChainSelector(123),
+							ChainSel:     selector1,
 							SeqNumsRange: cciptypes.NewSeqNumRange(1, 2),
 						},
 						{
-							ChainSel:     cciptypes.ChainSelector(456),
+							ChainSel:     selector2,
 							SeqNumsRange: cciptypes.NewSeqNumRange(3, 4),
 						},
 					},
 				},
 			},
 			method: plugincommon.OutcomeMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 2,
-				456: 4,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 2,
+				chain2: 4,
 			},
 		},
 	}
@@ -527,9 +533,9 @@ func Test_SequenceNumbers(t *testing.T) {
 				reporter.TrackOutcome(tc.out)
 			}
 
-			for sourceChainSelector, maxSeqNr := range tc.exp {
+			for sourceChain, maxSeqNr := range tc.exp {
 				seqNum := testutil.ToFloat64(
-					reporter.sequenceNumbers.WithLabelValues(chainID, sourceChainSelector.String(), tc.method),
+					reporter.sequenceNumbers.WithLabelValues(chainID, sourceChain, tc.method),
 				)
 				require.Equal(t, float64(maxSeqNr), seqNum)
 			}

execute/metrics/prom.go
@@ -1,6 +1,8 @@
 package metrics
 
 import (
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"golang.org/x/exp/maps"
@@ -15,19 +17,46 @@ import (
 )
 
 var (
-	promProcessorOutputCounter = promauto.NewCounterVec(
+	PromExecOutputCounter = promauto.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "ccip_exec_output_sizes",
-			Help: "This metric tracks the number of different items in the commit processor",
+			Help: "This metric tracks the number of different items in the exec plugin",
 		},
 		[]string{"chainID", "method", "state", "type"},
 	)
-	promSequenceNumbers = promauto.NewGaugeVec(
+	PromExecLatencyHistogram = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "ccip_exec_latency",
+			Help: "This metric tracks the client-observed latency of a single exec plugin method",
+			Buckets: []float64{
+				float64(50 * time.Millisecond),
+				float64(100 * time.Millisecond),
+				float64(200 * time.Millisecond),
+				float64(500 * time.Millisecond),
+				float64(700 * time.Millisecond),
+				float64(time.Second),
+				float64(2 * time.Second),
+				float64(5 * time.Second),
+				float64(7 * time.Second),
+				float64(10 * time.Second),
+				float64(20 * time.Second),
+			},
+		},
+		[]string{"chainID", "method", "state"},
+	)
+	PromExecErrors = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "ccip_exec_errors",
+			Help: "This metric tracks the number of errors in the exec plugin",
+		},
+		[]string{"chainID", "method", "state"},
+	)
+	PromSequenceNumbers = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "ccip_exec_max_sequence_number",
 			Help: "This metric tracks the max sequence number observed by the commit processor",
 		},
-		[]string{"chainID", "sourceChainSelector", "method"},
+		[]string{"chainID", "sourceChain", "method"},
 	)
 )
 
@@ -36,6 +65,8 @@ type PromReporter struct {
 	chainID string
 
 	// Prometheus reporters
+	latencyHistogram     *prometheus.HistogramVec
+	execErrors           *prometheus.CounterVec
 	outputDetailsCounter *prometheus.CounterVec
 	sequenceNumbers      *prometheus.GaugeVec
 }
@@ -50,8 +81,10 @@ func NewPromReporter(lggr logger.Logger, selector cciptypes.ChainSelector) (*Pro
 		lggr:    lggr,
 		chainID: chainID,
 
-		outputDetailsCounter: promProcessorOutputCounter,
-		sequenceNumbers:      promSequenceNumbers,
+		latencyHistogram:     PromExecLatencyHistogram,
+		execErrors:           PromExecErrors,
+		outputDetailsCounter: PromExecOutputCounter,
+		sequenceNumbers:      PromSequenceNumbers,
 	}, nil
 }
 
@@ -74,6 +107,24 @@ func (p *PromReporter) TrackOutcome(outcome exectypes.Outcome, state exectypes.P
 	}
 }
 
+func (p *PromReporter) TrackLatency(
+	state exectypes.PluginState,
+	method plugincommon.MethodType,
+	latency time.Duration,
+	err error,
+) {
+	if err != nil {
+		p.execErrors.
+			WithLabelValues(p.chainID, method, string(state)).
+			Inc()
+		return
+	}
+
+	p.latencyHistogram.
+		WithLabelValues(p.chainID, method, string(state)).
+		Observe(float64(latency))
+}
+
 func (p *PromReporter) trackMaxSequenceNumber(
 	sourceChainSelector cciptypes.ChainSelector,
 	maxSeqNr int,
@@ -83,9 +134,23 @@ func (p *PromReporter) trackMaxSequenceNumber(
 		return
 	}
 
+	sourceChain, err := sel.GetChainIDFromSelector(uint64(sourceChainSelector))
+	if err != nil {
+		p.lggr.Errorw("failed to get chain ID from selector", "err", err)
+		return
+	}
+
 	p.sequenceNumbers.
-		WithLabelValues(p.chainID, sourceChainSelector.String(), method).
+		WithLabelValues(p.chainID, sourceChain, method).
 		Set(float64(maxSeqNr))
+
+	p.lggr.Debugw(
+		"commit latest max seq num",
+		"method", method,
+		"sourceChain", sourceChain,
+		"destChain", p.chainID,
+		"maxSeqNr", maxSeqNr,
+	)
 }
 
 func (p *PromReporter) trackOutputStats(

execute/metrics/prom_test.go
@@ -3,12 +3,14 @@ package metrics
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-ccip/execute/exectypes"
+	"github.com/smartcontractkit/chainlink-ccip/internal"
 	"github.com/smartcontractkit/chainlink-ccip/internal/plugincommon"
 	cciptypes "github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 )
@@ -283,51 +285,56 @@ func Test_TrackingOutcomes(t *testing.T) {
 }
 
 func Test_SequenceNumbers(t *testing.T) {
+	chain1 := "2337"
+	selector1 := cciptypes.ChainSelector(12922642891491394802)
+	chain2 := "3337"
+	selector2 := cciptypes.ChainSelector(4793464827907405086)
+
 	tt := []struct {
 		name   string
 		obs    exectypes.Observation
 		out    exectypes.Outcome
 		method plugincommon.MethodType
-		exp    map[cciptypes.ChainSelector]cciptypes.SeqNum
+		exp    map[string]cciptypes.SeqNum
 	}{
 		{
 			name:   "empty observation should not report anything",
 			obs:    exectypes.Observation{},
 			method: plugincommon.ObservationMethod,
-			exp:    map[cciptypes.ChainSelector]cciptypes.SeqNum{},
+			exp:    map[string]cciptypes.SeqNum{},
 		},
 		{
 			name: "single chain observation with seq nr",
 			obs: exectypes.Observation{
 				Messages: exectypes.MessageObservations{
-					123: {
+					selector1: {
 						1: {},
 						4: {},
 					},
 				},
 			},
 			method: plugincommon.ObservationMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 4,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 4,
 			},
 		},
 		{
 			name: "multiple chain observations with sequence numbers",
 			obs: exectypes.Observation{
 				Messages: exectypes.MessageObservations{
-					123: {
+					selector1: {
 						1: {},
 						2: {},
 					},
-					456: {
+					selector2: {
 						4: {},
 					},
 				},
 			},
 			method: plugincommon.ObservationMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 2,
-				456: 4,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 2,
+				chain2: 4,
 			},
 		},
 		{
@@ -335,7 +342,7 @@ func Test_SequenceNumbers(t *testing.T) {
 			out: exectypes.Outcome{
 				CommitReports: []exectypes.CommitData{
 					{
-						SourceChain: 123,
+						SourceChain: selector1,
 						Messages: []cciptypes.Message{
 							{
 								Header: cciptypes.RampMessageHeader{
@@ -347,8 +354,8 @@ func Test_SequenceNumbers(t *testing.T) {
 				},
 			},
 			method: plugincommon.OutcomeMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 2,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 2,
 			},
 		},
 		{
@@ -356,7 +363,7 @@ func Test_SequenceNumbers(t *testing.T) {
 			out: exectypes.Outcome{
 				CommitReports: []exectypes.CommitData{
 					{
-						SourceChain: 123,
+						SourceChain: selector1,
 						Messages: []cciptypes.Message{
 							{
 								Header: cciptypes.RampMessageHeader{
@@ -366,7 +373,7 @@ func Test_SequenceNumbers(t *testing.T) {
 						},
 					},
 					{
-						SourceChain: 456,
+						SourceChain: selector2,
 						Messages: []cciptypes.Message{
 							{
 								Header: cciptypes.RampMessageHeader{
@@ -383,9 +390,9 @@ func Test_SequenceNumbers(t *testing.T) {
 				},
 			},
 			method: plugincommon.OutcomeMethod,
-			exp: map[cciptypes.ChainSelector]cciptypes.SeqNum{
-				123: 2,
-				456: 4,
+			exp: map[string]cciptypes.SeqNum{
+				chain1: 2,
+				chain2: 4,
 			},
 		},
 	}
@@ -404,9 +411,9 @@ func Test_SequenceNumbers(t *testing.T) {
 				reporter.TrackOutcome(tc.out, exectypes.GetCommitReports)
 			}
 
-			for sourceChainSelector, maxSeqNr := range tc.exp {
+			for sourceChain, maxSeqNr := range tc.exp {
 				seqNum := testutil.ToFloat64(
-					reporter.sequenceNumbers.WithLabelValues(chainID, sourceChainSelector.String(), tc.method),
+					reporter.sequenceNumbers.WithLabelValues(chainID, sourceChain, tc.method),
 				)
 				require.Equal(t, float64(maxSeqNr), seqNum)
 			}
@@ -414,9 +421,47 @@ func Test_SequenceNumbers(t *testing.T) {
 	}
 }
 
+func Test_ExecLatency(t *testing.T) {
+	reporter, err := NewPromReporter(logger.Test(t), selector)
+	require.NoError(t, err)
+
+	t.Run("single latency observation", func(t *testing.T) {
+		reporter.TrackLatency(exectypes.GetCommitReports, plugincommon.ObservationMethod, 100, nil)
+		l1 := internal.CounterFromHistogramByLabels(t, reporter.latencyHistogram, chainID, "observation", "GetCommitReports")
+		require.Equal(t, 1, l1)
+
+		errs := testutil.ToFloat64(
+			reporter.execErrors.WithLabelValues(chainID, "observation", "GetCommitReports"),
+		)
+		require.Equal(t, float64(0), errs)
+	})
+
+	t.Run("multiple latency outcomes", func(t *testing.T) {
+		passCounter := 10
+		for i := 0; i < passCounter; i++ {
+			reporter.TrackLatency(exectypes.Filter, plugincommon.OutcomeMethod, time.Second, nil)
+		}
+		l2 := internal.CounterFromHistogramByLabels(t, reporter.latencyHistogram, chainID, "outcome", "Filter")
+		require.Equal(t, passCounter, l2)
+	})
+
+	t.Run("multiple latency observation with errors", func(t *testing.T) {
+		errCounter := 5
+		for i := 0; i < errCounter; i++ {
+			reporter.TrackLatency(exectypes.GetMessages, plugincommon.ObservationMethod, time.Second, fmt.Errorf("error"))
+		}
+		errs := testutil.ToFloat64(
+			reporter.execErrors.WithLabelValues(chainID, "observation", "GetMessages"),
+		)
+		require.Equal(t, float64(errCounter), errs)
+	})
+}
+
 func cleanupMetrics(p *PromReporter) func() {
 	return func() {
 		p.sequenceNumbers.Reset()
 		p.outputDetailsCounter.Reset()
+		p.latencyHistogram.Reset()
+		p.execErrors.Reset()
 	}
 }

execute/metrics/reporter.go
@@ -1,6 +1,11 @@
 package metrics
 
-import "github.com/smartcontractkit/chainlink-ccip/execute/exectypes"
+import (
+	"time"
+
+	"github.com/smartcontractkit/chainlink-ccip/execute/exectypes"
+	"github.com/smartcontractkit/chainlink-ccip/internal/plugincommon"
+)
 
 // Reporter is a simple interface used for tracking observations and outcomes of the execution plugin.
 // Default implementation is based on the Prometheus metrics, but it can be extended to support other metrics systems.
@@ -10,6 +15,7 @@ import "github.com/smartcontractkit/chainlink-ccip/execute/exectypes"
 type Reporter interface {
 	TrackObservation(obs exectypes.Observation, state exectypes.PluginState)
 	TrackOutcome(outcome exectypes.Outcome, state exectypes.PluginState)
+	TrackLatency(state exectypes.PluginState, method plugincommon.MethodType, latency time.Duration, err error)
 }
 
 type Noop struct{}
@@ -17,6 +23,8 @@ type Noop struct{}
 func (n *Noop) TrackObservation(exectypes.Observation, exectypes.PluginState) {}
 
 func (n *Noop) TrackOutcome(exectypes.Outcome, exectypes.PluginState) {}
+
+func (n *Noop) TrackLatency(exectypes.PluginState, plugincommon.MethodType, time.Duration, error) {}
 
 var _ Reporter = &Noop{}
 var _ Reporter = &PromReporter{}

execute/plugin.go
@@ -101,10 +101,11 @@ func NewPlugin(
 	lggr logger.Logger,
 	costlyMessageObserver costlymessages.Observer,
 	metricsReporter metrics.Reporter,
-) *Plugin {
+) ocr3types.ReportingPlugin[[]byte] {
 	lggr.Infow("creating new plugin instance", "p2pID", oracleIDToP2pID[reportingCfg.OracleID])
 
-	return &Plugin{
+	codec := ocrtypecodec.NewExecCodecJSON()
+	p := &Plugin{
 		donID:                 donID,
 		reportingCfg:          reportingCfg,
 		offchainCfg:           offchainCfg,
@@ -139,8 +140,9 @@ func NewPlugin(
 			offchainCfg.MessageVisibilityInterval.Duration(),
 			time.Minute*5),
 		inflightMessageCache: cache.NewInflightMessageCache(offchainCfg.InflightCacheExpiry.Duration()),
-		ocrTypeCodec:         ocrtypecodec.NewExecCodecJSON(),
+		ocrTypeCodec:         codec,
 	}
+	return NewTrackedPlugin(p, lggr, metricsReporter, codec)
 }
 
 func (p *Plugin) Query(ctx context.Context, outctx ocr3types.OutcomeContext) (types.Query, error) {

execute/test_utils.go
@@ -345,8 +345,13 @@ func (it *IntTest) newNode(
 		&metrics.Noop{},
 	)
 
+	// FIXME: Test should not rely on the specific type of the plugin but rather than that on
+	// the interface type
+	p := node1.(*TrackedPlugin)
+	pp := p.ReportingPlugin.(*Plugin)
+
 	return nodeSetup{
-		node:        node1,
+		node:        pp,
 		reportCodec: reportCodec,
 		msgHasher:   it.msgHasher,
 	}

commit 549961cd1584697e48c4a977ec8e9d5e037ecfb9

execute/tracked: fix logging
Regression in #632, produces 

{"L":"ERROR","T":"16:28:28.546783396","N":"CCIPExecPlugin.evm.90000002.0x0eEEe012627fF6860f504830d47e72C2955fB203","C":"zap@v1.27.0/sugar.go:251","M":"Ignored key without a value.","plugin":"Execute","oracleID":2,"donID":2,"configDigest":"000af19ca106634e2ba54f14dc1c80c8ec17d608272e7ceefb815031238035ce","ignored":"29.27407ms"}

execute/tracked.go
@@ -85,7 +85,7 @@ func withTrackedMethod[T any](
 
 	p.reporter.TrackLatency(state, method, latency, err)
 	p.lggr.Debugw("tracking exec latency",
-		"state",
+		"state", state,
 		"method", method,
 		"latency", latency,
 	)