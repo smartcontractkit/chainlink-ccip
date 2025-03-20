77225f7bd3c6f96c386ba5c48ec2bfce3e0e3ac8
Matt Yang <matt.yang@smartcontract.com>
2025-03-19 22:27:29 -0400

    exp lint

--- chains/solana/contracts/tests/ccip/ccip_router_test.go
+++ chains/solana/contracts/tests/ccip/ccip_router_test.go
@@ -8233,8 +8233,8 @@ func TestCCIPRouter(t *testing.T) {
 
 						tx = testutils.SendAndConfirm(ctx, t, solanaGoClient, []solana.Instruction{instruction}, user, config.DefaultCommitment)
 
-						executionEvents, err := common.ParseMultipleEvents[ccip.EventExecutionStateChanged](tx.Meta.LogMessages, "ExecutionStateChanged", config.PrintEvents)
-						require.NoError(t, err)
+						executionEvents, err2 := common.ParseMultipleEvents[ccip.EventExecutionStateChanged](tx.Meta.LogMessages, "ExecutionStateChanged", config.PrintEvents)
+						require.NoError(t, err2)
 
 						require.Equal(t, 2, len(executionEvents))
 						require.Equal(t, config.EvmChainSelector, executionEvents[0].SourceChainSelector)
