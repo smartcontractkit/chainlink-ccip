a105e806e2cb494cd19ade1c7369cc0e359d3830
Will Winder <wwinder.unh@gmail.com>
2025-03-21 10:35:07 -0400

    Add fancy renderer option.

--- cmd/carpenter/main.go
+++ cmd/carpenter/main.go
@@ -9,6 +9,7 @@ import (
 
 	// Register the renderers
 	_ "github.com/smartcontractkit/chainlink-ccip/cmd/carpenter/internal/render/basic"
+	_ "github.com/smartcontractkit/chainlink-ccip/cmd/carpenter/internal/render/fancy"
 )
 
 func main() {
