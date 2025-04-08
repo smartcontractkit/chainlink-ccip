45d08fa4d9c1f1bfd2703c81cbc62f0a1ef0060a
Jade Park <jadepark.dev@gmail.com>
2025-04-08 14:44:40 +0100

    chore: revert A8 changes

--- chains/solana/contracts/target/types/ccip_offramp.ts
+++ chains/solana/contracts/target/types/ccip_offramp.ts
@@ -2379,16 +2379,11 @@ export type CcipOfframp = {
     },
     {
       "code": 9053,
-      "name": "Ocr3SignaturesProvidedWhenVerificationDisabled",
-      "msg": "Signatures provided when verification is disabled"
-    },
-    {
-      "code": 9054,
       "name": "InvalidOnrampAddress",
       "msg": "Invalid onramp address"
     },
     {
-      "code": 9055,
+      "code": 9054,
       "name": "CommitReportHasPendingMessages",
       "msg": "Commit report has pending messages"
     }
@@ -4776,16 +4771,11 @@ export const IDL: CcipOfframp = {
     },
     {
       "code": 9053,
-      "name": "Ocr3SignaturesProvidedWhenVerificationDisabled",
-      "msg": "Signatures provided when verification is disabled"
-    },
-    {
-      "code": 9054,
       "name": "InvalidOnrampAddress",
       "msg": "Invalid onramp address"
     },
     {
-      "code": 9055,
+      "code": 9054,
       "name": "CommitReportHasPendingMessages",
       "msg": "Commit report has pending messages"
     }
