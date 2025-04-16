8c3c666147feed1d6b64f68f5970ee8b8a5b4425
toblich <toblich@users.noreply.github.com>
2025-04-15 22:13:37 -0300

    Add rerun configs on build

--- chains/solana/contracts/programs/ccip-router/build.rs
+++ chains/solana/contracts/programs/ccip-router/build.rs
@@ -2,12 +2,20 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("git")
+    let hash_output = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
         .expect("Failed to execute git");
 
-    let git_hash = String::from_utf8(output.stdout).expect("Invalid UTF-8");
+    let git_hash = String::from_utf8(hash_output.stdout).expect("Invalid UTF-8");
+
+    let dir_output = Command::new("git")
+        .args(&["rev-parse", "--git-dir"])
+        .output()
+        .expect("Failed to execute git");
+    let git_dir = String::from_utf8(dir_output.stdout).expect("Invalid UTF-8");
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash.trim());
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed={}/HEAD", git_dir);
 }
