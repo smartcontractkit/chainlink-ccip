21d5d710728424d009b5cd3a8b183ec1703af597
toblich <toblich@users.noreply.github.com>
2025-04-25 14:13:05 -0300

    Add support for git hash via env var, for CI purposes and dockerized builds

--- chains/solana/contracts/crates/build-commit/src/lib.rs
+++ chains/solana/contracts/crates/build-commit/src/lib.rs
@@ -1,14 +1,50 @@
+use std::env::var;
 use std::path::Path;
 use std::process::Command;
 
+const GIT_HASH_ENV_VAR: &str = "CCIP_BUILD_GIT_HASH";
+
 pub fn cargo_instructions(source_file: &str) {
-    let program_name = Path::new(source_file)
+    let program_name = get_program_name(source_file);
+
+    let git_hash = if let Ok(git_hash) = var(GIT_HASH_ENV_VAR) {
+        assert!(
+            git_hash.len() == 40,
+            "GIT_HASH_ENV_VAR must be a 40 character hash"
+        );
+        git_hash
+    } else {
+        let (git_hash, git_dir, git_ref) = git_strategy();
+        println!("cargo:rerun-if-changed={}/HEAD", git_dir);
+        if let Some(reference) = git_ref {
+            println!("cargo:rerun-if-changed={}/{}", git_dir, reference);
+        }
+
+        git_hash
+    };
+
+    println!("cargo:rerun-if-env-changed={}", GIT_HASH_ENV_VAR);
+    println!("cargo:rustc-env=CCIP_BUILD_PROGRAM_NAME={}", program_name);
+    println!("cargo:rustc-env=CCIP_BUILD_GIT_HASH={}", git_hash);
+    println!(
+        "cargo:rustc-env=CCIP_BUILD_TYPE_VERSION={} {}",
+        program_name, git_hash,
+    );
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed={}", source_file);
+    println!("cargo:rerun-if-changed={}", file!());
+}
+
+fn get_program_name(source_file: &str) -> &str {
+    Path::new(source_file)
         .parent()
         .and_then(|p| p.file_name())
         .and_then(|name| name.to_str())
         .unwrap()
-        .trim();
+        .trim()
+}
 
+fn git_strategy() -> (String, String, Option<String>) {
     let hash_output = Command::new("git")
         .args(["rev-parse", "HEAD"])
         .output()
@@ -37,21 +73,13 @@ pub fn cargo_instructions(source_file: &str) {
         .trim()
         .to_string();
 
-    if head.starts_with("ref: ") {
-        let reference = head.split_whitespace().nth(1).unwrap().trim().to_string();
-        println!("cargo:rerun-if-changed={}/{}", git_dir, reference);
-    }
+    let reference = if head.starts_with("ref: ") {
+        Some(head.split_whitespace().nth(1).unwrap().trim().to_string())
+    } else {
+        None
+    };
 
-    println!("cargo:rustc-env=CCIP_BUILD_PROGRAM_NAME={}", program_name);
-    println!("cargo:rustc-env=CCIP_BUILD_GIT_HASH={}", git_hash);
-    println!(
-        "cargo:rustc-env=CCIP_BUILD_TYPE_VERSION={} {}",
-        program_name, git_hash,
-    );
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed={}", source_file);
-    println!("cargo:rerun-if-changed={}", file!());
-    println!("cargo:rerun-if-changed={}/HEAD", git_dir);
+    (git_hash, git_dir, reference)
 }
 
 #[cfg(test)]
@@ -60,6 +88,15 @@ mod tests {
 
     #[test]
     fn test_cargo_instructions() {
+        // This test is meant to be manually run and inspect the output.
+        // Not asserting anything programmatically, as the method prints directly to stdout and the output
+        // varies depending on the current git branch/commit
+        cargo_instructions("<some_prefix>/chainlink-ccip/chains/solana/contracts/programs/burnmint-token-pool/build.rs");
+    }
+
+    #[test]
+    fn test_cargo_instructions_with_env_var() {
+        std::env::set_var(GIT_HASH_ENV_VAR, "1234567890123456789012345678901234567890");
         // This test is meant to be manually run and inspect the output.
         // Not asserting anything programmatically, as the method prints directly to stdout and the output
         // varies depending on the current git branch/commit
