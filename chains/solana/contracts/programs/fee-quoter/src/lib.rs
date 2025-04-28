eb31d682c205ccf466a8c816431a4bac950cc8ec
toblich <toblich@users.noreply.github.com>
2025-04-28 14:59:27 -0300

    Revert "Revert "Add type_version method to contracts [NONEVM-1646] (#870)" (#880)"
    This reverts commit bdc1558a2a3ce5e03a24efdb556d148538955b53.

--- chains/solana/contracts/programs/fee-quoter/src/lib.rs
+++ chains/solana/contracts/programs/fee-quoter/src/lib.rs
@@ -74,6 +74,17 @@ pub mod fee_quoter {
         Ok(())
     }
 
+    /// Returns the program type (name) and version.
+    /// Used by offchain code to easily determine which program & version is being interacted with.
+    ///
+    /// # Arguments
+    /// * `ctx` - The context
+    pub fn type_version(_ctx: Context<Empty>) -> Result<String> {
+        let response = env!("CCIP_BUILD_TYPE_VERSION").to_string();
+        msg!("{}", response);
+        Ok(response)
+    }
+
     /// Transfers the ownership of the fee quoter to a new proposed owner.
     ///
     /// Shared func signature with other programs
