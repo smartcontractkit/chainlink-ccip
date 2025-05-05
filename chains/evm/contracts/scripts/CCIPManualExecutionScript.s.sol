03d0bf244f52f88bedaabf1e47bda6930937fdba
Josh <josh.weintraub@smartcontract.com>
2025-05-05 14:10:28 -0500

    move user provided values to storage not inside function call

--- chains/evm/contracts/scripts/CCIPManualExecutionScript.s.sol
+++ chains/evm/contracts/scripts/CCIPManualExecutionScript.s.sol
@@ -22,39 +22,40 @@ contract CCIPManualExecutionScript is Script {
   error ManualExecutionFailed();
   error ManualExecutionNotAllowed();
 
-  function run() public {
-    // 1. Define which chain you would like to use (Ex: "ETHEREUM" as defined in .env)
-    // [INSERT BELOW]
-    string memory chainIdentifier;
+  string public chainIdentifier; // The Chain to use (Ex: "ETHEREUM" as defined in the .env)
+  uint64 sourceChainSelector; // The CCIP chain selector the message originated from
 
-    // 2. Retrieve the RPC-URL based on the identifier and defined in .env
-    // Ex: "ETHEREUM" -> "ETHEREUM_RPC_URL"
+  // Define the sequence number of the message to be manually executed. The sequence number can be found
+  // on the CCIP-Explorer page for the given message.
+  // Note: The OffRamp will use the sequencer number and NOT the messageId to acquire message status, so if this
+  // value is set incorrectly, unexpected behavior may result by this script.
+  uint64 public sequenceNumber;
+
+  // Define the manual execution data to be used. Given that manual execution data can be hard to derive manually,
+  // due to the existence of offChain data and various proof flags, the data for manual execution should be acquired
+  // from the CCIP-Explorer webpage or the ccip-tools-ts repository on Github (https://github.com/smartcontractkit/ccip-tools-ts)
+  // Manual Execution Tutorial: https://docs.chain.link/ccip/tutorials/manual-execution#trigger-manual-execution
+  // Note: The manual execution data should be formatted as calldata to be sent to the OffRamp and invoke the
+  // function "manuallyExecute"
+  bytes public manualExecutionData;
+
+  function run() public {
     vm.createSelectFork(string.concat(chainIdentifier, "_RPC_URL"));
 
-    // 3. Acquire the private key from the .env file and derive address
+    // Acquire the private key from the .env file and derive address
     uint256 privateKey = vm.envUint("PRIVATE_KEY");
     address sender = vm.rememberKey(privateKey);
 
     console.log("Sender: %s", sender);
     console.log("Starting Script...");
 
-    // 4. Define the selector of the source chain the message originated on.
-    // [INSERT BELOW]
-    uint64 sourceChainSelector;
-
-    // 5. Define the sequence number of the message to be manually executed. The sequence number can be found
-    // on the CCIP-Explorer page for the given message.
-    // Note: The OffRamp will use the sequencer number and NOT the messageId to acquire message status, so if this
-    // value is set incorrectly, unexpected behavior may result by this script.
-    uint64 sequenceNumber;
-
-    // 6. Acquire the address of the OffRamp to send the manual execution call to
-    // [INSERT BELOW]
     address router = vm.envAddress(string.concat(chainIdentifier, "_ROUTER"));
     address offRamp;
     Router.OffRamp[] memory offRamps = Router(router).getOffRamps();
+
     // Perform a linear search for the offRamp contract using the known source chain selector.
-    // Note: Given that this operation is performed off-chain, a linear search is an acceptable time-complexity.
+    // Note: Given that this operation is being performed off-chain, and thus gas is not a consideration,
+    // a linear search is an acceptable time-complexity.
     for (uint256 i = 0; i < offRamps.length; ++i) {
       if (offRamps[i].sourceChainSelector == sourceChainSelector) {
         offRamp = offRamps[i].offRamp;
@@ -62,7 +63,6 @@ contract CCIPManualExecutionScript is Script {
       }
     }
 
-    // 7. Check that the message status is appropriate for manual execution, revert otherwise.
     Internal.MessageExecutionState executionState =
       OffRamp(offRamp).getExecutionState(sourceChainSelector, sequenceNumber);
     if (
@@ -70,20 +70,12 @@ contract CCIPManualExecutionScript is Script {
         && executionState != Internal.MessageExecutionState.UNTOUCHED
     ) revert ManualExecutionNotAllowed();
 
-    // 8. Define the manual execution data to be used. Given that manual execution data can be hard to derive manually,
-    // due to the existence of offChain data and various proof flags, the data for manual execution should be acquired
-    // from the CCIP-Explorer webpage or the ccip-tools-ts repository on Github (https://github.com/smartcontractkit/ccip-tools-ts)
-    // Manual Execution Tutorial: https://docs.chain.link/ccip/tutorials/manual-execution#trigger-manual-execution
-    // Note: The manual execution data should be formatted as calldata to be sent to the OffRamp and invoke the
-    // function "manuallyExecute"
-    bytes memory manualExecutionData;
-
-    // 9. Attempt the call to the offRamp to manually execute the message.
+    // Attempt the call to the offRamp to manually execute the message.
     vm.startBroadcast(privateKey);
     (bool success,) = address(offRamp).call(manualExecutionData);
     vm.stopBroadcast();
 
-    // 10. Revert if the execution was not successful.
+    // Revert if the execution was not successful.
     if (!success) revert ManualExecutionFailed();
     executionState = OffRamp(offRamp).getExecutionState(sourceChainSelector, sequenceNumber);
     if (executionState != Internal.MessageExecutionState.SUCCESS) revert ManualExecutionFailed();
--- chains/evm/contracts/scripts/CCIPSendTestScript.s.sol
+++ chains/evm/contracts/scripts/CCIPSendTestScript.s.sol
@@ -21,16 +21,23 @@ contract CCIPSendTestScript is Script {
 
   error ChainNotSupported(uint64 destChainSelector);
 
-  function run() public {
-    // 1. Define which chain you would like to use (Ex: "ETHEREUM" as defined in .env)
-    // [INSERT BELOW]
-    string memory chainIdentifier;
+  // For the script to run successfully, please define the following constants below
+  // [REQUIRED]
+  string public chainIdentifier; // The Chain to use (Ex: "ETHEREUM" as defined in the .env)
+  uint64 public destChainSelector; // The CCIP-Specific chain selector to send the message to
+  uint256 public numTokens; // The number of tokens to be sent
 
-    // Retrieve the RPC-URL based on the identifier and defined in .env
-    // Ex: "ETHEREUM" -> "ETHEREUM_RPC_URL"
+  bytes public recipient; // The recipient to receive both the tokens and the arbitrary data.
+  bytes public data; // Define the message data to be passed to the recipient if it is NOT an EOA
+  bytes public extraArgs; // If any extraArgs are needed, define below. Due to different chains families having different extraArgs formats, they should be passed as raw bytes, and encoded separately.
+
+  address public feeToken; // The token to pay CCIP Message Fees in, address(0) for native.
+
+  function run() public {
     vm.createSelectFork(string.concat(chainIdentifier, "_RPC_URL"));
 
-    uint256 privateKey = vm.envUint("PRIVATE_KEY"); // Acquire the private key from the .env file and derive address
+    // Acquire the private key from the .env file and derive address
+    uint256 privateKey = vm.envUint("PRIVATE_KEY");
     address sender = vm.rememberKey(privateKey);
 
     vm.startBroadcast(privateKey);
@@ -39,36 +46,25 @@ contract CCIPSendTestScript is Script {
     console.log("Starting Script...");
 
     Client.EVM2AnyMessage memory message;
-    address feeToken;
 
-    // 1. Define the CCIP-Specific Chain Selector to send the message to. Automatic
-    // [INSERT BELOW]
-    uint64 destChainSelector;
-
-    // 2. Get the router address based on the current local chain identifier
-    // Ex: "ETHEREUM" -> "ETHEREUM_ROUTER"
+    // Get the router using the chain identifier Ex: "ETHEREUM" -> "ETHEREUM_ROUTER"
     address router = vm.envAddress(string.concat(chainIdentifier, "_ROUTER"));
 
     // Scoping to prevent a "stack-too-deep" error.
     {
-      // 3. Validate that the destination chain selector is supported by CCIP, and revert if not.
       bool isSupported = Router(router).isChainSupported(destChainSelector);
       if (!isSupported) revert ChainNotSupported(destChainSelector);
-
-      // 4. Declare how many unique tokens should be sent in the message
-      // [INSERT BELOW]
-      uint256 numTokens;
 
       address[] memory tokenAddresses = new address[](numTokens);
       uint256[] memory tokenAmounts = new uint256[](numTokens);
 
-      // 5. Manually define the addresses and amounts of each token that should be sent. They will automatically
+      // Since solidity does not support array literals being defined in storage or constant, manually define the addresses and amounts of each token that should be sent. They will automatically
       // be converted into a Client.EVMTokenAmount format for the CCIP-Message.
       // Ex: tokenAddresses[0] = address(1);
       // Ex: tokenAmounts[0] = 1e18;
       // [INSERT HERE]
 
-      console.log("1. Approving Send Tokens...");
+      console.log("Approving Send Tokens...");
 
       Client.EVMTokenAmount[] memory tokens = new Client.EVMTokenAmount[](numTokens);
       for (uint256 i = 0; i < tokens.length; ++i) {
@@ -88,24 +84,7 @@ contract CCIPSendTestScript is Script {
       }
       console.log("--- Tokens Approved ---");
 
-      // 6. Define the message recipient.
-      // [INSERT BELOW]
-      bytes memory recipient;
-
-      // 7. Define the message data to be passed to the recipient if it is NOT an EOA.
-      // [INSERT BELOW]
-      bytes memory data;
-
-      // 8. If any extraArgs are needed, define below. Due to different chains families having different
-      // extraArgs formats, they should be passed as raw bytes, and encoded separately.
-      // [INSERT BELOW]
-      bytes memory extraArgs;
-
-      // 9. Define the fee token to pay for the message.
-      // Ex: feeToken = address(0);
-      // [INSERT BELOW]
-
-      // 10. Construct the EVM2AnyMessage using the fields defined above.
+      // Construct the EVM2AnyMessage using the fields defined above.
       message = Client.EVM2AnyMessage({
         receiver: recipient,
         data: data,
@@ -115,7 +94,7 @@ contract CCIPSendTestScript is Script {
       });
     }
 
-    // 11. Calculate the fee in WEI for the message and approve the router if necessary.
+    // Calculate the fee in WEI for the message and approve the router if necessary.
     // Note: Even if the token is not native, it will still be provided in WEI.
     uint256 fee = Router(router).getFee(destChainSelector, message);
     console.log("Fee in WEI: %s", fee);
@@ -124,7 +103,7 @@ contract CCIPSendTestScript is Script {
       IERC20(feeToken).safeIncreaseAllowance(router, fee);
     }
 
-    // 12. Send the message, forwarding native tokens if necessary to pay the fee.
+    // Send the message, forwarding native tokens if necessary to pay the fee.
     console.log("Sending message to %i", destChainSelector);
     bytes32 messageId = Router(router).ccipSend{value: feeToken == address(0) ? fee : 0}(destChainSelector, message);
 
