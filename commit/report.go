6e730357592823c7a2b9dc05412638b6bc2ef88b
dimkouv <dimitrios.kouveris@smartcontract.com>
2025-05-05 14:52:35 +0300

    optimize report transmission checks

--- commit/report.go
+++ commit/report.go
@@ -227,7 +227,8 @@ func (p *Plugin) validateReport(
 			plugincommon.NewErrValidatingReport(fmt.Errorf("get latest price seq nr: %w", err))
 	}
 
-	if p.isStaleReport(lggr, seqNr, latestPriceSeqNr, decodedReport) {
+	multiPriceReports := p.offchainCfg.MaxPricesPerReport > 0
+	if p.isStaleReport(lggr, seqNr, latestPriceSeqNr, decodedReport, multiPriceReports) {
 		return cciptypes.CommitPluginReport{}, plugincommon.NewErrInvalidReport("stale report")
 	}
 
@@ -292,16 +293,22 @@ func (p *Plugin) decodeReport(
 
 func (p *Plugin) isStaleReport(
 	lggr logger.Logger,
-	seqNr,
-	latestPriceSeqNr uint64,
+	reportSeqNr,
+	latestOnChainReportSeqNr uint64,
 	decodedReport cciptypes.CommitPluginReport,
+	multiPriceReports bool, // multiple price reports within the same ocr round
 ) bool {
-	if seqNr <= latestPriceSeqNr &&
+	reportSeqNumStale := reportSeqNr <= latestOnChainReportSeqNr
+	if multiPriceReports {
+		reportSeqNumStale = reportSeqNr < latestOnChainReportSeqNr
+	}
+
+	if reportSeqNumStale &&
 		len(decodedReport.BlessedMerkleRoots) == 0 &&
 		len(decodedReport.UnblessedMerkleRoots) == 0 {
 		lggr.Infow(
 			"skipping stale report due to stale price seq nr and no merkle roots",
-			"latestPriceSeqNr", latestPriceSeqNr)
+			"latestPriceSeqNr", latestOnChainReportSeqNr)
 		return true
 	}
 	return false
