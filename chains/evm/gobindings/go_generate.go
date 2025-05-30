f051950affe0f2cf1a311f4c153bcf678e0c18e9
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2025-05-30 13:55:26 +0530

    update go generate

--- chains/evm/gobindings/go_generate.go
+++ chains/evm/gobindings/go_generate.go
@@ -25,6 +25,8 @@ package ccip
 //go:generate go run generation/generate/wrap.go ccip USDCTokenPool usdc_token_pool latest
 //go:generate go run generation/generate/wrap.go ccip SiloedLockReleaseTokenPool siloed_lock_release_token_pool latest
 //go:generate go run generation/generate/wrap.go ccip BurnToAddressMintTokenPool burn_to_address_mint_token_pool latest
+//go:generate go run generation/generate/wrap.go ccip FastTranserTokenPoolAbstract fast_transfer_token_pool_abstract latest
+//go:generate go run generation/generate/wrap.go ccip BurnMintFastTransferTokenPool fast_transfer_token_pool latest
 
 // Helpers
 //go:generate go run generation/generate/wrap.go ccip MaybeRevertMessageReceiver maybe_revert_message_receiver latest
