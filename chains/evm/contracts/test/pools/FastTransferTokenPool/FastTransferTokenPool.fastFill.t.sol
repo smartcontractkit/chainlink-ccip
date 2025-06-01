ba61422f539fd72f35c982179bdcc3fedced1e76
Rens Rooimans <github@rensrooimans.nl>
2025-06-01 17:57:52 +0200

    snap, wrap, test fix

--- chains/evm/contracts/test/pools/FastTransferTokenPool/FastTransferTokenPool.fastFill.t.sol
+++ chains/evm/contracts/test/pools/FastTransferTokenPool/FastTransferTokenPool.fastFill.t.sol
@@ -3,14 +3,13 @@ pragma solidity ^0.8.24;
 
 import {IFastTransferPool} from "../../../interfaces/IFastTransferPool.sol";
 
-import {FastTransferTokenPoolAbstract} from "../../../pools/FastTransferTokenPoolAbstract.sol";
-
 import {Internal} from "../../../libraries/Internal.sol";
+import {FastTransferTokenPoolAbstract} from "../../../pools/FastTransferTokenPoolAbstract.sol";
 import {FastTransferTokenPoolSetup} from "./FastTransferTokenPoolSetup.t.sol";
 
 contract FastTransferTokenPool_fastFill_Test is FastTransferTokenPoolSetup {
-  bytes32 public fillRequestId;
-  address public receiver = address(0x5);
+  bytes32 public constant FILL_REQUEST_ID = bytes32("fillRequestId");
+  address public constant RECEIVER = address(0x5);
 
   function setUp() public override {
     super.setUp();
@@ -21,38 +20,15 @@ contract FastTransferTokenPool_fastFill_Test is FastTransferTokenPoolSetup {
   }
 
   function test_FastFill() public {
-    bytes32 fillId = s_pool.computeFillId(fillRequestId, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
+    uint256 balanceBefore = s_token.balanceOf(s_filler);
 
-    s_pool.fastFill(fillRequestId, fillId, DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
+    bytes32 fillId = s_pool.computeFillId(FILL_REQUEST_ID, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
+
+    s_pool.fastFill(FILL_REQUEST_ID, fillId, DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
 
     // Verify token balances
-    assertEq(s_token.balanceOf(s_filler), 1000 ether - SOURCE_AMOUNT);
-    assertEq(s_token.balanceOf(receiver), SOURCE_AMOUNT);
-  }
-
-  function test_FastFill_RevertWhen_AlreadyFilled() public {
-    bytes32 fillId = s_pool.computeFillId(fillRequestId, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
-
-    // First fill
-    s_pool.fastFill(fillRequestId, fillId, DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
-
-    // Attempt second fill
-    vm.expectRevert(abi.encodeWithSelector(IFastTransferPool.AlreadyFilled.selector, fillRequestId));
-    s_pool.fastFill(fillRequestId, fillId, DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
-  }
-
-  function test_FastFill_RevertWhen_FillerNotWhitelisted() public {
-    vm.stopPrank();
-
-    address nonWhitelistedFiller = address(0x6);
-    vm.startPrank(nonWhitelistedFiller);
-
-    vm.expectRevert(
-      abi.encodeWithSelector(
-        FastTransferTokenPoolAbstract.FillerNotAllowlisted.selector, DEST_CHAIN_SELECTOR, nonWhitelistedFiller
-      )
-    );
-    s_pool.fastFill(fillRequestId, bytes32(0x0), DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
+    assertEq(s_token.balanceOf(s_filler), balanceBefore - SOURCE_AMOUNT);
+    assertEq(s_token.balanceOf(RECEIVER), SOURCE_AMOUNT);
   }
 
   function test_FastFill_WhitelistDisabled() public {
@@ -74,7 +50,7 @@ contract FastTransferTokenPool_fastFill_Test is FastTransferTokenPoolSetup {
 
     address nonWhitelistedFiller = address(0x6);
 
-    bytes32 fillId = s_pool.computeFillId(fillRequestId, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
+    bytes32 fillId = s_pool.computeFillId(FILL_REQUEST_ID, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
 
     // Mint tokens to non-whitelisted filler
     deal(nonWhitelistedFiller, 1000 ether);
@@ -83,10 +59,35 @@ contract FastTransferTokenPool_fastFill_Test is FastTransferTokenPoolSetup {
     s_token.approve(address(s_pool), type(uint256).max);
 
     // Should succeed even though filler is not whitelisted
-    s_pool.fastFill(fillRequestId, fillId, SOURCE_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, receiver);
+    s_pool.fastFill(FILL_REQUEST_ID, fillId, SOURCE_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
 
     // Verify token balances
     assertEq(s_token.balanceOf(nonWhitelistedFiller), 1000 ether - SOURCE_AMOUNT);
-    assertEq(s_token.balanceOf(receiver), SOURCE_AMOUNT);
+    assertEq(s_token.balanceOf(RECEIVER), SOURCE_AMOUNT);
+  }
+
+  function test_FastFill_RevertWhen_AlreadyFilled() public {
+    bytes32 fillId = s_pool.computeFillId(FILL_REQUEST_ID, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
+
+    // First fill
+    s_pool.fastFill(FILL_REQUEST_ID, fillId, DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
+
+    // Attempt second fill
+    vm.expectRevert(abi.encodeWithSelector(IFastTransferPool.AlreadyFilled.selector, FILL_REQUEST_ID));
+    s_pool.fastFill(FILL_REQUEST_ID, fillId, DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
+  }
+
+  function test_FastFill_RevertWhen_FillerNotWhitelisted() public {
+    vm.stopPrank();
+
+    address nonWhitelistedFiller = address(0x6);
+    vm.startPrank(nonWhitelistedFiller);
+
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        FastTransferTokenPoolAbstract.FillerNotAllowlisted.selector, DEST_CHAIN_SELECTOR, nonWhitelistedFiller
+      )
+    );
+    s_pool.fastFill(FILL_REQUEST_ID, bytes32(0x0), DEST_CHAIN_SELECTOR, SOURCE_AMOUNT, SOURCE_DECIMALS, RECEIVER);
   }
 }
