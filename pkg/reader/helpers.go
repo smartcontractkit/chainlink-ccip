a9c9fac02463dea3d513b3a3d5df50d8c27089e0
Will Winder <wwinder.unh@gmail.com>
2025-06-02 15:46:09 -0400

    Use generic reader helpers instead of casting inputs.

--- pkg/reader/helpers.go
+++ pkg/reader/helpers.go
@@ -23,67 +23,27 @@ const (
 	HomeChainPollingInterval = 15 * time.Second
 )
 
-// bindable is a helper interface to represent all different types of contract readers.
-// We expose per-reader type functions, but then cast to this common interface to avoid code duplication.
-type bindable interface {
-	Bind(ctx context.Context, bindings []types.BoundContract) error
-}
-
-func bindExtendedReaderContract(
-	ctx context.Context,
-	lggr logger.Logger,
-	readers map[cciptypes.ChainSelector]contractreader.Extended,
-	chainSel cciptypes.ChainSelector,
-	contractName string,
-	address []byte,
-	addrCodec cciptypes.AddressCodec,
-) (types.BoundContract, error) {
-	casted := make(map[cciptypes.ChainSelector]bindable, len(readers))
-	for k, v := range readers {
-		casted[k] = v
-	}
-	return bindReaderContract(ctx, lggr, casted, chainSel, contractName, address, addrCodec)
-}
-
-func bindFacadeReaderContract(
-	ctx context.Context,
-	lggr logger.Logger,
-	readers map[cciptypes.ChainSelector]contractreader.ContractReaderFacade,
-	chainSel cciptypes.ChainSelector,
-	contractName string,
-	address []byte,
-	addrCodec cciptypes.AddressCodec,
-) (types.BoundContract, error) {
-	casted := make(map[cciptypes.ChainSelector]bindable, len(readers))
-	for k, v := range readers {
-		casted[k] = v
-	}
-	return bindReaderContract(ctx, lggr, casted, chainSel, contractName, address, addrCodec)
-}
-
 // bindReaderContract is a generic helper for binding contracts to readers, the addresses input is the same object
 // returned by DiscoverContracts.
 //
 // No error is returned if contractName is not found in the contracts. This allows calling the function before all
 // contracts are discovered.
-func bindReaderContract(
+func bindReaderContract[T contractreader.ContractReaderFacade](
 	ctx context.Context,
 	lggr logger.Logger,
-	readers map[cciptypes.ChainSelector]bindable,
+	readers map[cciptypes.ChainSelector]T,
 	chainSel cciptypes.ChainSelector,
 	contractName string,
 	address []byte,
 	codec cciptypes.AddressCodec,
 ) (types.BoundContract, error) {
-	var empty types.BoundContract
-
 	if err := validateReaderExistence(readers, chainSel); err != nil {
-		return empty, fmt.Errorf("validate reader existence: %w", err)
+		return types.BoundContract{}, fmt.Errorf("validate reader existence: %w", err)
 	}
 
 	addressStr, err := codec.AddressBytesToString(address, chainSel)
 	if err != nil {
-		return empty, fmt.Errorf("unable to convert address bytes to string: %w, address: %v", err, address)
+		return types.BoundContract{}, fmt.Errorf("unable to convert address bytes to string: %w, address: %v", err, address)
 	}
 
 	contract := types.BoundContract{
@@ -101,25 +61,14 @@ func bindReaderContract(
 	// If the address is changed -> updates the address, overwrites the existing one
 	// If the contract not bound -> binds to the new address
 	if err := readers[chainSel].Bind(ctx, []types.BoundContract{contract}); err != nil {
-		return empty, fmt.Errorf("unable to bind %s %s for chain %d: %w", contractName, addressStr, chainSel, err)
+		return types.BoundContract{}, fmt.Errorf("unable to bind %s %s for chain %d: %w", contractName, addressStr, chainSel, err)
 	}
 
 	return contract, nil
 }
 
-func validateExtendedReaderExistence(
-	readers map[cciptypes.ChainSelector]contractreader.Extended,
-	chains ...cciptypes.ChainSelector,
-) error {
-	casted := make(map[cciptypes.ChainSelector]bindable, len(readers))
-	for k, v := range readers {
-		casted[k] = v
-	}
-	return validateReaderExistence(casted, chains...)
-}
-
-func validateReaderExistence(
-	readers map[cciptypes.ChainSelector]bindable,
+func validateReaderExistence[T contractreader.ContractReaderFacade](
+	readers map[cciptypes.ChainSelector]T,
 	chains ...cciptypes.ChainSelector,
 ) error {
 	for _, ch := range chains {
