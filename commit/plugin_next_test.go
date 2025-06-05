76378f16839f5a30cdb805503f662679ce93d65d
dimkouv <dimitrios.kouveris@smartcontract.com>
2025-06-05 11:22:43 +0300

    test observation next

---
+++ commit/plugin_next_test.go
@@ -0,0 +1,212 @@
+package commit
+
+import (
+	"errors"
+	"math/big"
+	"testing"
+	"time"
+
+	chainsel "github.com/smartcontractkit/chain-selectors"
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
+	"github.com/smartcontractkit/libocr/commontypes"
+	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
+	"github.com/smartcontractkit/libocr/offchainreporting2plus/types"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/chainlink-ccip/commit/chainfee"
+	"github.com/smartcontractkit/chainlink-ccip/commit/committypes"
+	"github.com/smartcontractkit/chainlink-ccip/commit/merkleroot"
+	"github.com/smartcontractkit/chainlink-ccip/commit/metrics"
+	"github.com/smartcontractkit/chainlink-ccip/commit/tokenprice"
+	"github.com/smartcontractkit/chainlink-ccip/mocks/internal_/plugincommon"
+	reader2 "github.com/smartcontractkit/chainlink-ccip/mocks/internal_/reader"
+	"github.com/smartcontractkit/chainlink-ccip/mocks/pkg/reader"
+	v1 "github.com/smartcontractkit/chainlink-ccip/pkg/ocrtypecodec/v1"
+	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
+)
+
+func Test_observationNext_prices(t *testing.T) {
+	destChainSel := chainsel.ABSTRACT_TESTNET.Selector
+	fDestChain := 2
+	testCases := []struct {
+		name                        string
+		oracleSupportsDestChain     bool
+		currentRoundOcrSeqNum       uint64
+		inflightOcrSeqNum           uint64
+		remainingChecks             int
+		onchainOcrSeqNum            uint64
+		rpcErr                      error
+		expObservedOnChainOcrSeqNum uint64
+		expObservedPrices           bool
+	}{
+		{
+			name:                        "no inflight prices, no onchain prices, initial state",
+			oracleSupportsDestChain:     true,
+			currentRoundOcrSeqNum:       1,
+			inflightOcrSeqNum:           0,
+			remainingChecks:             0,
+			onchainOcrSeqNum:            0,
+			expObservedOnChainOcrSeqNum: 0,
+			expObservedPrices:           false,
+		},
+		{
+			name:                        "initial prices observed on this round",
+			oracleSupportsDestChain:     true,
+			currentRoundOcrSeqNum:       2,
+			inflightOcrSeqNum:           0,
+			remainingChecks:             0,
+			onchainOcrSeqNum:            0,
+			expObservedOnChainOcrSeqNum: 0,
+			expObservedPrices:           true,
+		},
+		{
+			name:                        "inflight prices from previous round not seen on-chain",
+			oracleSupportsDestChain:     true,
+			currentRoundOcrSeqNum:       3,
+			inflightOcrSeqNum:           2,
+			remainingChecks:             10,
+			onchainOcrSeqNum:            0,
+			expObservedOnChainOcrSeqNum: 0,
+			expObservedPrices:           false,
+		},
+		{
+			name:                        "inflight prices from previous round are seen on-chain",
+			oracleSupportsDestChain:     true,
+			currentRoundOcrSeqNum:       5,
+			inflightOcrSeqNum:           2,
+			remainingChecks:             10,
+			onchainOcrSeqNum:            2, // <--
+			expObservedOnChainOcrSeqNum: 2,
+			expObservedPrices:           false,
+		},
+		{
+			name:                        "inflight prices seen on-chain but oracle des not support dest",
+			oracleSupportsDestChain:     false,
+			currentRoundOcrSeqNum:       5,
+			inflightOcrSeqNum:           2,
+			remainingChecks:             10,
+			onchainOcrSeqNum:            2,
+			expObservedOnChainOcrSeqNum: 0, // <-- observes 0
+			expObservedPrices:           false,
+		},
+		{
+			name:                        "no inflight prices, no onchain prices, later state",
+			oracleSupportsDestChain:     true,
+			currentRoundOcrSeqNum:       5678,
+			inflightOcrSeqNum:           0,
+			remainingChecks:             0,
+			onchainOcrSeqNum:            5600,
+			expObservedOnChainOcrSeqNum: 0,
+			expObservedPrices:           true,
+		},
+		{
+			name:                        "inflight prices, later state",
+			oracleSupportsDestChain:     true,
+			currentRoundOcrSeqNum:       5678,
+			inflightOcrSeqNum:           5670, // <--
+			remainingChecks:             50,
+			onchainOcrSeqNum:            5600,
+			expObservedOnChainOcrSeqNum: 5600,
+			expObservedPrices:           false,
+		},
+		{
+			name:                        "rpc error while getting on chain seq num",
+			oracleSupportsDestChain:     true,
+			currentRoundOcrSeqNum:       5678,
+			inflightOcrSeqNum:           5670,
+			remainingChecks:             50,
+			onchainOcrSeqNum:            5600,
+			rpcErr:                      errors.New("some rpc error"),
+			expObservedOnChainOcrSeqNum: 0, // <----
+			expObservedPrices:           false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// setup mock dependencies
+			ctx := tests.Context(t)
+			cs := plugincommon.NewMockChainSupport(t)
+			ccr := reader.NewMockCCIPReader(t)
+			mrp := plugincommon.NewMockPluginProcessor[merkleroot.Query, merkleroot.Observation, merkleroot.Outcome](t)
+			tpp := plugincommon.NewMockPluginProcessor[tokenprice.Query, tokenprice.Observation, tokenprice.Outcome](t)
+			cfp := plugincommon.NewMockPluginProcessor[chainfee.Query, chainfee.Observation, chainfee.Outcome](t)
+			home := reader2.NewMockHomeChain(t)
+
+			// initialize plugin
+			p := &Plugin{
+				lggr:                logger.Test(t),
+				ocrTypeCodec:        v1.NewCommitCodecProto(),
+				merkleRootProcessor: mrp,
+				tokenPriceProcessor: tpp,
+				chainFeeProcessor:   cfp,
+				metricsReporter:     &metrics.Noop{},
+				chainSupport:        cs,
+				ccipReader:          ccr,
+				destChain:           ccipocr3.ChainSelector(destChainSel),
+				homeChain:           home,
+				oracleID:            commontypes.OracleID(9),
+			}
+
+			// set expectations
+			home.EXPECT().GetFChain().Return(map[ccipocr3.ChainSelector]int{
+				ccipocr3.ChainSelector(destChainSel): fDestChain,
+			}, nil)
+
+			cs.EXPECT().SupportsDestChain(p.oracleID).Return(tc.oracleSupportsDestChain, nil).Maybe()
+
+			mrp.EXPECT().Observation(mock.Anything, mock.Anything, mock.Anything).Return(merkleroot.Observation{}, nil)
+
+			ccr.EXPECT().GetLatestPriceSeqNr(mock.Anything).Return(tc.onchainOcrSeqNum, tc.rpcErr).Maybe()
+
+			tokenPriceObs := tokenprice.Observation{}
+			if tc.expObservedPrices {
+				tokenPriceObs.FeedTokenPrices = map[ccipocr3.UnknownEncodedAddress]ccipocr3.BigInt{
+					"123": ccipocr3.NewBigIntFromInt64(2),
+				}
+			}
+			tpp.EXPECT().Observation(mock.Anything, mock.Anything, mock.Anything).Return(tokenPriceObs, nil).Maybe()
+
+			chainFeeObs := chainfee.Observation{}
+			if tc.expObservedPrices {
+				chainFeeObs.ChainFeeUpdates = map[ccipocr3.ChainSelector]chainfee.Update{
+					ccipocr3.ChainSelector(destChainSel): {
+						Timestamp: time.Now(),
+						ChainFee: chainfee.ComponentsUSDPrices{
+							ExecutionFeePriceUSD: big.NewInt(2),
+							DataAvFeePriceUSD:    big.NewInt(3),
+						},
+					},
+				}
+			}
+			cfp.EXPECT().Observation(mock.Anything, mock.Anything, mock.Anything).Return(chainFeeObs, nil).Maybe()
+
+			// encode previous outcome and call observation function
+			prevOutcome, err := p.ocrTypeCodec.EncodeOutcome(committypes.Outcome{
+				MainOutcome: committypes.MainOutcome{
+					InflightPriceOcrSequenceNumber: ccipocr3.SeqNum(tc.inflightOcrSeqNum),
+					RemainingPriceChecks:           tc.remainingChecks,
+				},
+			})
+			require.NoError(t, err)
+
+			obsBytes, err := p.observationNext(ctx, ocr3types.OutcomeContext{
+				SeqNr:           tc.currentRoundOcrSeqNum,
+				PreviousOutcome: prevOutcome,
+			}, types.Query{})
+			require.NoError(t, err)
+
+			// assert results are the expected
+			obs, err := p.ocrTypeCodec.DecodeObservation(obsBytes)
+			require.NoError(t, err)
+
+			require.Equal(t, tc.expObservedOnChainOcrSeqNum, obs.OnChainPriceOcrSeqNum)
+			if tc.expObservedPrices {
+				require.NotEmpty(t, obs.TokenPriceObs.FeedTokenPrices)
+				require.NotEmpty(t, obs.ChainFeeObs.ChainFeeUpdates)
+			}
+		})
+	}
+}
