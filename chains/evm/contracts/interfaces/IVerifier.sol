19cff1f0f40763b688f3aaff8845770531abcb66
Rens Rooimans <github@rensrooimans.nl>
2025-06-16 13:09:57 +0200

    move nonces to commit verifier

--- chains/evm/contracts/interfaces/IVerifier.sol
+++ chains/evm/contracts/interfaces/IVerifier.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {Internal} from "../libraries/Internal.sol";
+
 interface IVerifier {
-  function validateReport(bytes memory rawReport, bytes memory ocrProof, uint256 verifierIndex) external;
+  function validateReport(
+    bytes memory rawReport,
+    bytes memory ocrProof,
+    uint256 verifierIndex,
+    Internal.MessageExecutionState originalState
+  ) external;
 }
