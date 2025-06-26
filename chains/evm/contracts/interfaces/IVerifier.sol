57a1af957cf4f76b31a3fca806be08e390bf05e8
Rens Rooimans <github@rensrooimans.nl>
2025-06-26 17:16:49 +0200

    sending side interfacs

--- chains/evm/contracts/interfaces/IVerifier.sol
+++ chains/evm/contracts/interfaces/IVerifier.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.8.0;
 import {Internal} from "../libraries/Internal.sol";
 
 interface IVerifier {
+  /// @notice Message execution
   function validateReport(
     bytes memory rawReport,
     bytes memory ocrProof,
     uint256 verifierIndex,
     Internal.MessageExecutionState originalState
   ) external;
+}
+
+interface IVerifierSender {
+  /// @notice Message sending
+  // TODO versioning?
+  function forwardToVerifier(bytes memory rawMessage, uint256 verifierIndex) external returns (bytes memory);
 }
