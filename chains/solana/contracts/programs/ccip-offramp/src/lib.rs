4da4f60d08973940d333cebaeba3b8bb3464094a
PabloMansanet <pablo.mansanet@smartcontract.com>
2025-07-07 16:58:18 +0200

    Fix CCTP/derivation related comments

--- chains/solana/contracts/programs/ccip-offramp/src/lib.rs
+++ chains/solana/contracts/programs/ccip-offramp/src/lib.rs
@@ -607,12 +607,12 @@ pub mod ccip_offramp {
     ///
     /// * `accounts_to_save`: The caller must append these accounts to a list they maintain.
     ///   When complete, this list will contain all accounts needed to call `ccip_execute`.
-    /// * `ask_again_with`: When this list is not empty, the caller must call `derive_accounts_execute`
+    /// * `ask_again_with`: When `next_stage` is not empty, the caller must call `derive_accounts_execute`
     ///   again, including exactly these accounts as the `remaining_accounts`.
     /// * `lookup_tables_to_save`: The caller must save those LUTs. They can be used for `ccip_execute`.
     /// * `current_stage`: A string describing the current stage of the derivation process. When the stage
-    ///   is "TokenTransferAccounts", it means the `accounts_to_save` block in this response contains
-    ///   all accounts relating to a single token being transferred. Use this information to construct
+    ///   is "TokenTransferStaticAccounts/<N>/0", it means the `accounts_to_save` block in this response contains
+    ///   all accounts relating to the Nth token being transferred. Use this information to construct
     ///   the `token_indexes` vector that `execute` requires.
     /// * `next_stage`: If nonempty, this means the instruction must get called again with this value
     ///   as the `stage` argument.
