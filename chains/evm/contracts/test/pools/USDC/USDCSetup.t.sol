6d5a967bd298798ceefbc443b0a47aec0969bcdf
Josh <josh.weintraub@smartcontract.com>
2025-07-07 14:31:30 -0500

    forge fmt

--- chains/evm/contracts/test/pools/USDC/USDCSetup.t.sol
+++ chains/evm/contracts/test/pools/USDC/USDCSetup.t.sol
@@ -18,7 +18,6 @@ import {MockE2EUSDCTransmitter} from "../../mocks/MockE2EUSDCTransmitter.sol";
 import {MockUSDCTokenMessenger} from "../../mocks/MockUSDCTokenMessenger.sol";
 
 contract USDCSetup is BaseTest {
-
   // solhint-disable-next-line gas-struct-packing
   struct USDCMessage {
     uint32 version;
