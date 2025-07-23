6f28e44ea851ab455316ab1bac404c4dc1a668f2
Josh <josh.weintraub@smartcontract.com>
2025-07-23 12:07:47 -0400

    remove unused dependencies

--- chains/evm/contracts/pools/ERC20LockBox.sol
+++ chains/evm/contracts/pools/ERC20LockBox.sol
@@ -4,18 +4,16 @@ pragma solidity ^0.8.24;
 import {TokenAdminRegistry} from "../tokenAdminRegistry/TokenAdminRegistry.sol";
 
 import {Ownable2StepMsgSender} from "@chainlink/contracts/src/v0.8/shared/access/Ownable2StepMsgSender.sol";
+
 import {IERC20} from
   "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from
   "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
-import {EnumerableSet} from
-  "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v5.0.2/contracts/utils/structs/EnumerableSet.sol";
 
 /// @title ERC20 Lock Box
 /// @notice A contract that holds ERC20 tokens for a token pool to simplify pool upgrades without requiring a liquidity migration.
 contract ERC20LockBox is Ownable2StepMsgSender {
   using SafeERC20 for IERC20;
-  using EnumerableSet for EnumerableSet.AddressSet;
 
   error Unauthorized(address caller);
   error InsufficientBalance(uint64 remoteChainSelector, uint256 requested, uint256 available);
@@ -77,6 +75,7 @@ contract ERC20LockBox is Ownable2StepMsgSender {
       revert RecipientCannotBeZeroAddress();
     }
 
+    // Sanity Check for balance to prevent underflow
     uint256 balance = s_tokenBalances[token][remoteChainSelector];
     if (balance < amount) {
       revert InsufficientBalance(remoteChainSelector, amount, balance);
