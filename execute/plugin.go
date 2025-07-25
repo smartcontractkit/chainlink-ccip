29c5e8e10825042fa5fcc92ef41294c02457edc8
dimitris <dimitrios.kouveris@smartcontract.com>
2025-07-25 09:55:38 +0300

    Fix exec plugin observation validation (#1104)
    * fix exec observation validation
    
    * include state in validations
    
    * re-org the code as Makram recommended
    
    * fix linter

--- execute/plugin.go
+++ execute/plugin.go
@@ -384,7 +384,9 @@ func (p *Plugin) ValidateObservation(
 		}
 	}
 
-	if err = validateCommonStateObservations(p, ao.Observer, decodedObservation, supportedChains); err != nil {
+	state := previousOutcome.State.Next()
+
+	if err = validateCommonStateObservations(p, ao.Observer, decodedObservation, supportedChains, state); err != nil {
 		return err
 	}
 
@@ -413,17 +415,19 @@ func validateCommonStateObservations(
 	oracleID commontypes.OracleID,
 	decodedObservation exectypes.Observation,
 	supportedChains mapset.Set[cciptypes.ChainSelector],
+	state exectypes.PluginState,
 ) error {
 	if err := plugincommon.ValidateFChain(decodedObservation.FChain); err != nil {
 		return fmt.Errorf("failed to validate FChain: %w", err)
 	}
 
 	// These checks are common to all states.
-	if err := validateCommitReportsReadingEligibility(supportedChains, decodedObservation.CommitReports); err != nil {
+	if err := validateCommitReportsReadingEligibility(
+		state, supportedChains, p.destChain, decodedObservation.CommitReports); err != nil {
 		return fmt.Errorf("validate commit reports reading eligibility: %w", err)
 	}
 
-	if err := validateObservedSequenceNumbers(supportedChains, decodedObservation.CommitReports); err != nil {
+	if err := validateObservedSequenceNumbers(decodedObservation.CommitReports); err != nil {
 		return fmt.Errorf("validate observed sequence numbers: %w", err)
 	}
 
