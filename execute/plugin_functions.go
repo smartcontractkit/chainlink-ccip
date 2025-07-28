cc090aa53bc8dcd32e8b0b4e87dd06c18ccaea82
asoliman <abdelrahman.soliman@smartcontract.com>
2025-07-28 14:47:31 +0400

    fix: handle multiple consensus messages with pseudo deletions

--- execute/plugin_functions.go
+++ execute/plugin_functions.go
@@ -451,8 +451,33 @@ func computeMessageObservationsConsensus(
 					results[chain] = make(map[cciptypes.SeqNum]cciptypes.Message)
 				}
 				results[chain][seqNum] = msgsWithConsensus[0]
+			case 2:
+				// In rare cases where more than f+1 nodes observed the message and f+1 nodes pseudo deleted the message
+				// because of observation size limit, we can end up with 2 consensus on the same message with one of
+				// them being psuedo deleted. We need to choose the one that wasn't deleted as the one with consensus.
+				var msg *cciptypes.Message
+
+				if msgsWithConsensus[0].IsPseudoDeleted() && !msgsWithConsensus[1].IsPseudoDeleted() {
+					msg = &msgsWithConsensus[1]
+				} else if msgsWithConsensus[1].IsPseudoDeleted() && !msgsWithConsensus[0].IsPseudoDeleted() {
+					msg = &msgsWithConsensus[0]
+				}
+
+				if msg == nil {
+					lggr.Errorw("more than one message reached consensus for a sequence number, skipping it",
+						"chain", chain, "seqNum", seqNum, "msgs", msgsWithConsensus)
+				}
+
+				if _, ok := results[chain]; !ok && msg != nil {
+					results[chain] = make(map[cciptypes.SeqNum]cciptypes.Message)
+				}
+
+				if msg != nil {
+					results[chain][seqNum] = *msg
+				}
+
 			default:
-				lggr.Warnw("more than one message reached consensus for a sequence number, skipping it",
+				lggr.Errorw("more than one message reached consensus for a sequence number, skipping it",
 					"chain", chain, "seqNum", seqNum, "msgs", msgsWithConsensus)
 			}
 		}
