ee76d3cf259f36f3cebdc9be6b7a71ce46d58c7f
asoliman <abdelrahman.soliman@smartcontract.com>
2025-07-28 15:46:08 +0400

    Fix consensus test

--- execute/outcome_test.go
+++ execute/outcome_test.go
@@ -21,45 +21,23 @@ func TestOutcome_PseudoDeletedConsensusConflict(t *testing.T) {
 	srcChain2 := cciptypes.ChainSelector(222)
 	srcChain1SeqNr := cciptypes.SeqNum(4001)
 	srcChain2SeqNr := cciptypes.SeqNum(5001)
-	fullObs := exectypes.Observation{
-		CommitReports: nil,
-		Messages: exectypes.MessageObservations{
-			srcChain1: map[cciptypes.SeqNum]cciptypes.Message{
-				srcChain1SeqNr: {
-					Header: cciptypes.RampMessageHeader{
-						MessageID:      cciptypes.Bytes32{4, 0, 0, 1},
-						SequenceNumber: srcChain1SeqNr,
-					},
-					Sender: []byte{1, 1, 1, 1, 1},
-				},
-			},
-			srcChain2: map[cciptypes.SeqNum]cciptypes.Message{
-				srcChain2SeqNr: {
-					Header: cciptypes.RampMessageHeader{
-						MessageID:      cciptypes.Bytes32{5, 0, 0, 1},
-						SequenceNumber: srcChain2SeqNr,
-					},
-					Sender: []byte{1, 1, 1, 1, 1},
-				},
-			},
+	msg1 := cciptypes.Message{
+		Header: cciptypes.RampMessageHeader{
+			MessageID:           cciptypes.Bytes32{4, 0, 0, 1},
+			SequenceNumber:      srcChain1SeqNr,
+			SourceChainSelector: srcChain1,
+			DestChainSelector:   destChain,
 		},
-		Hashes: exectypes.MessageHashes{
-			srcChain1: map[cciptypes.SeqNum]cciptypes.Bytes32{srcChain1SeqNr: {1, 1, 1, 1, 1, 1}},
-			srcChain2: map[cciptypes.SeqNum]cciptypes.Bytes32{srcChain2SeqNr: {1, 1, 1, 1, 1, 2}},
+		Sender: []byte{1, 1, 1, 1, 1},
+	}
+	msg2 := cciptypes.Message{
+		Header: cciptypes.RampMessageHeader{
+			MessageID:           cciptypes.Bytes32{5, 0, 0, 1},
+			SequenceNumber:      srcChain2SeqNr,
+			SourceChainSelector: srcChain2,
+			DestChainSelector:   destChain,
 		},
-		TokenData: map[cciptypes.ChainSelector]map[cciptypes.SeqNum]exectypes.MessageTokenData{
-			srcChain1: {
-				srcChain1SeqNr: exectypes.NewMessageTokenData(),
-			},
-			srcChain2: {
-				srcChain2SeqNr: exectypes.NewMessageTokenData(),
-			},
-		},
-		FChain: map[cciptypes.ChainSelector]int{
-			srcChain1: 1,
-			srcChain2: 1,
-			destChain: 1,
-		},
+		Sender: []byte{1, 1, 1, 1, 1},
 	}
 
 	oracleObservations := make(map[commontypes.OracleID]exectypes.Observation)
@@ -67,13 +45,40 @@ func TestOutcome_PseudoDeletedConsensusConflict(t *testing.T) {
 	// oracles 1 and 2 make a full observation with populated msgs
 	// oracles 3 and 4 observe pseudo deleted msgs
 	for _, oracleID := range []uint8{1, 2, 3, 4} {
-		obs := fullObs
+		obs := exectypes.Observation{
+			CommitReports: nil,
+			Messages: exectypes.MessageObservations{
+				srcChain1: map[cciptypes.SeqNum]cciptypes.Message{
+					srcChain1SeqNr: msg1,
+				},
+				srcChain2: map[cciptypes.SeqNum]cciptypes.Message{
+					srcChain2SeqNr: msg2,
+				},
+			},
+			Hashes: exectypes.MessageHashes{
+				srcChain1: map[cciptypes.SeqNum]cciptypes.Bytes32{srcChain1SeqNr: {1, 1, 1, 1, 1, 1}},
+				srcChain2: map[cciptypes.SeqNum]cciptypes.Bytes32{srcChain2SeqNr: {1, 1, 1, 1, 1, 2}},
+			},
+			TokenData: map[cciptypes.ChainSelector]map[cciptypes.SeqNum]exectypes.MessageTokenData{
+				srcChain1: {
+					srcChain1SeqNr: exectypes.NewMessageTokenData(),
+				},
+				srcChain2: {
+					srcChain2SeqNr: exectypes.NewMessageTokenData(),
+				},
+			},
+			FChain: map[cciptypes.ChainSelector]int{
+				srcChain1: 1,
+				srcChain2: 1,
+				destChain: 1,
+			},
+		}
 
 		// oracles 3 and 4 observe chain 2 msg as pseudo deleted
 		if oracleID == 3 || oracleID == 4 {
 			// Create a pseudo deleted message for srcChain2
-			msg2 := createEmptyMessageWithIDAndSeqNum(obs.Messages[srcChain2][srcChain2SeqNr])
-			obs.Messages[srcChain2][srcChain2SeqNr] = msg2
+			msg2PseudoDeleted := createEmptyMessageWithIDAndSeqNum(obs.Messages[srcChain2][srcChain2SeqNr])
+			obs.Messages[srcChain2][srcChain2SeqNr] = msg2PseudoDeleted
 		}
 		oracleObservations[commontypes.OracleID(oracleID)] = obs
 	}
@@ -89,6 +94,6 @@ func TestOutcome_PseudoDeletedConsensusConflict(t *testing.T) {
 	consensusObs, err := computeConsensusObservation(lggr, aos, 999, 1)
 	require.NoError(t, err)
 	require.Len(t, consensusObs.Messages, 2)
-	require.Equal(t, fullObs.Messages[srcChain1][srcChain1SeqNr], consensusObs.Messages[srcChain1][srcChain1SeqNr])
-	require.Equal(t, fullObs.Messages[srcChain2][srcChain2SeqNr], consensusObs.Messages[srcChain2][srcChain2SeqNr])
+	require.Equal(t, msg1, consensusObs.Messages[srcChain1][srcChain1SeqNr])
+	require.Equal(t, msg2, consensusObs.Messages[srcChain2][srcChain2SeqNr])
 }
