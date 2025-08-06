fdcea84339e16206081c3b2e884fa6a3e46511f8
Josh <josh.weintraub@smartcontract.com>
2025-08-06 10:16:14 -0500

    comments and an additional test

--- chains/evm/contracts/tokenAdminRegistry/TokenPoolFactory/HyperEVMLinker.sol
+++ chains/evm/contracts/tokenAdminRegistry/TokenPoolFactory/HyperEVMLinker.sol
@@ -2,9 +2,15 @@ pragma solidity ^0.8.25;
 
 import {Ownable2StepMsgSender} from "@chainlink/contracts/src/v0.8/shared/access/Ownable2StepMsgSender.sol";
 
+/// @title HyperEVMLinker
+/// @notice This contract is used to link the hyperEVM to HyperCore by storing a special address in a required slot.
+/// @dev This contract is built in accordance with the Hyperliq standard at the following URL:
+/// https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/hyperevm/hypercore-less-than-greater-than-hyperevm-transfers#linking-core-and-evm-spot-assets
 abstract contract HyperEVMLinker is Ownable2StepMsgSender {
   // keccak256("HyperCore deployer")
-  bytes32 private constant HYPER_EVM_LINKER_SLOT = 0x8c306a6a12fff1951878e8621be6674add1102cd359dd968efbbe797629ef84f;
+  // Inline assembly requires manually specifying the hash and Hyperliquid documents require the address to be stored
+  // in this specific storage slot.
+  bytes32 internal constant HYPER_EVM_LINKER_SLOT = 0x8c306a6a12fff1951878e8621be6674add1102cd359dd968efbbe797629ef84f;
 
   error LinkerAddressCannotBeZero();
 
