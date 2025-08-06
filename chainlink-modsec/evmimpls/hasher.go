b516ace0a9778903b226d2d7a1df1e0138aacf4f
Makram Kamaleddine <makramkd@users.noreply.github.com>
2025-08-06 17:33:41 +0300

    add hasher and signer

---
+++ chainlink-modsec/evmimpls/hasher.go
@@ -0,0 +1,17 @@
+package evmimpls
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/smartcontractkit/chainlink-ccip/chainlink-modsec/modsectypes"
+)
+
+var _ modsectypes.Hasher = (*EVMHasher)(nil)
+
+type EVMHasher struct {
+}
+
+func (h *EVMHasher) Hash(ctx context.Context, data []byte) ([]byte, error) {
+	return crypto.Keccak256(data), nil
+}
---
+++ chainlink-modsec/evmimpls/signer.go
@@ -0,0 +1,24 @@
+package evmimpls
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/smartcontractkit/chainlink-ccip/chainlink-modsec/modsectypes"
+)
+
+var _ modsectypes.Signer = (*EVMSigner)(nil)
+
+type EVMSigner struct {
+	privateKey *ecdsa.PrivateKey
+}
+
+func (s *EVMSigner) Sign(ctx context.Context, digest []byte) ([]byte, error) {
+	if len(digest) != 32 {
+		return nil, fmt.Errorf("digest must be 32 bytes long")
+	}
+
+	return crypto.Sign(digest, s.privateKey)
+}
