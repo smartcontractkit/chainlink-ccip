3dd16c87adb580db49504259f4649a63461052bf
Josh <josh.weintraub@smartcontract.com>
2025-08-07 15:27:07 -0500

    productionize

---
+++ chains/evm/contracts/test/pools/USDC/USDCTokenPoolProxy/USDCTokenPoolProxy.lockOrBurn.t.sol
@@ -0,0 +1,213 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.24;
+
+import {Pool} from "../../../../libraries/Pool.sol";
+
+import {TokenPool} from "../../../../pools/TokenPool.sol";
+import {USDCTokenPool} from "../../../../pools/USDC/USDCTokenPool.sol";
+import {USDCTokenPoolProxy} from "../../../../pools/USDC/USDCTokenPoolProxy.sol";
+import {USDCTokenPoolProxySetup} from "./USDCTokenPoolProxySetup.t.sol";
+
+contract USDCTokenPoolProxy_lockOrBurn is USDCTokenPoolProxySetup {
+  address internal s_sender = makeAddr("sender");
+  address internal s_receiver = makeAddr("receiver");
+  bytes internal s_destPoolData = abi.encode(1, 2, 3);
+
+  function setUp() public virtual override {
+    super.setUp();
+
+    // Configure lock or burn mechanisms for different chains
+    uint64[] memory chainSelectors = new uint64[](3);
+    chainSelectors[0] = SOURCE_CHAIN_SELECTOR;
+    chainSelectors[1] = DEST_CHAIN_SELECTOR;
+    chainSelectors[2] = 12345; // Another test chain
+
+    USDCTokenPoolProxy.LockOrBurnMechanism[] memory mechanisms = new USDCTokenPoolProxy.LockOrBurnMechanism[](3);
+    mechanisms[0] = USDCTokenPoolProxy.LockOrBurnMechanism.CCTP_V1;
+    mechanisms[1] = USDCTokenPoolProxy.LockOrBurnMechanism.CCTP_V2;
+    mechanisms[2] = USDCTokenPoolProxy.LockOrBurnMechanism.LOCK_RELEASE;
+
+    s_usdcTokenPoolProxy.updateLockOrBurnMechanisms(chainSelectors, mechanisms);
+  }
+
+  function test_lockOrBurn_CCTPV1() public {
+    uint256 amount = 100;
+    bytes memory destTokenAddress = abi.encode(address(s_USDCToken));
+
+    // Set the DEST_CHAIN_SELECTOR to use CCTP V1 using the update function
+    uint64[] memory selectors = new uint64[](1);
+    selectors[0] = DEST_CHAIN_SELECTOR;
+    USDCTokenPoolProxy.LockOrBurnMechanism[] memory mechs = new USDCTokenPoolProxy.LockOrBurnMechanism[](1);
+    mechs[0] = USDCTokenPoolProxy.LockOrBurnMechanism.CCTP_V1;
+    s_usdcTokenPoolProxy.updateLockOrBurnMechanisms(selectors, mechs);
+
+    Pool.LockOrBurnInV1 memory lockOrBurnIn = Pool.LockOrBurnInV1({
+      receiver: abi.encode(s_receiver),
+      remoteChainSelector: DEST_CHAIN_SELECTOR,
+      originalSender: s_sender,
+      amount: amount,
+      localToken: address(s_USDCToken)
+    });
+
+    // Mock the CCTP V1 pool's lockOrBurn to return expected output
+    Pool.LockOrBurnOutV1 memory expectedOutput =
+      Pool.LockOrBurnOutV1({destTokenAddress: destTokenAddress, destPoolData: s_destPoolData});
+
+    vm.mockCall(
+      address(s_cctpV1Pool),
+      abi.encodeWithSelector(USDCTokenPool.lockOrBurn.selector, lockOrBurnIn),
+      abi.encode(expectedOutput)
+    );
+
+    vm.startPrank(s_routerAllowedOnRamp);
+
+    Pool.LockOrBurnOutV1 memory result = s_usdcTokenPoolProxy.lockOrBurn(lockOrBurnIn);
+
+    assertEq(result.destTokenAddress, expectedOutput.destTokenAddress);
+    assertEq(result.destPoolData, expectedOutput.destPoolData);
+  }
+
+  function test_lockOrBurn_CCTPV2() public {
+    // Arrange: Define test constants
+    uint256 amount = 200;
+    bytes memory destTokenAddress = abi.encode(address(s_USDCToken));
+
+    Pool.LockOrBurnInV1 memory lockOrBurnIn = Pool.LockOrBurnInV1({
+      receiver: abi.encode(s_receiver),
+      remoteChainSelector: DEST_CHAIN_SELECTOR,
+      originalSender: s_sender,
+      amount: amount,
+      localToken: address(s_USDCToken)
+    });
+
+    // Mock the CCTP V2 pool's lockOrBurn to return expected output
+    Pool.LockOrBurnOutV1 memory expectedOutput =
+      Pool.LockOrBurnOutV1({destTokenAddress: destTokenAddress, destPoolData: s_destPoolData});
+
+    vm.mockCall(
+      address(s_cctpV2Pool),
+      abi.encodeWithSelector(USDCTokenPool.lockOrBurn.selector, lockOrBurnIn),
+      abi.encode(expectedOutput)
+    );
+
+    vm.startPrank(s_routerAllowedOnRamp);
+    Pool.LockOrBurnOutV1 memory result = s_usdcTokenPoolProxy.lockOrBurn(lockOrBurnIn);
+    assertEq(result.destTokenAddress, expectedOutput.destTokenAddress);
+    assertEq(result.destPoolData, expectedOutput.destPoolData);
+  }
+
+  function test_lockOrBurn_LockRelease() public {
+    uint64 testChainSelector = 12345;
+    uint256 amount = 300;
+    bytes memory destTokenAddress = abi.encode(address(s_USDCToken));
+    bytes[] memory remotePoolAddresses = new bytes[](1);
+    remotePoolAddresses[0] = abi.encode(address(s_lockReleasePool));
+
+    vm.mockCall(
+      address(s_router),
+      abi.encodeWithSelector(bytes4(keccak256("getOnRamp(uint64)")), uint64(testChainSelector)),
+      abi.encode(s_routerAllowedOnRamp)
+    );
+
+    Pool.LockOrBurnInV1 memory lockOrBurnIn = Pool.LockOrBurnInV1({
+      receiver: abi.encode(s_receiver),
+      remoteChainSelector: testChainSelector,
+      originalSender: s_sender,
+      amount: amount,
+      localToken: address(s_USDCToken)
+    });
+
+    // Mock the lock release pool's lockOrBurn to return expected output
+    Pool.LockOrBurnOutV1 memory expectedOutput =
+      Pool.LockOrBurnOutV1({destTokenAddress: destTokenAddress, destPoolData: s_destPoolData});
+
+    vm.mockCall(
+      address(s_lockReleasePool),
+      abi.encodeWithSelector(USDCTokenPool.lockOrBurn.selector, lockOrBurnIn),
+      abi.encode(expectedOutput)
+    );
+
+    vm.startPrank(s_routerAllowedOnRamp);
+
+    Pool.LockOrBurnOutV1 memory result = s_usdcTokenPoolProxy.lockOrBurn(lockOrBurnIn);
+    assertEq(result.destTokenAddress, expectedOutput.destTokenAddress);
+    assertEq(result.destPoolData, expectedOutput.destPoolData);
+  }
+
+  // Reverts
+
+  function test_lockOrBurn_RevertWhen_InvalidLockOrBurnMechanism() public {
+    uint64 testChainSelector = 99999;
+    uint256 amount = 100;
+    bytes[] memory remotePoolAddresses = new bytes[](1);
+    remotePoolAddresses[0] = abi.encode(address(s_lockReleasePool));
+
+    // Call applyChainUpdates to add support for remoteChainSelector 99999
+    {
+      TokenPool.ChainUpdate[] memory chainUpdates = new TokenPool.ChainUpdate[](1);
+      chainUpdates[0] = TokenPool.ChainUpdate({
+        remoteChainSelector: testChainSelector,
+        remotePoolAddresses: remotePoolAddresses,
+        remoteTokenAddress: abi.encode(address(s_USDCToken)),
+        outboundRateLimiterConfig: _getOutboundRateLimiterConfig(),
+        inboundRateLimiterConfig: _getInboundRateLimiterConfig()
+      });
+      s_usdcTokenPoolProxy.applyChainUpdates(new uint64[](0), chainUpdates);
+    }
+
+    vm.mockCall(
+      address(s_router),
+      abi.encodeWithSelector(bytes4(keccak256("getOnRamp(uint64)")), uint64(testChainSelector)),
+      abi.encode(s_routerAllowedOnRamp)
+    );
+
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        USDCTokenPoolProxy.InvalidLockOrBurnMechanism.selector, USDCTokenPoolProxy.LockOrBurnMechanism(0)
+      )
+    );
+
+    vm.startPrank(s_routerAllowedOnRamp);
+    s_usdcTokenPoolProxy.lockOrBurn(
+      Pool.LockOrBurnInV1({
+        receiver: abi.encode(s_receiver),
+        remoteChainSelector: testChainSelector, // Chain with no configured mechanism
+        originalSender: s_sender,
+        amount: amount,
+        localToken: address(s_USDCToken)
+      })
+    );
+  }
+
+  function test_lockOrBurn_RevertWhen_InvalidReceiver() public {
+    uint256 amount = 100;
+    bytes memory invalidReceiver = abi.encode(s_receiver, "extra"); // Invalid receiver format
+
+    Pool.LockOrBurnInV1 memory lockOrBurnIn = Pool.LockOrBurnInV1({
+      receiver: invalidReceiver,
+      remoteChainSelector: DEST_CHAIN_SELECTOR,
+      originalSender: s_sender,
+      amount: amount,
+      localToken: address(s_USDCToken)
+    });
+
+    vm.expectRevert();
+    vm.startPrank(s_routerAllowedOnRamp);
+    s_usdcTokenPoolProxy.lockOrBurn(lockOrBurnIn);
+  }
+
+  function test_lockOrBurn_RevertWhen_InvalidAmount() public {
+    vm.startPrank(s_routerAllowedOnRamp);
+
+    vm.expectRevert();
+    s_usdcTokenPoolProxy.lockOrBurn(
+      Pool.LockOrBurnInV1({
+        receiver: abi.encode(s_receiver),
+        remoteChainSelector: DEST_CHAIN_SELECTOR,
+        originalSender: s_sender,
+        amount: 0, // Invalid amount
+        localToken: address(s_USDCToken)
+      })
+    );
+  }
+}
---
+++ chains/evm/contracts/test/pools/USDC/USDCTokenPoolProxy/USDCTokenPoolProxySetup.t.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.24;
+
+import {TokenPool} from "../../../../pools/TokenPool.sol";
+import {CCTPMessageTransmitterProxy} from "../../../../pools/USDC/CCTPMessageTransmitterProxy.sol";
+import {USDCTokenPoolProxy} from "../../../../pools/USDC/USDCTokenPoolProxy.sol";
+import {USDCSetup} from "../USDCSetup.t.sol";
+
+contract USDCTokenPoolProxySetup is USDCSetup {
+  address internal s_cctpV1Pool = makeAddr("cctpV1Pool");
+  address internal s_cctpV2Pool = makeAddr("cctpV2Pool");
+  address internal s_lockReleasePool = makeAddr("lockReleasePool");
+  address internal s_mockTransmitterProxy = makeAddr("mockTransmitterProxy");
+  uint64 internal s_remoteLockReleaseChainSelector = 12345;
+
+  USDCTokenPoolProxy internal s_usdcTokenPoolProxy;
+
+  function setUp() public virtual override {
+    super.setUp();
+
+    // Mock the transmitter proxy's receiveMessage function to return true
+    vm.mockCall(
+      address(s_cctpV1Pool),
+      abi.encodeWithSelector(bytes4(keccak256("i_messageTransmitterProxy()"))),
+      abi.encode(s_mockTransmitterProxy)
+    );
+
+    // Deploy the proxy
+    s_usdcTokenPoolProxy = new USDCTokenPoolProxy(
+      s_USDCToken,
+      address(s_router),
+      new address[](0),
+      address(s_mockRMNRemote),
+      USDCTokenPoolProxy.PoolAddresses({
+        cctpV1Pool: s_cctpV1Pool,
+        cctpV2Pool: s_cctpV2Pool,
+        lockReleasePool: s_lockReleasePool
+      })
+    );
+
+    // Deal some tokens to the proxy to test the transfer to the destination pool
+    deal(address(s_USDCToken), address(s_usdcTokenPoolProxy), 1000e6);
+
+    // Configure the pools
+    _poolApplyChainUpdates(address(s_usdcTokenPoolProxy));
+
+    bytes[] memory sourcePoolAddresses = new bytes[](1);
+    sourcePoolAddresses[0] = abi.encode(SOURCE_CHAIN_USDC_POOL);
+
+    TokenPool.ChainUpdate[] memory chainUpdates = new TokenPool.ChainUpdate[](1);
+    chainUpdates[0] = TokenPool.ChainUpdate({
+      remoteChainSelector: s_remoteLockReleaseChainSelector,
+      remotePoolAddresses: sourcePoolAddresses,
+      remoteTokenAddress: abi.encode(address(s_USDCToken)),
+      outboundRateLimiterConfig: _getOutboundRateLimiterConfig(),
+      inboundRateLimiterConfig: _getInboundRateLimiterConfig()
+    });
+
+    s_usdcTokenPoolProxy.applyChainUpdates(new uint64[](0), chainUpdates);
+
+    // Configure allowed callers for the CCTP message transmitter proxy
+    CCTPMessageTransmitterProxy.AllowedCallerConfigArgs[] memory allowedCallerParams =
+      new CCTPMessageTransmitterProxy.AllowedCallerConfigArgs[](3);
+    allowedCallerParams[0] =
+      CCTPMessageTransmitterProxy.AllowedCallerConfigArgs({caller: address(s_cctpV1Pool), allowed: true});
+    allowedCallerParams[1] =
+      CCTPMessageTransmitterProxy.AllowedCallerConfigArgs({caller: address(s_cctpV2Pool), allowed: true});
+    allowedCallerParams[2] =
+      CCTPMessageTransmitterProxy.AllowedCallerConfigArgs({caller: address(s_lockReleasePool), allowed: true});
+    s_cctpMessageTransmitterProxy.configureAllowedCallers(allowedCallerParams);
+  }
+}
