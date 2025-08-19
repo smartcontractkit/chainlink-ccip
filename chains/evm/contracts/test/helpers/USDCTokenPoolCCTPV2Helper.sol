a34a005d312991a206eb083a4cd224264d965e1f
Josh Weintraub <26035072+jhweintraub@users.noreply.github.com>
2025-08-19 17:33:41 -0400

    CCIP-6697 CCTP V2 Phase 2: Child Token Pool Changes (#1133)
    * begin migrating files from other PR for phase 2
    
    * initial compilation and tests passing.
    
    * cleanup and wrapper updates
    
    * remove previousPool from the USDC Token Pool as it will be moved to the future proxy
    
    * more cleanup
    
    * checkpointing
    
    * cleanup
    
    * additional comments
    
    * forge fmt
    
    * bump type and version for future release cuts
    
    * wrappers regen
    
    * changeset
    
    * review comment fixes and test cleanup
    
    * fix CI from merge conflicts
    
    * code review fixes
    
    * feedback fixes
    
    * comment fix
    
    * typo fix

--- chains/evm/contracts/test/helpers/USDCTokenPoolCCTPV2Helper.sol
+++ chains/evm/contracts/test/helpers/USDCTokenPoolCCTPV2Helper.sol
@@ -5,9 +5,9 @@ import {ITokenMessenger} from "../../pools/USDC/interfaces/ITokenMessenger.sol";
 import {IBurnMintERC20} from "@chainlink/contracts/src/v0.8/shared/token/ERC20/IBurnMintERC20.sol";
 
 import {CCTPMessageTransmitterProxy} from "../../pools/USDC/CCTPMessageTransmitterProxy.sol";
-import {USDCTokenPool} from "../../pools/USDC/USDCTokenPool.sol";
+import {USDCTokenPoolCCTPV2} from "../../pools/USDC/USDCTokenPoolCCTPV2.sol";
 
-contract USDCTokenPoolHelper is USDCTokenPool {
+contract USDCTokenPoolCCTPV2Helper is USDCTokenPoolCCTPV2 {
   constructor(
     ITokenMessenger tokenMessenger,
     CCTPMessageTransmitterProxy messageTransmitterProxy,
@@ -15,7 +15,7 @@ contract USDCTokenPoolHelper is USDCTokenPool {
     address[] memory allowlist,
     address rmnProxy,
     address router
-  ) USDCTokenPool(tokenMessenger, messageTransmitterProxy, token, allowlist, rmnProxy, router, 0) {}
+  ) USDCTokenPoolCCTPV2(tokenMessenger, messageTransmitterProxy, token, allowlist, rmnProxy, router) {}
 
   function validateMessage(bytes memory usdcMessage, SourceTokenDataPayload memory sourceTokenData) external view {
     return _validateMessage(usdcMessage, sourceTokenData);
