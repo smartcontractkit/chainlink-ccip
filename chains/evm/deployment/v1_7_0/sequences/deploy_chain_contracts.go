8425ca53c6c2478649db544bcd75c48ed3ca20a1
kylesmartin <54827727+kylesmartin@users.noreply.github.com>
2025-09-04 07:46:33 -0400

    Add ExecutorOnRamp (#1157)
    * Add ExecutorOnRamp
    
    * Add operations, finish first draft
    
    * Address comments
    
    * Add tests
    
    * Add getFee test
    
    * Comments
    
    * Gethwrappers, precommit
    
    * Conditionally emit CCVAllowlistUpdated

--- chains/evm/deployment/v1_7_0/sequences/deploy_chain_contracts.go
+++ chains/evm/deployment/v1_7_0/sequences/deploy_chain_contracts.go
@@ -20,6 +20,7 @@ import (
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_7_0/operations/ccv_proxy"
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_7_0/operations/commit_offramp"
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_7_0/operations/commit_onramp"
+	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_7_0/operations/executor_onramp"
 	"github.com/smartcontractkit/chainlink-deployments-framework/chain/evm"
 	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
 	cldf_deployment "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
@@ -28,7 +29,7 @@ import (
 )
 
 const (
-	NUM_CONTRACTS = 12
+	NUM_CONTRACTS = 13
 	NUM_TXS       = 3
 )
 
@@ -60,13 +61,18 @@ type FeeQuoterParams struct {
 	WETHPremiumMultiplierWeiPerEth uint64
 }
 
+type ExecutorOnRampParams struct {
+	MaxCCVsPerMsg uint8
+}
+
 type ContractParams struct {
-	RMNRemote     RMNRemoteParams
-	CCVAggregator CCVAggregatorParams
-	CommitOnRamp  CommitOnRampParams
-	CommitOffRamp CommitOffRampParams
-	CCVProxy      CCVProxyParams
-	FeeQuoter     FeeQuoterParams
+	RMNRemote      RMNRemoteParams
+	CCVAggregator  CCVAggregatorParams
+	CommitOnRamp   CommitOnRampParams
+	CommitOffRamp  CommitOffRampParams
+	CCVProxy       CCVProxyParams
+	FeeQuoter      FeeQuoterParams
+	ExecutorOnRamp ExecutorOnRampParams
 }
 
 type DeployChainContractsInput struct {
@@ -272,6 +278,18 @@ var DeployChainContracts = cldf_ops.NewSequence(
 			return DeployChainContractsOutput{}, fmt.Errorf("failed to deploy CommitOnRamp: %w", err)
 		}
 		addresses = append(addresses, commitOnRampRef)
+
+		// Deploy ExecutorOnRamp
+		executorOnRampRef, err := maybeDeployContract(b, executor_onramp.Deploy, executor_onramp.ContractType, chain, deployment.Input[executor_onramp.ConstructorArgs]{
+			ChainSelector: chain.Selector,
+			Args: executor_onramp.ConstructorArgs{
+				MaxCCVsPerMsg: input.ContractParams.ExecutorOnRamp.MaxCCVsPerMsg,
+			},
+		}, input.ExistingAddresses)
+		if err != nil {
+			return DeployChainContractsOutput{}, fmt.Errorf("failed to deploy ExecutorOnRamp: %w", err)
+		}
+		addresses = append(addresses, executorOnRampRef)
 
 		// Deploy CommitOffRamp
 		commitOffRampRef, err := maybeDeployContract(b, commit_offramp.Deploy, commit_offramp.ContractType, chain, deployment.Input[commit_offramp.ConstructorArgs]{
