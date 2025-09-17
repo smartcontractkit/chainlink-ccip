144eceffdadd04a31cdfbb28cc4ee5faadfa7b45
Rens Rooimans <github@rensrooimans.nl>
2025-09-17 14:17:42 +0200

    add commit offramp tests

--- chains/evm/contracts/offRamp/CommitOffRamp.sol
+++ chains/evm/contracts/offRamp/CommitOffRamp.sol
@@ -11,12 +11,21 @@ import {IERC165} from
   "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v5.0.2/contracts/utils/introspection/IERC165.sol";
 
 contract CommitOffRamp is ICCVOffRampV1, SignatureQuorumVerifier, ITypeAndVersion {
+  error InvalidCCVData();
+
   string public constant override typeAndVersion = "CommitOffRamp 1.7.0-dev";
 
   uint256 internal constant SIGNATURE_LENGTH_BYTES = 2;
 
   function verifyMessage(MessageV1Codec.MessageV1 calldata, bytes32 messageHash, bytes calldata ccvData) external view {
+    if (ccvData.length < SIGNATURE_LENGTH_BYTES) {
+      revert InvalidCCVData();
+    }
+
     uint256 signatureLength = uint16(bytes2(ccvData[:SIGNATURE_LENGTH_BYTES]));
+    if (ccvData.length < SIGNATURE_LENGTH_BYTES + signatureLength) {
+      revert InvalidCCVData();
+    }
 
     // Even though the current version of this contract only expects signatures to be included in the ccvData, bounding
     // it to the given length allows potential forward compatibility with future formats that supply more data.
