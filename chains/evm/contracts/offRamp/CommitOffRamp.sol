96b376eb4f1bd48d48bb13fb3a7802ffabc7ae96
Abdelrahman Soliman (Boda) <2677789+asoliman92@users.noreply.github.com>
2025-09-17 18:32:46 +0400

    Remove nonce from commitofframp (#1202)
    * Remove nonce from commitofframp
    
    * linting
    
    * compile

--- chains/evm/contracts/offRamp/CommitOffRamp.sol
+++ chains/evm/contracts/offRamp/CommitOffRamp.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import {ICCVOffRampV1} from "../interfaces/ICCVOffRampV1.sol";
-import {INonceManager} from "../interfaces/INonceManager.sol";
 import {ITypeAndVersion} from "@chainlink/contracts/src/v0.8/shared/interfaces/ITypeAndVersion.sol";
 
 import {Internal} from "../libraries/Internal.sol";
@@ -31,32 +30,15 @@ contract CommitOffRamp is ICCVOffRampV1, SignatureQuorumVerifier, ITypeAndVersio
   }
 
   function verifyMessage(
-    MessageV1Codec.MessageV1 calldata message,
+    MessageV1Codec.MessageV1 calldata,
     bytes32 messageHash,
     bytes calldata ccvData,
-    Internal.MessageExecutionState originalState
+    Internal.MessageExecutionState
   ) external {
     (bytes memory ccvArgs, bytes32[] memory rs, bytes32[] memory ss) =
       abi.decode(ccvData, (bytes, bytes32[], bytes32[]));
 
     _validateSignatures(keccak256(bytes.concat(messageHash, ccvArgs)), rs, ss);
-
-    uint64 nonce = abi.decode(ccvArgs, (uint64));
-
-    // Nonce changes per state transition (these only apply for ordered messages):
-    // UNTOUCHED -> FAILURE  nonce bump.
-    // UNTOUCHED -> SUCCESS  nonce bump.
-    // FAILURE   -> SUCCESS  no nonce bump.
-    // UNTOUCHED messages MUST be executed in order always.
-    // If nonce == 0 then out of order execution is allowed.
-    if (nonce != 0) {
-      if (originalState == Internal.MessageExecutionState.UNTOUCHED) {
-        // If a nonce is not incremented, that means it was skipped, and we can ignore the message.
-        if (!INonceManager(i_nonceManager).incrementInboundNonce(message.sourceChainSelector, nonce, message.sender)) {
-          revert InvalidNonce(nonce);
-        }
-      }
-    }
   }
 
   function supportsInterface(
