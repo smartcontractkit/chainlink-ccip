0a12dc1130545caadecfdf47268af51f63af28fc
Josh <josh.weintraub@smartcontract.com>
2025-09-18 11:07:03 -0400

    linter fix

--- chains/evm/contracts/test/pools/USDC/USDCTokenPoolProxy/USDCTokenPoolProxySetup.t.sol
+++ chains/evm/contracts/test/pools/USDC/USDCTokenPoolProxy/USDCTokenPoolProxySetup.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
-import {TokenPool} from "../../../../pools/TokenPool.sol";
 import {CCTPMessageTransmitterProxy} from "../../../../pools/USDC/CCTPMessageTransmitterProxy.sol";
 import {USDCTokenPoolProxy} from "../../../../pools/USDC/USDCTokenPoolProxy.sol";
 import {USDCSetup} from "../USDCSetup.t.sol";
