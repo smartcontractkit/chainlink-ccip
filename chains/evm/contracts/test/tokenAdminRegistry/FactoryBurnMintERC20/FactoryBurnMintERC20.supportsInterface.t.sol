75d5ac91e4f4366b61eb42489a0cdb88cedbd0a7
Josh <josh.weintraub@smartcontract.com>
2025-09-23 10:47:33 -0500

    CI coverage gap fill in

--- chains/evm/contracts/test/tokenAdminRegistry/FactoryBurnMintERC20/FactoryBurnMintERC20.supportsInterface.t.sol
+++ chains/evm/contracts/test/tokenAdminRegistry/FactoryBurnMintERC20/FactoryBurnMintERC20.supportsInterface.t.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+import {IGetCCIPAdmin} from "../../../interfaces/IGetCCIPAdmin.sol";
 
 import {BurnMintERC20Setup} from "./BurnMintERC20Setup.t.sol";
 import {IBurnMintERC20} from "@chainlink/contracts/src/v0.8/shared/token/ERC20/IBurnMintERC20.sol";
@@ -11,5 +12,7 @@ contract FactoryBurnMintERC20_supportsInterface is BurnMintERC20Setup {
     assertTrue(s_burnMintERC20.supportsInterface(type(IERC20).interfaceId));
     assertTrue(s_burnMintERC20.supportsInterface(type(IBurnMintERC20).interfaceId));
     assertTrue(s_burnMintERC20.supportsInterface(type(IERC165).interfaceId));
+    assertTrue(s_burnMintERC20.supportsInterface(type(IGetCCIPAdmin).interfaceId));
+
   }
 }
