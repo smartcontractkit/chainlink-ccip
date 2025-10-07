5129e8b4e2d8435d93c66186c594b26ba22bcf67
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2025-10-07 15:03:30 +0530

    add tests

--- chains/evm/contracts/test/pools/FastTransferTokenPool/FastTransferTokenPool.getCcipSendTokenFee.t.sol
+++ chains/evm/contracts/test/pools/FastTransferTokenPool/FastTransferTokenPool.getCcipSendTokenFee.t.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.24;
 import {IFastTransferPool} from "../../../interfaces/IFastTransferPool.sol";
 import {IRouterClient} from "../../../interfaces/IRouterClient.sol";
 
+import {FastTransferTokenPoolAbstract} from "../../../pools/FastTransferTokenPoolAbstract.sol";
+import {TokenPool} from "../../../pools/TokenPool.sol";
 import {FastTransferTokenPoolSetup} from "./FastTransferTokenPoolSetup.t.sol";
 
 contract FastTransferTokenPool_getCcipSendTokenFee_Test is FastTransferTokenPoolSetup {
-  function test_GetCcipSendTokenFee() public {
+  function test_getCcipSendTokenFee() public {
     uint256 fastFee = SOURCE_AMOUNT * FAST_FEE_FILLER_BPS / 10000;
     uint256 settlementQuote = 1 ether;
 
@@ -21,7 +23,17 @@ contract FastTransferTokenPool_getCcipSendTokenFee_Test is FastTransferTokenPool
     assertEq(quote.ccipSettlementFee, settlementQuote);
   }
 
-  function test_GetCcipSendTokenFee_WithNativeFeeToken() public {
+  function test_getCcipSendTokenFee_WithNativeFeeToken() public {
+    TokenPool.ChainUpdate[] memory chainUpdates = new TokenPool.ChainUpdate[](1);
+    chainUpdates[0] = TokenPool.ChainUpdate({
+      remoteChainSelector: 9999,
+      remotePoolAddresses: new bytes[](0),
+      remoteTokenAddress: abi.encode(address(2)),
+      outboundRateLimiterConfig: _getOutboundRateLimiterConfig(),
+      inboundRateLimiterConfig: _getInboundRateLimiterConfig()
+    });
+    s_pool.applyChainUpdates(new uint64[](0), chainUpdates);
+
     uint256 fastFee = SOURCE_AMOUNT * FAST_FEE_FILLER_BPS / 10000;
     uint256 settlementQuote = 1 ether;
 
