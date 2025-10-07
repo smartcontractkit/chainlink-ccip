face92eb35648d6f367337d8f78d7acbc698d923
tt-cll <141346969+tt-cll@users.noreply.github.com>
2025-10-07 09:47:27 -0700

    cld ccip api e2e (#1227)
    * wip
    
    * flatten
    
    * flatten
    
    * iterate
    
    * iterate
    
    * iterate
    
    * iterate
    
    * iterate
    
    * iterate
    
    * iterate
    
    * seed solana
    
    * move ref

---
+++ chains/solana/deployment/v1_6_0/adapter.go
@@ -0,0 +1,38 @@
+package v160
+
+import (
+	"github.com/Masterminds/semver/v3"
+	chain_selectors "github.com/smartcontractkit/chain-selectors"
+
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+
+	mcmstypes "github.com/smartcontractkit/mcms/types"
+
+	ccipapi "github.com/smartcontractkit/chainlink-ccip/deployment/v1_6"
+)
+
+func init() {
+	v, err := semver.NewVersion("1.6.0")
+	if err != nil {
+		panic(err)
+	}
+	ccipapi.RegisterChainAdapter(chain_selectors.FamilySolana, v, &SolanaAdapter{})
+}
+
+type SolanaAdapter struct{}
+
+func (a *SolanaAdapter) GetOnRampAddress(e cldf.Environment, chainSelector uint64) ([]byte, error) {
+	return []byte{}, nil // Not implemented for Solana
+}
+
+func (a *SolanaAdapter) GetOffRampAddress(e cldf.Environment, chainSelector uint64) ([]byte, error) {
+	return []byte{}, nil // Not implemented for Solana
+}
+
+func (a *SolanaAdapter) GetTimelockAddress(e cldf.Environment, chainSelector uint64) (string, error) {
+	return "", nil // Not implemented for Solana
+}
+
+func (a *SolanaAdapter) GetMCMSMetadata(e cldf.Environment, chainSelector uint64, action mcmstypes.TimelockAction) (mcmstypes.ChainMetadata, error) {
+	return mcmstypes.ChainMetadata{}, nil // Not implemented for Solana
+}
