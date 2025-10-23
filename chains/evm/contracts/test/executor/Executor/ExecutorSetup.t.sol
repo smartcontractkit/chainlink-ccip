547d96574039f184e69b37c1563459816154d0ad
Rens Rooimans <github@rensrooimans.nl>
2025-10-23 14:26:44 +0200

    introduce dynamicConfif

--- chains/evm/contracts/test/executor/Executor/ExecutorSetup.t.sol
+++ chains/evm/contracts/test/executor/Executor/ExecutorSetup.t.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.24;
 
 import {Executor} from "../../../executor/Executor.sol";
 import {BaseTest} from "../../BaseTest.t.sol";
+import {BurnMintERC20} from "@chainlink/contracts/src/v0.8/shared/token/ERC20/BurnMintERC20.sol";
 
 contract ExecutorSetup is BaseTest {
-  Executor internal s_executor;
   address internal constant INITIAL_CCV = address(121212);
+  address internal constant FEE_AGGREGATOR = address(999999);
   uint8 internal constant INITIAL_MAX_CCVS = 1;
   uint16 internal constant DEFAULT_EXEC_FEE_USD_CENTS = 89;
   uint32 internal constant DEFAULT_EXEC_GAS = 150_000;
@@ -14,10 +15,20 @@ contract ExecutorSetup is BaseTest {
 
   uint8 internal constant EVM_ADDRESS_LENGTH = 20;
 
-  function setUp() public override {
+  Executor internal s_executor;
+  address internal s_sourceFeeToken;
+
+  function setUp() public virtual override {
     super.setUp();
 
-    s_executor = new Executor(INITIAL_MAX_CCVS, MIN_BLOCK_CONFIRMATIONS);
+    Executor.DynamicConfig memory dynamicConfig = Executor.DynamicConfig({
+      feeAggregator: FEE_AGGREGATOR,
+      minBlockConfirmations: MIN_BLOCK_CONFIRMATIONS,
+      ccvAllowlistEnabled: true
+    });
+
+    s_executor = new Executor(INITIAL_MAX_CCVS, dynamicConfig);
+    s_sourceFeeToken = address(new BurnMintERC20("test", "test", 18, 0, 0));
 
     address[] memory ccvs = new address[](1);
     ccvs[0] = INITIAL_CCV;
