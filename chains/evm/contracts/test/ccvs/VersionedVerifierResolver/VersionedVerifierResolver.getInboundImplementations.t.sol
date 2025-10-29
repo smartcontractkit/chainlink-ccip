9e1638516a9cff08de3db80b1a2980183c93a54a
Kyle Martin <kyle.martin@smartcontract.com>
2025-10-29 07:09:20 -0700

    Lint

--- chains/evm/contracts/test/ccvs/VersionedVerifierResolver/VersionedVerifierResolver.getInboundImplementations.t.sol
+++ chains/evm/contracts/test/ccvs/VersionedVerifierResolver/VersionedVerifierResolver.getInboundImplementations.t.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
-import {VersionedVerifierResolver} from "../../../ccvs/VersionedVerifierResolver.sol";
 import {VersionedVerifierResolverSetup} from "./VersionedVerifierResolverSetup.t.sol";
-import {Ownable2Step} from "@chainlink/contracts/src/v0.8/shared/access/Ownable2Step.sol";
 
 contract VersionedVerifierResolver_getInboundImplementation is VersionedVerifierResolverSetup {
   function test_getInboundImplementation_InvalidCCVDataLength() public view {
