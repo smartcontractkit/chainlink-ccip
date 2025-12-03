f17dc5224bf74aee53daa9a9a638706d02277a87
Alkhara <cawley22@gmail.com>
2025-12-03 15:09:37 -0500

    fix fmt

--- chains/evm/contracts/test/fork/Liquidity_Migration.t.sol
+++ chains/evm/contracts/test/fork/Liquidity_Migration.t.sol
@@ -6,70 +6,83 @@ import {MCMSForkTest} from "./MCMSForkTest.t.sol";
 import {IERC20} from "@openzeppelin/contracts@4.8.3/token/ERC20/IERC20.sol";
 
 interface LockReleaseTokenPool {
-    function getRebalancer() external view returns (address);
+  function getRebalancer() external view returns (address);
 }
 
 contract LiquidityMigration is MCMSForkTest {
-    address private oldPoolAddress;
-    address private newPoolAddress;
-    address private tokenAddress;
-    address private tokenAdminRegistryAddress;
-    address private rebalancerAddress;
-    address private timelockAddress;
-    uint256 private forkId;
-    bytes[] private payloads;
-    uint256 private transferAmount;
+  address private oldPoolAddress;
+  address private newPoolAddress;
+  address private tokenAddress;
+  address private tokenAdminRegistryAddress;
+  address private rebalancerAddress;
+  address private timelockAddress;
+  uint256 private forkId;
+  bytes[] private payloads;
+  uint256 private transferAmount;
 
-    function setUp() public {
-        forkId = vm.createFork(vm.envString("RPC_URL"));
-        
-        // Load payloads dynamically based on PAYLOAD_COUNT
-        uint256 payloadCount = vm.envUint("PAYLOAD_COUNT");
-        payloads = new bytes[](payloadCount);
-        for (uint256 i = 0; i < payloadCount; i++) {
-            payloads[i] = vm.envBytes(string.concat("PAYLOAD_", vm.toString(i + 1)));
-        }
-        
-        transferAmount = vm.envUint("TRANSFER_AMOUNT");
-        oldPoolAddress = vm.envAddress("OLD_POOL_ADDRESS");
-        newPoolAddress = vm.envAddress("NEW_POOL_ADDRESS");
-        tokenAddress = vm.envAddress("TOKEN_ADDRESS");
-        tokenAdminRegistryAddress = vm.envAddress("TOKEN_ADMIN_REGISTRY_ADDRESS");
-        rebalancerAddress = vm.envAddress("REBALANCER_ADDRESS");
-        timelockAddress = vm.envAddress("TIMELOCK_ADDRESS");
+  function setUp() public {
+    forkId = vm.createFork(vm.envString("RPC_URL"));
+
+    // Load payloads dynamically based on PAYLOAD_COUNT
+    uint256 payloadCount = vm.envUint("PAYLOAD_COUNT");
+    payloads = new bytes[](payloadCount);
+    for (uint256 i = 0; i < payloadCount; i++) {
+      payloads[i] = vm.envBytes(string.concat("PAYLOAD_", vm.toString(i + 1)));
     }
 
-    function testMigration() public {
-        vm.selectFork(forkId);
+    transferAmount = vm.envUint("TRANSFER_AMOUNT");
+    oldPoolAddress = vm.envAddress("OLD_POOL_ADDRESS");
+    newPoolAddress = vm.envAddress("NEW_POOL_ADDRESS");
+    tokenAddress = vm.envAddress("TOKEN_ADDRESS");
+    tokenAdminRegistryAddress = vm.envAddress("TOKEN_ADMIN_REGISTRY_ADDRESS");
+    rebalancerAddress = vm.envAddress("REBALANCER_ADDRESS");
+    timelockAddress = vm.envAddress("TIMELOCK_ADDRESS");
+  }
 
-        // Get the token balance for the old pool
-        uint256 oldPoolBalance = IERC20(tokenAddress).balanceOf(oldPoolAddress);
+  function testMigration() public {
+    vm.selectFork(forkId);
 
-        // Get the token balance for the new pool
-        uint256 newPoolBalance = IERC20(tokenAddress).balanceOf(newPoolAddress);
+    // Get the token balance for the old pool
+    uint256 oldPoolBalance = IERC20(tokenAddress).balanceOf(oldPoolAddress);
 
-        // Apply the liquidity migration payloads
-        for (uint256 i = 0; i < payloads.length; i++) {
-            applyPayload(timelockAddress, payloads[i]);
-        }
+    // Get the token balance for the new pool
+    uint256 newPoolBalance = IERC20(tokenAddress).balanceOf(newPoolAddress);
 
-        // Get the token config for the token
-        TokenAdminRegistry.TokenConfig memory cfg = TokenAdminRegistry(tokenAdminRegistryAddress).getTokenConfig(tokenAddress);
-        assertEq(cfg.tokenPool, newPoolAddress, "Registry should have the new Token Pool");
-
-        // Get the token balance for the old pool
-        uint256 updatedOldPoolBalance = IERC20(tokenAddress).balanceOf(oldPoolAddress);
-
-        // Get the token balance for the new pool
-        uint256 updatedNewPoolBalance = IERC20(tokenAddress).balanceOf(newPoolAddress);
-
-        // Ensure that the old pool balance has decreased by the expected amounts
-        assertEq(oldPoolBalance - updatedOldPoolBalance, transferAmount, "TOKEN balance should have decreased by the expected amount");
-
-        // Ensure that the new pool balance has increased by the expected amounts
-        assertEq(updatedNewPoolBalance - newPoolBalance, transferAmount, "TOKEN balance should have increased by the expected amount");
-
-        // Ensure that the rebalancer is set to the expected address
-        assertEq(LockReleaseTokenPool(newPoolAddress).getRebalancer(), rebalancerAddress, "Rebalancer should be set to the expected address");
+    // Apply the liquidity migration payloads
+    for (uint256 i = 0; i < payloads.length; i++) {
+      applyPayload(timelockAddress, payloads[i]);
     }
+
+    // Get the token config for the token
+    TokenAdminRegistry.TokenConfig memory cfg =
+      TokenAdminRegistry(tokenAdminRegistryAddress).getTokenConfig(tokenAddress);
+    assertEq(cfg.tokenPool, newPoolAddress, "Registry should have the new Token Pool");
+
+    // Get the token balance for the old pool
+    uint256 updatedOldPoolBalance = IERC20(tokenAddress).balanceOf(oldPoolAddress);
+
+    // Get the token balance for the new pool
+    uint256 updatedNewPoolBalance = IERC20(tokenAddress).balanceOf(newPoolAddress);
+
+    // Ensure that the old pool balance has decreased by the expected amounts
+    assertEq(
+      oldPoolBalance - updatedOldPoolBalance,
+      transferAmount,
+      "TOKEN balance should have decreased by the expected amount"
+    );
+
+    // Ensure that the new pool balance has increased by the expected amounts
+    assertEq(
+      updatedNewPoolBalance - newPoolBalance,
+      transferAmount,
+      "TOKEN balance should have increased by the expected amount"
+    );
+
+    // Ensure that the rebalancer is set to the expected address
+    assertEq(
+      LockReleaseTokenPool(newPoolAddress).getRebalancer(),
+      rebalancerAddress,
+      "Rebalancer should be set to the expected address"
+    );
+  }
 }
--- chains/evm/contracts/test/fork/MCMSForkTest.t.sol
+++ chains/evm/contracts/test/fork/MCMSForkTest.t.sol
@@ -4,22 +4,22 @@ pragma solidity ^0.8.24;
 import {Test} from "forge-std/Test.sol";
 
 contract MCMSForkTest is Test {
-    struct Call {
-        address target;
-        uint256 value;
-        bytes data;
-    }
+  struct Call {
+    address target;
+    uint256 value;
+    bytes data;
+  }
 
-    error TransactionReverted();
+  error TransactionReverted();
 
-    function applyPayload(address sender, bytes memory payload) internal {
-        (MCMSForkTest.Call[] memory calls, , , ) = abi.decode(payload, (MCMSForkTest.Call[], bytes32, bytes32, uint256));
-        for (uint256 i = 0; i < calls.length; ++i) {
-            MCMSForkTest.Call memory call = calls[i];
-            vm.startPrank(sender);
-            (bool success, ) = call.target.call{value: call.value}(call.data);
-            if (!success) revert TransactionReverted();
-            vm.stopPrank();
-        }
+  function applyPayload(address sender, bytes memory payload) internal {
+    (MCMSForkTest.Call[] memory calls,,,) = abi.decode(payload, (MCMSForkTest.Call[], bytes32, bytes32, uint256));
+    for (uint256 i = 0; i < calls.length; ++i) {
+      MCMSForkTest.Call memory call = calls[i];
+      vm.startPrank(sender);
+      (bool success,) = call.target.call{value: call.value}(call.data);
+      if (!success) revert TransactionReverted();
+      vm.stopPrank();
     }
+  }
 }
