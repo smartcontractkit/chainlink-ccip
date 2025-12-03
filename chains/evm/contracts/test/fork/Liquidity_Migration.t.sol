ef9d4b7bef482d6093e94537115ce365f1d967c2
Alkhara <cawley22@gmail.com>
2025-12-03 14:45:06 -0500

    Allow for any number of payloads

--- chains/evm/contracts/test/fork/Liquidity_Migration.t.sol
+++ chains/evm/contracts/test/fork/Liquidity_Migration.t.sol
@@ -16,16 +16,21 @@ contract LiquidityMigration is MCMSForkTest {
     address private tokenAdminRegistryAddress;
     address private rebalancerAddress;
     address private timelockAddress;
-    uint256 private ethereumForkId;
-    bytes private ethereumPayload;
-    bytes private ethereumPayload2;
-    uint256 private ethereumTransferAmount;
+    uint256 private forkId;
+    bytes[] private payloads;
+    uint256 private transferAmount;
 
     function setUp() public {
-        ethereumForkId = vm.createFork(vm.envString("ETHEREUM_RPC_URL"));
-        ethereumPayload = vm.envBytes("ETHEREUM_PAYLOAD");
-        ethereumPayload2 = vm.envBytes("ETHEREUM_PAYLOAD_2");
-        ethereumTransferAmount = vm.envUint("ETHEREUM_TRANSFER_AMOUNT");
+        forkId = vm.createFork(vm.envString("RPC_URL"));
+        
+        // Load payloads dynamically based on PAYLOAD_COUNT
+        uint256 payloadCount = vm.envUint("PAYLOAD_COUNT");
+        payloads = new bytes[](payloadCount);
+        for (uint256 i = 0; i < payloadCount; i++) {
+            payloads[i] = vm.envBytes(string.concat("PAYLOAD_", vm.toString(i + 1)));
+        }
+        
+        transferAmount = vm.envUint("TRANSFER_AMOUNT");
         oldPoolAddress = vm.envAddress("OLD_POOL_ADDRESS");
         newPoolAddress = vm.envAddress("NEW_POOL_ADDRESS");
         tokenAddress = vm.envAddress("TOKEN_ADDRESS");
@@ -35,7 +40,7 @@ contract LiquidityMigration is MCMSForkTest {
     }
 
     function testMigration() public {
-        vm.selectFork(ethereumForkId);
+        vm.selectFork(forkId);
 
         // Get the token balance for the old pool
         uint256 oldPoolBalance = IERC20(tokenAddress).balanceOf(oldPoolAddress);
@@ -43,9 +48,10 @@ contract LiquidityMigration is MCMSForkTest {
         // Get the token balance for the new pool
         uint256 newPoolBalance = IERC20(tokenAddress).balanceOf(newPoolAddress);
 
-        // Apply the liquidity migration
-        applyPayload(timelockAddress, ethereumPayload);
-        applyPayload(timelockAddress, ethereumPayload2);
+        // Apply the liquidity migration payloads
+        for (uint256 i = 0; i < payloads.length; i++) {
+            applyPayload(timelockAddress, payloads[i]);
+        }
 
         // Get the token config for the token
         TokenAdminRegistry.TokenConfig memory cfg = TokenAdminRegistry(tokenAdminRegistryAddress).getTokenConfig(tokenAddress);
@@ -58,10 +64,10 @@ contract LiquidityMigration is MCMSForkTest {
         uint256 updatedNewPoolBalance = IERC20(tokenAddress).balanceOf(newPoolAddress);
 
         // Ensure that the old pool balance has decreased by the expected amounts
-        assertEq(oldPoolBalance - updatedOldPoolBalance, ethereumTransferAmount, "TOKEN balance should have decreased by the expected amount");
+        assertEq(oldPoolBalance - updatedOldPoolBalance, transferAmount, "TOKEN balance should have decreased by the expected amount");
 
         // Ensure that the new pool balance has increased by the expected amounts
-        assertEq(updatedNewPoolBalance - newPoolBalance, ethereumTransferAmount, "TOKEN balance should have increased by the expected amount");
+        assertEq(updatedNewPoolBalance - newPoolBalance, transferAmount, "TOKEN balance should have increased by the expected amount");
 
         // Ensure that the rebalancer is set to the expected address
         assertEq(LockReleaseTokenPool(newPoolAddress).getRebalancer(), rebalancerAddress, "Rebalancer should be set to the expected address");
