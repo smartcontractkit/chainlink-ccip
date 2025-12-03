e9b1aca028f70f085df0236a2f8879f9f07c5251
Alkhara <cawley22@gmail.com>
2025-12-03 15:04:48 -0500

    fix version

--- chains/evm/contracts/test/fork/Liquidity_Migration.t.sol
+++ chains/evm/contracts/test/fork/Liquidity_Migration.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import {TokenAdminRegistry} from "../../tokenAdminRegistry/TokenAdminRegistry.sol";
 import {MCMSForkTest} from "./MCMSForkTest.t.sol";
-import {IERC20} from "@openzeppelin/contracts@4.7.3/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts@4.8.3/token/ERC20/IERC20.sol";
 
 interface LockReleaseTokenPool {
     function getRebalancer() external view returns (address);
