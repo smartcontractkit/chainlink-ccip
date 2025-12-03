391a035534f37b88205b0df7bc0a3eaf72395996
Suryansh <39276431+0xsuryansh@users.noreply.github.com>
2025-12-03 21:32:45 +0530

    Lombard Token Pool v2 flow (#1437)

---
+++ chains/evm/contracts/test/helpers/LombardTokenPoolHelper.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.24;
+
+import {LombardTokenPool} from "../../pools/Lombard/LombardTokenPool.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts@4.8.3/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract LombardTokenPoolHelper is LombardTokenPool {
+  constructor(
+    IERC20Metadata token,
+    address verifier,
+    address rmnProxy,
+    address router,
+    uint8 fallbackDecimals
+  ) LombardTokenPool(token, verifier, address(0), rmnProxy, router, fallbackDecimals) {}
+
+  function getTokenDecimals(IERC20Metadata token, uint8 fallbackDecimals) external view returns (uint8) {
+    return _getTokenDecimals(token, fallbackDecimals);
+  }
+}
