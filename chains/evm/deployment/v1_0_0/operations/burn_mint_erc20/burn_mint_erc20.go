9179dd72df8fd42d6149ba6f494ca34ebff9968d
Balamurali Gopalswami <balamurali.gopalswami@smartcontract.com>
2025-12-18 13:51:58 -0500

    fix import

--- chains/evm/deployment/v1_0_0/operations/burn_mint_erc20/burn_mint_erc20.go
+++ chains/evm/deployment/v1_0_0/operations/burn_mint_erc20/burn_mint_erc20.go
@@ -4,9 +4,9 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
-
-	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils"
+	
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/operations/contract"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils"
 	cldf_deployment "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	"github.com/smartcontractkit/chainlink-evm/gethwrappers/shared/generated/initial/burn_mint_erc20"
 )
--- chains/evm/deployment/v1_0_0/operations/factory_burn_mint_erc20/factory_burn_mint_erc20.go
+++ chains/evm/deployment/v1_0_0/operations/factory_burn_mint_erc20/factory_burn_mint_erc20.go
@@ -4,10 +4,10 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
-
-	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils"
+	
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/operations/contract"
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/latest/factory_burn_mint_erc20"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils"
 	cldf_deployment "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 )
 
