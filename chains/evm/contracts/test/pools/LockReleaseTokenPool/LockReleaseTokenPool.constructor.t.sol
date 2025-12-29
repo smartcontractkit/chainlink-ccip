6fad401c8ad4984e13b1a8cc89297f46154eef59
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2025-12-29 19:20:01 +0530

    fix
    add missing test

--- chains/evm/contracts/test/pools/LockReleaseTokenPool/LockReleaseTokenPool.constructor.t.sol
+++ chains/evm/contracts/test/pools/LockReleaseTokenPool/LockReleaseTokenPool.constructor.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 import {ERC20LockBox} from "../../../pools/ERC20LockBox.sol";
 import {LockReleaseTokenPool} from "../../../pools/LockReleaseTokenPool.sol";
+import {TokenPool} from "../../../pools/TokenPool.sol";
 import {BaseTest} from "../../BaseTest.t.sol";
 import {BurnMintERC20} from "@chainlink/contracts/src/v0.8/shared/token/ERC20/BurnMintERC20.sol";
 
@@ -17,5 +18,22 @@ contract LockReleaseTokenPool_constructor is BaseTest {
 
     assertEq(address(pool.getToken()), address(token));
     assertEq(pool.typeAndVersion(), "LockReleaseTokenPool 1.7.0-dev");
+  }
+
+  function test_constructor_RevertWhen_InvalidToken() public {
+    BurnMintERC20 token = new BurnMintERC20("T", "T", 18, 0, 0);
+    ERC20LockBox lockBox = new ERC20LockBox(address(token));
+
+    BurnMintERC20 invalidToken = new BurnMintERC20("IT", "IT", 18, 0, 0);
+
+    vm.expectRevert(abi.encodeWithSelector(TokenPool.InvalidToken.selector, address(invalidToken)));
+    new LockReleaseTokenPool(
+      invalidToken,
+      DEFAULT_TOKEN_DECIMALS,
+      address(0),
+      address(s_mockRMNRemote),
+      address(s_sourceRouter),
+      address(lockBox)
+    );
   }
 }
