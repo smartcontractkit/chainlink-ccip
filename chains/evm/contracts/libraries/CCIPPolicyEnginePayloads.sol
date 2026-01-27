1ef8231040dc570f493808483cbd3ef17fe2d1c5
Matt Yang <matt.yang@smartcontract.com>
2026-01-27 15:53:13 -0800

    wording tweak

--- chains/evm/contracts/libraries/CCIPPolicyEnginePayloads.sol
+++ chains/evm/contracts/libraries/CCIPPolicyEnginePayloads.sol
@@ -18,7 +18,7 @@ library CCIPPolicyEnginePayloads {
     /// @dev NOTE: This is the user-entered transfer amount, excluding any source pool fees.
     /// It may not reflect the actual tokens sent to the destination.
     uint256 amount; //                        The amount of tokens, denominated in the source token's decimals.
-    address localToken; //                    The token address on this chain.
+    address localToken; //                    The token address on source chain.
     bytes tokenArgs; //                       Additional token-specific arguments.
   }
 
@@ -28,7 +28,7 @@ library CCIPPolicyEnginePayloads {
     uint64 remoteChainSelector; //          │ The source chain selector.
     address receiver; // ───────────────────╯ The recipient of the tokens on this chain.
     uint256 amount; //                        The amount of tokens, denominated in the source token's decimals.
-    address localToken; //                    The token address on this chain.
+    address localToken; //                    The token address on dest chain.
     bytes sourcePoolAddress; //               The source pool address, abi-encoded.
     bytes sourcePoolData; //                  The data received from the source pool.
     bytes offchainTokenData; //               The offchain attestation data.
