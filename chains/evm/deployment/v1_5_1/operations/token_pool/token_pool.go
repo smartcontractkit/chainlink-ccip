0fb1caccbabc5b9c91517800766446aa627c8026
chris-de-leon-cll <147140544+chris-de-leon-cll@users.noreply.github.com>
2026-01-28 21:56:45 -0800

    feat: implement ConfigureTokenForTransfersSequence for the EVM tokens API

--- chains/evm/deployment/v1_5_1/operations/token_pool/token_pool.go
+++ chains/evm/deployment/v1_5_1/operations/token_pool/token_pool.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Masterminds/semver/v3"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v1_5_1/token_pool"
 
@@ -16,6 +17,22 @@ var (
 	Version      *semver.Version              = semver.MustParse("1.5.1")
 )
 
+type ApplyChainUpdatesArgs struct {
+	RemoteChainSelectorsToRemove []uint64
+	ChainsToAdd                  []token_pool.TokenPoolChainUpdate
+}
+
+type SetRateLimiterConfigArgs struct {
+	RemoteChainSelector     uint64
+	OutboundRateLimitConfig token_pool.RateLimiterConfig
+	InboundRateLimitConfig  token_pool.RateLimiterConfig
+}
+
+type AddRemotePoolArgs struct {
+	RemoteChainSelector uint64
+	RemotePoolAddress   []byte
+}
+
 var GetToken = contract.NewRead(contract.ReadParams[struct{}, common.Address, *token_pool.TokenPool]{
 	Name:         "token-pool:get-token",
 	Version:      Version,
@@ -24,5 +41,47 @@ var GetToken = contract.NewRead(contract.ReadParams[struct{}, common.Address, *t
 	NewContract:  token_pool.NewTokenPool,
 	CallContract: func(tp *token_pool.TokenPool, opts *bind.CallOpts, args struct{}) (common.Address, error) {
 		return tp.GetToken(opts)
+	},
+})
+
+var ApplyChainUpdates = contract.NewWrite(contract.WriteParams[ApplyChainUpdatesArgs, *token_pool.TokenPool]{
+	Name:            "token-pool:apply-chain-updates",
+	Version:         Version,
+	Description:     "Applies chain updates to the TokenPool 1.5.1 contract",
+	ContractType:    ContractType,
+	ContractABI:     token_pool.TokenPoolABI,
+	NewContract:     token_pool.NewTokenPool,
+	IsAllowedCaller: contract.OnlyOwner[*token_pool.TokenPool, ApplyChainUpdatesArgs],
+	Validate:        func(args ApplyChainUpdatesArgs) error { return nil },
+	CallContract: func(tp *token_pool.TokenPool, opts *bind.TransactOpts, args ApplyChainUpdatesArgs) (*types.Transaction, error) {
+		return tp.ApplyChainUpdates(opts, args.RemoteChainSelectorsToRemove, args.ChainsToAdd)
+	},
+})
+
+var SetRateLimiterConfig = contract.NewWrite(contract.WriteParams[SetRateLimiterConfigArgs, *token_pool.TokenPool]{
+	Name:            "token-pool:set-rate-limit-config",
+	Version:         Version,
+	Description:     "Sets the rate limiter configuration for a remote chain on the TokenPool 1.5.1 contract",
+	ContractType:    ContractType,
+	ContractABI:     token_pool.TokenPoolABI,
+	NewContract:     token_pool.NewTokenPool,
+	IsAllowedCaller: contract.OnlyOwner[*token_pool.TokenPool, SetRateLimiterConfigArgs],
+	Validate:        func(args SetRateLimiterConfigArgs) error { return nil },
+	CallContract: func(tp *token_pool.TokenPool, opts *bind.TransactOpts, args SetRateLimiterConfigArgs) (*types.Transaction, error) {
+		return tp.SetChainRateLimiterConfig(opts, args.RemoteChainSelector, args.OutboundRateLimitConfig, args.InboundRateLimitConfig)
+	},
+})
+
+var AddRemotePool = contract.NewWrite(contract.WriteParams[AddRemotePoolArgs, *token_pool.TokenPool]{
+	Name:            "token-pool:add-remote-pool",
+	Version:         Version,
+	Description:     "Adds a remote pool for a given chain selector on the TokenPool 1.5.1 contract",
+	ContractType:    ContractType,
+	ContractABI:     token_pool.TokenPoolABI,
+	NewContract:     token_pool.NewTokenPool,
+	IsAllowedCaller: contract.OnlyOwner[*token_pool.TokenPool, AddRemotePoolArgs],
+	Validate:        func(args AddRemotePoolArgs) error { return nil },
+	CallContract: func(tp *token_pool.TokenPool, opts *bind.TransactOpts, args AddRemotePoolArgs) (*types.Transaction, error) {
+		return tp.AddRemotePool(opts, args.RemoteChainSelector, args.RemotePoolAddress)
 	},
 })
--- deployment/tokens/configure_tokens_for_transfers.go
+++ deployment/tokens/configure_tokens_for_transfers.go
@@ -3,6 +3,7 @@ package tokens
 import (
 	"fmt"
 
+	"github.com/Masterminds/semver/v3"
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/changesets"
 	datastore_utils "github.com/smartcontractkit/chainlink-ccip/deployment/utils/datastore"
@@ -33,6 +34,8 @@ type TokenTransferConfig struct {
 
 // ConfigureTokensForTransfersConfig is the configuration for the ConfigureTokensForTransfers changeset.
 type ConfigureTokensForTransfersConfig struct {
+	// ChainAdapterVersion specifies the version of the chain adapter to use for this chain.
+	ChainAdapterVersion *semver.Version `yaml:"chain-adapter-version" json:"chainAdapterVersion"`
 	// Tokens specifies the tokens to configure for cross-chain transfers.
 	Tokens []TokenTransferConfig
 	// MCMS configures the resulting proposal.
@@ -70,7 +73,7 @@ func makeApply(tokenRegistry *TokenAdapterRegistry, mcmsRegistry *changesets.MCM
 			if err != nil {
 				return cldf.ChangesetOutput{}, fmt.Errorf("failed to get chain family for chain selector %d: %w", token.ChainSelector, err)
 			}
-			adapter, ok := tokenRegistry.GetTokenAdapter(family, tokenPool.Version)
+			adapter, ok := tokenRegistry.GetTokenAdapter(family, cfg.ChainAdapterVersion)
 			if !ok {
 				return cldf.ChangesetOutput{}, fmt.Errorf("no token adapter registered for chain family '%s' and token pool version '%s'", family, tokenPool.Version)
 			}
