f74390e5a981cb26b8b21ad109413008ad0ffbe4
Mateusz Sekara <mateusz.sekara@gmail.com>
2026-02-02 18:33:33 +0100

    CCIP-9312 Lombard chain deployment changeset (#1591)

---
+++ deployment/v1_7_0/adapters/lombard.go
@@ -0,0 +1,79 @@
+package adapters
+
+import (
+	"fmt"
+	"sync"
+
+	cldf_chain "github.com/smartcontractkit/chainlink-deployments-framework/chain"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	cldf_ops "github.com/smartcontractkit/chainlink-deployments-framework/operations"
+
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/sequences"
+)
+
+type DeployLombardInput struct {
+	// ChainSelector is the selector for the chain being deployed.
+	ChainSelector uint64
+	// Bridge is the address of the Bridge contract provided by Lombard
+	Bridge string
+	// Token is the address of the token to be used in the LombardTokenPool.
+	Token string
+	// DeployerContract is a contract that can be used to deploy other contracts.
+	// i.e. A CREATE2Factory contract on Ethereum can enable consistent deployments.
+	DeployerContract string
+	// StorageLocations is the set of storage locations for the LombardVerifier contract.
+	StorageLocations []string
+	// FeeAggregator is the address to which fees are withdrawn.
+	FeeAggregator string
+	// RateLimitAdmin is the address allowed to update token pool rate limits.
+	RateLimitAdmin string
+}
+
+// DeployLombardChainDeps are the dependencies for the DeployLombardChain sequence.
+type DeployLombardChainDeps struct {
+	// BlockChains are the chains in the environment.
+	BlockChains cldf_chain.BlockChains
+	// DataStore defines all addresses in the environment.
+	DataStore datastore.DataStore
+}
+
+// LombardChain is a configurable Lombard chain.
+type LombardChain interface {
+	// DeployLombardChain deploys the Lombard contracts on the chain.
+	DeployLombardChain() *cldf_ops.Sequence[DeployLombardInput, sequences.OnChainOutput, DeployLombardChainDeps]
+	// AddressRefToBytes converts an AddressRef to a byte slice representing the address.
+	// Each chain family has their own way of serializing addresses from strings and needs to specify this logic.
+	AddressRefToBytes(ref datastore.AddressRef) ([]byte, error)
+}
+
+// LombardChainRegistry maintains a registry of Lombard chains.
+type LombardChainRegistry struct {
+	mu sync.Mutex
+	m  map[string]LombardChain
+}
+
+// NewLombardChainRegistry creates a new Lombard chain registry.
+func NewLombardChainRegistry() *LombardChainRegistry {
+	return &LombardChainRegistry{
+		m: make(map[string]LombardChain),
+	}
+}
+
+// RegisterLombardChain allows Lombard chains to register their changeset logic.
+func (r *LombardChainRegistry) RegisterLombardChain(chainFamily string, adapter LombardChain) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if _, exists := r.m[chainFamily]; exists {
+		panic(fmt.Errorf("LombardChain '%s' already registered", chainFamily))
+	}
+	r.m[chainFamily] = adapter
+}
+
+// GetLombardChain retrieves a registered Lombard chain for the given chain family.
+// The boolean return value indicates whether a Lombard chain was found.
+func (r *LombardChainRegistry) GetLombardChain(chainFamily string) (LombardChain, bool) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	adapter, ok := r.m[chainFamily]
+	return adapter, ok
+}
---
+++ deployment/v1_7_0/changesets/deploy_lombard_chains.go
@@ -0,0 +1,133 @@
+package changesets
+
+import (
+	"fmt"
+
+	chain_selectors "github.com/smartcontractkit/chain-selectors"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	cldf_ops "github.com/smartcontractkit/chainlink-deployments-framework/operations"
+	mcms_types "github.com/smartcontractkit/mcms/types"
+
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/changesets"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/mcms"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/v1_7_0/adapters"
+)
+
+type LombardChainConfig struct {
+	// Bridge is the address of the Bridge contract provided by Lombard
+	Bridge string
+	// Token is the address of the token to be used in the LombardTokenPool.
+	Token string
+	// DeployerContract is a contract that can be used to deploy other contracts.
+	// i.e. A CREATE2Factory contract on Ethereum can enable consistent deployments.
+	DeployerContract string
+	// StorageLocations is the set of storage locations for the LombardVerifier contract.
+	StorageLocations []string
+	// FeeAggregator is the address to which fees are withdrawn.
+	FeeAggregator string
+	// RateLimitAdmin is the address allowed to update token pool rate limits.
+	RateLimitAdmin string
+	// RemoteChains is the set of remote chains to configure.
+	RemoteChains map[uint64]struct{}
+}
+
+type DeployLombardChainsConfig struct {
+	Chains map[uint64]LombardChainConfig
+	// MCMS configures the resulting proposal.
+	MCMS *mcms.Input
+}
+
+func DeployLombardChains(lombardChainRegistry *adapters.LombardChainRegistry, mcmsRegistry *changesets.MCMSReaderRegistry) cldf.ChangeSetV2[DeployLombardChainsConfig] {
+	return cldf.CreateChangeSet(
+		makeApplyDeployLombardChains(lombardChainRegistry, mcmsRegistry),
+		makeVerifyDeployLombardChains(lombardChainRegistry, mcmsRegistry),
+	)
+}
+
+func makeVerifyDeployLombardChains(_ *adapters.LombardChainRegistry, _ *changesets.MCMSReaderRegistry) func(cldf.Environment, DeployLombardChainsConfig) error {
+	return func(e cldf.Environment, cfg DeployLombardChainsConfig) error {
+		if cfg.MCMS != nil {
+			err := cfg.MCMS.Validate()
+			if err != nil {
+				return fmt.Errorf("failed to validate MCMS input: %w", err)
+			}
+		}
+
+		for chainSel, chainCfg := range cfg.Chains {
+			if _, err := chain_selectors.GetSelectorFamily(chainSel); err != nil {
+				return err
+			}
+			for remoteChainSelector := range chainCfg.RemoteChains {
+				if _, err := chain_selectors.GetSelectorFamily(remoteChainSelector); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	}
+}
+
+func makeApplyDeployLombardChains(lombardChainRegistry *adapters.LombardChainRegistry, mcmsRegistry *changesets.MCMSReaderRegistry) func(cldf.Environment, DeployLombardChainsConfig) (cldf.ChangesetOutput, error) {
+	return func(e cldf.Environment, cfg DeployLombardChainsConfig) (cldf.ChangesetOutput, error) {
+		batchOps := make([]mcms_types.BatchOperation, 0)
+		reports := make([]cldf_ops.Report[any, any], 0)
+
+		adaptersByChain := make(map[uint64]adapters.LombardChain)
+		for chainSel := range cfg.Chains {
+			family, err := chain_selectors.GetSelectorFamily(chainSel)
+			if err != nil {
+				return cldf.ChangesetOutput{}, fmt.Errorf("failed to get chain family for chain selector %d: %w", chainSel, err)
+			}
+			adapter, ok := lombardChainRegistry.GetLombardChain(family)
+			if !ok {
+				return cldf.ChangesetOutput{}, fmt.Errorf("no CCTP adapter registered for chain family '%s'", family)
+			}
+			adaptersByChain[chainSel] = adapter
+		}
+
+		// Deploy across all chains.
+		newDS := datastore.NewMemoryDataStore()
+		for chainSel, chainCfg := range cfg.Chains {
+			dep := adapters.DeployLombardChainDeps{
+				BlockChains: e.BlockChains,
+				DataStore:   e.DataStore,
+			}
+
+			in := adapters.DeployLombardInput{
+				ChainSelector:    chainSel,
+				Bridge:           chainCfg.Bridge,
+				Token:            chainCfg.Token,
+				DeployerContract: chainCfg.DeployerContract,
+				StorageLocations: chainCfg.StorageLocations,
+				FeeAggregator:    chainCfg.FeeAggregator,
+				RateLimitAdmin:   chainCfg.RateLimitAdmin,
+			}
+			deployLombardChainReport, err := cldf_ops.ExecuteSequence(
+				e.OperationsBundle, adaptersByChain[chainSel].DeployLombardChain(), dep, in)
+			if err != nil {
+				return cldf.ChangesetOutput{}, fmt.Errorf("failed to deploy Lombard on chain with selector %d: %w", chainSel, err)
+			}
+
+			batchOps = append(batchOps, deployLombardChainReport.Output.BatchOps...)
+			reports = append(reports, deployLombardChainReport.ExecutionReports...)
+			for _, r := range deployLombardChainReport.Output.Addresses {
+				if err := newDS.Addresses().Add(r); err != nil {
+					return cldf.ChangesetOutput{}, fmt.Errorf("failed to add %s %s with address %s on chain with selector %d to datastore: %w", r.Type, r.Version, r.Address, r.ChainSelector, err)
+				}
+			}
+		}
+
+		var mcmsInput mcms.Input
+		if cfg.MCMS != nil {
+			mcmsInput = *cfg.MCMS
+		}
+
+		return changesets.NewOutputBuilder(e, mcmsRegistry).
+			WithReports(reports).
+			WithBatchOps(batchOps).
+			WithDataStore(newDS).
+			Build(mcmsInput)
+	}
+}
