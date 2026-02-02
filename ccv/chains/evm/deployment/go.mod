1804cbfd70dd013b6f367e56e76adcffa8a99147
Mateusz Sekara <mateusz.sekara@gmail.com>
2026-02-02 13:37:37 +0100

    Fixes

--- ccv/chains/evm/deployment/go.mod
+++ ccv/chains/evm/deployment/go.mod
@@ -9,10 +9,10 @@ require (
 	github.com/Masterminds/semver/v3 v3.4.0
 	github.com/ethereum/go-ethereum v1.16.8
 	github.com/smartcontractkit/chain-selectors v1.0.89
-	github.com/smartcontractkit/chainlink-ccip v0.1.1-solana.0.20260202121053-dca0df46b296
-	github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm v0.0.0-20260202120047-159d5f42b664
-	github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment v0.0.0-20260202120047-159d5f42b664
-	github.com/smartcontractkit/chainlink-ccip/deployment v0.0.0-20260202120047-159d5f42b664
+	github.com/smartcontractkit/chainlink-ccip v0.1.1-solana.0.20260202122355-43871d795284
+	github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm v0.0.0-20260202122355-43871d795284
+	github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment v0.0.0-20260202122355-43871d795284
+	github.com/smartcontractkit/chainlink-ccip/deployment v0.0.0-20260202122355-43871d795284
 	github.com/smartcontractkit/chainlink-common v0.9.6-0.20260107095648-223976d2b9f1
 	github.com/smartcontractkit/chainlink-deployments-framework v0.74.2
 	github.com/smartcontractkit/chainlink-evm/gethwrappers v0.0.0-20251222115927-36a18321243c
--- ccv/chains/evm/deployment/v1_7_0/adapters/lombard_chain.go
+++ ccv/chains/evm/deployment/v1_7_0/adapters/lombard_chain.go
@@ -3,12 +3,12 @@ package adapters
 import (
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/sequences/lombard"
 	seq_core "github.com/smartcontractkit/chainlink-ccip/deployment/utils/sequences"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/v1_7_0/adapters"
-	"github.com/smartcontractkit/chainlink-deployments-framework/chain"
 	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
+
+	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/sequences/lombard"
 )
 
 var _ adapters.LombardChain = &LombardChainAdapter{}
@@ -22,6 +22,6 @@ func (c *LombardChainAdapter) AddressRefToBytes(ref datastore.AddressRef) ([]byt
 }
 
 // DeployLombardChain returns the sequence for deploying a Lombard chain.
-func (c *LombardChainAdapter) DeployLombardChain() *operations.Sequence[adapters.DeployLombardInput[string, []byte], seq_core.OnChainOutput, chain.BlockChains] {
+func (c *LombardChainAdapter) DeployLombardChain() *operations.Sequence[adapters.DeployLombardInput, seq_core.OnChainOutput, adapters.DeployLombardChainDeps] {
 	return lombard.DeployLombardChain
 }
