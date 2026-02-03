04192d7e60df92c436275111fd37e6989ff3a178
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2026-02-03 13:28:08 -0800

    Ccip-8552 import config from 1.5 and 1.6 contracts to be used for 1.7 deployment (#1626) (#1630)

---
+++ chains/evm/deployment/v1_6_0/adapters/configimport.go
@@ -0,0 +1,195 @@
+package adapters
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	cldf_chain "github.com/smartcontractkit/chainlink-deployments-framework/chain"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+	cldf_ops "github.com/smartcontractkit/chainlink-deployments-framework/operations"
+
+	evm_datastore_utils "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/datastore"
+	adapters1_5 "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_5_0/adapters"
+	tokenadminops "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_5_0/operations/token_admin_registry"
+	fqops "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_0/operations/fee_quoter"
+	offrampops "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_0/operations/offramp"
+	onrampops "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_0/operations/onramp"
+	seq1_6 "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_0/sequences"
+	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v1_2_0/router"
+	api "github.com/smartcontractkit/chainlink-ccip/deployment/deploy"
+	datastore_utils "github.com/smartcontractkit/chainlink-ccip/deployment/utils/datastore"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/sequences"
+)
+
+type ConfigImportAdapter struct {
+	FeeQuoter     map[uint64]common.Address
+	OnRamp        map[uint64]common.Address
+	OffRamp       map[uint64]common.Address
+	Router        map[uint64]common.Address
+	TokenAdminReg map[uint64]common.Address
+}
+
+func (ci *ConfigImportAdapter) InitializeAdapter(e cldf.Environment, selectors []uint64) error {
+	ci.FeeQuoter = make(map[uint64]common.Address)
+	ci.Router = make(map[uint64]common.Address)
+	ci.TokenAdminReg = make(map[uint64]common.Address)
+	ci.OnRamp = make(map[uint64]common.Address)
+	ci.OffRamp = make(map[uint64]common.Address)
+	for _, chainSelector := range selectors {
+		fqRef, err := datastore_utils.FindAndFormatRef(e.DataStore, datastore.AddressRef{
+			Type:          datastore.ContractType(fqops.ContractType),
+			Version:       fqops.Version,
+			ChainSelector: chainSelector,
+		}, chainSelector, evm_datastore_utils.ToEVMAddress)
+		if err != nil {
+			return fmt.Errorf("failed to find fee quoter contract ref for chain %d: %w", chainSelector, err)
+		}
+		ci.FeeQuoter[chainSelector] = fqRef
+		routerRef, err := datastore_utils.FindAndFormatRef(e.DataStore, datastore.AddressRef{
+			Type:          datastore.ContractType("Router"),
+			Version:       semver.MustParse("1.2.0"),
+			ChainSelector: chainSelector,
+		}, chainSelector, evm_datastore_utils.ToEVMAddress)
+		if err != nil {
+			return fmt.Errorf("failed to find router contract ref for chain %d: %w", chainSelector, err)
+		}
+		ci.Router[chainSelector] = routerRef
+		tokenAdminRegRef, err := datastore_utils.FindAndFormatRef(e.DataStore, datastore.AddressRef{
+			Type:          datastore.ContractType(tokenadminops.ContractType),
+			Version:       tokenadminops.Version,
+			ChainSelector: chainSelector,
+		}, chainSelector, evm_datastore_utils.ToEVMAddress)
+		if err != nil {
+			return fmt.Errorf("failed to find token admin registry contract ref for chain %d: %w", chainSelector, err)
+		}
+		ci.TokenAdminReg[chainSelector] = tokenAdminRegRef
+		onRampRef, err := datastore_utils.FindAndFormatRef(e.DataStore, datastore.AddressRef{
+			Type:    datastore.ContractType(onrampops.ContractType),
+			Version: semver.MustParse("1.6.0"),
+		}, chainSelector, evm_datastore_utils.ToEVMAddress)
+		if err != nil {
+			return fmt.Errorf("failed to find onramp contract ref for chain %d: %w", chainSelector, err)
+		}
+		ci.OnRamp[chainSelector] = onRampRef
+		offRampRef, err := datastore_utils.FindAndFormatRef(e.DataStore, datastore.AddressRef{
+			Type:    datastore.ContractType(offrampops.ContractType),
+			Version: semver.MustParse("1.6.0"),
+		}, chainSelector, evm_datastore_utils.ToEVMAddress)
+		if err != nil {
+			return fmt.Errorf("failed to find offramp contract ref for chain %d: %w", chainSelector, err)
+		}
+		ci.OffRamp[chainSelector] = offRampRef
+	}
+	return nil
+}
+
+func (ci *ConfigImportAdapter) SupportedTokensPerRemoteChain(e cldf.Environment, chainsel uint64) (map[uint64][]common.Address, error) {
+	chain, ok := e.BlockChains.EVMChains()[chainsel]
+	if !ok {
+		return nil, fmt.Errorf("chain with selector %d not found in environment", chainsel)
+	}
+	tokenAdminRegAddr, ok := ci.TokenAdminReg[chainsel]
+	if !ok {
+		return nil, fmt.Errorf("token admin registry address not found for chain %d", chainsel)
+	}
+	// get all supported tokens from token admin registry
+	return adapters1_5.GetSupportedTokensPerRemoteChain(tokenAdminRegAddr, chain)
+}
+
+func (ci *ConfigImportAdapter) ConnectedChains(e cldf.Environment, chainsel uint64) ([]uint64, error) {
+	chain, ok := e.BlockChains.EVMChains()[chainsel]
+	if !ok {
+		return nil, fmt.Errorf("chain with selector %d not found in environment", chainsel)
+	}
+	routerAddr, ok := ci.Router[chainsel]
+	if !ok {
+		return nil, fmt.Errorf("router address not found for chain %d", chainsel)
+	}
+	// get all offRamps from router to find connected chains
+	routerC, err := router.NewRouter(routerAddr, chain.Client)
+	if err != nil {
+		return nil, fmt.Errorf("failed to instantiate router contract at %s on chain %d: %w", routerAddr.String(), chain.Selector, err)
+	}
+	offRamps, err := routerC.GetOffRamps(&bind.CallOpts{
+		Context: e.GetContext(),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get off ramps from router at %s on chain %d: %w", routerAddr.String(), chain.Selector, err)
+	}
+	connectedChains := make([]uint64, 0)
+	for _, offRamp := range offRamps {
+		if offRamp.OffRamp == (common.Address{}) {
+			continue // skip uninitialized off-ramps
+		}
+		connectedChains = append(connectedChains, offRamp.SourceChainSelector)
+	}
+	return connectedChains, nil
+}
+
+func (ci *ConfigImportAdapter) SequenceImportConfig() *cldf_ops.Sequence[api.ImportConfigPerChainInput, sequences.OnChainOutput, cldf_chain.BlockChains] {
+	return cldf_ops.NewSequence(
+		"seq-config-import",
+		semver.MustParse("1.0.0"),
+		"Imports configuration for specified chains",
+		func(b cldf_ops.Bundle, chains cldf_chain.BlockChains, in api.ImportConfigPerChainInput) (output sequences.OnChainOutput, err error) {
+			evmChain, ok := chains.EVMChains()[in.ChainSelector]
+			if !ok {
+				return sequences.OnChainOutput{}, fmt.Errorf("chain with selector %d not found in environment", in.ChainSelector)
+			}
+			chainSelector := in.ChainSelector
+			b.Logger.Infof("Importing configuration for chain %d (%s)", chainSelector, evmChain.Name())
+			// read FQ config from onchain
+			fqAddress, ok := ci.FeeQuoter[chainSelector]
+			if !ok {
+				return sequences.OnChainOutput{}, fmt.Errorf("fee quoter address not found for chain %d", chainSelector)
+			}
+			var result sequences.OnChainOutput
+			// fetch fee quoter config
+			result, err = sequences.RunAndMergeSequence(b, chains,
+				seq1_6.FeeQuoterImportConfigSequence,
+				seq1_6.FeeQuoterImportConfigSequenceInput{
+					Address:              fqAddress,
+					ChainSelector:        chainSelector,
+					RemoteChains:         in.RemoteChains,
+					TokensPerRemoteChain: in.TokensPerRemoteChain,
+				}, result)
+			if err != nil {
+				return sequences.OnChainOutput{}, fmt.Errorf("failed to import fee quoter config on chain %d: %w", chainSelector, err)
+			}
+			// fetch onramp config
+			onRampAddress, ok := ci.OnRamp[chainSelector]
+			if !ok {
+				return sequences.OnChainOutput{}, fmt.Errorf("onramp address not found for chain %d", chainSelector)
+			}
+			result, err = sequences.RunAndMergeSequence(b, chains,
+				seq1_6.OnRampImportConfigSequence,
+				seq1_6.OnRampImportConfigSequenceInput{
+					Address:       onRampAddress,
+					ChainSelector: chainSelector,
+					RemoteChains:  in.RemoteChains,
+				}, result)
+			if err != nil {
+				return sequences.OnChainOutput{}, fmt.Errorf("failed to import onramp config on chain %d: %w", chainSelector, err)
+			}
+			// fetch offramp config
+			offRampAddress, ok := ci.OffRamp[chainSelector]
+			if !ok {
+				return sequences.OnChainOutput{}, fmt.Errorf("offramp address not found for chain %d", chainSelector)
+			}
+			result, err = sequences.RunAndMergeSequence(b, chains,
+				seq1_6.OffRampImportConfigSequence,
+				seq1_6.OffRampImportConfigSequenceInput{
+					Address:       offRampAddress,
+					ChainSelector: chainSelector,
+					RemoteChains:  in.RemoteChains,
+				}, result)
+			if err != nil {
+				return sequences.OnChainOutput{}, fmt.Errorf("failed to import offramp config on chain %d: %w", chainSelector, err)
+			}
+			return result, nil
+		},
+	)
+}
--- chains/evm/deployment/v1_6_0/sequences/fee_quoter.go
+++ chains/evm/deployment/v1_6_0/sequences/fee_quoter.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+
+	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v1_6_3/fee_quoter"
 
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/operations/contract"
 	fqops "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_0/operations/fee_quoter"
-	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v1_6_3/fee_quoter"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/sequences"
+
 	cldf_chain "github.com/smartcontractkit/chainlink-deployments-framework/chain"
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
 	mcms_types "github.com/smartcontractkit/mcms/types"
@@ -33,6 +36,18 @@ type FeeQuoterApplyTokenTransferFeeConfigUpdatesSequenceInput struct {
 	UpdatesByChain fqops.ApplyTokenTransferFeeConfigUpdatesInput
 }
 
+type FeeQuoterImportConfigSequenceInput struct {
+	Address              common.Address
+	ChainSelector        uint64
+	TokensPerRemoteChain map[uint64][]common.Address
+	RemoteChains         []uint64
+}
+
+type FeeQuoterImportConfigSequenceOutput struct {
+	DestChainCfg         fee_quoter.FeeQuoterDestChainConfig
+	TokenTransferFeeCfgs map[common.Address]fee_quoter.FeeQuoterTokenTransferFeeConfig
+}
+
 var (
 	FeeQuoterApplyDestChainConfigUpdatesSequence = operations.NewSequence(
 		"FeeQuoterApplyDestChainConfigUpdatesSequence",
@@ -44,6 +59,7 @@ var (
 			if !ok {
 				return sequences.OnChainOutput{}, fmt.Errorf("chain with selector %d not defined", input.ChainSelector)
 			}
+
 			report, err := operations.ExecuteOperation(b, fqops.FeeQuoterApplyDestChainConfigUpdates, chain, contract.FunctionInput[[]fee_quoter.FeeQuoterDestChainConfigArgs]{
 				ChainSelector: chain.Selector,
 				Address:       input.Address,
@@ -72,6 +88,7 @@ var (
 			if !ok {
 				return sequences.OnChainOutput{}, fmt.Errorf("chain with selector %d not defined", input.ChainSelector)
 			}
+
 			report, err := operations.ExecuteOperation(b, fqops.FeeQuoterUpdatePrices, chain, contract.FunctionInput[fee_quoter.InternalPriceUpdates]{
 				ChainSelector: chain.Selector,
 				Address:       input.Address,
@@ -100,6 +117,7 @@ var (
 			if !ok {
 				return sequences.OnChainOutput{}, fmt.Errorf("chain with selector %d not defined", input.ChainSelector)
 			}
+
 			report, err := operations.ExecuteOperation(b, fqops.FeeQuoterApplyTokenTransferFeeConfigUpdates, chain, contract.FunctionInput[fqops.ApplyTokenTransferFeeConfigUpdatesInput]{
 				ChainSelector: chain.Selector,
 				Address:       input.Address,
@@ -115,6 +133,80 @@ var (
 			}
 			return sequences.OnChainOutput{
 				BatchOps: []mcms_types.BatchOperation{batch},
+			}, nil
+		})
+
+	FeeQuoterImportConfigSequence = operations.NewSequence(
+		"FeeQuoterImportConfigSequence",
+		semver.MustParse("1.6.0"),
+		"Imports FeeQuoter configuration from on-chain contracts across multiple EVM chains",
+		func(b operations.Bundle, chains cldf_chain.BlockChains, in FeeQuoterImportConfigSequenceInput) (sequences.OnChainOutput, error) {
+			var contractMetadata []datastore.ContractMetadata
+			evmChain, ok := chains.EVMChains()[in.ChainSelector]
+			if !ok {
+				return sequences.OnChainOutput{}, fmt.Errorf("chain with selector %d not found in environment", in.ChainSelector)
+			}
+			fqAddress := in.Address
+			chainSelector := in.ChainSelector
+			b.Logger.Infof("Importing configuration for FeeQuoter %s on chain %d (%s)", fqAddress.Hex(), chainSelector, evmChain.Name())
+			fqOutput := make(map[uint64]FeeQuoterImportConfigSequenceOutput)
+			destChainConfigs := make(map[uint64]fee_quoter.FeeQuoterDestChainConfig)
+			for _, remoteChain := range in.RemoteChains {
+				opsOutput, err := operations.ExecuteOperation(b, fqops.GetDestChainConfig, evmChain, contract.FunctionInput[uint64]{
+					Address:       fqAddress,
+					ChainSelector: chainSelector,
+					Args:          remoteChain,
+				})
+				if err != nil {
+					return sequences.OnChainOutput{}, fmt.Errorf("failed to get dest chain config for "+
+						"remote chain %d from feequoter %s on chain %d: %w",
+						remoteChain, fqAddress.Hex(), chainSelector, err)
+				}
+				destChainConfigs[remoteChain] = opsOutput.Output
+			}
+
+			tokenTransferFeeCfgsPerChain := make(map[uint64]map[common.Address]fee_quoter.FeeQuoterTokenTransferFeeConfig)
+
+			for remoteChain, tokens := range in.TokensPerRemoteChain {
+				tokenTransferFeeCfgs := make(map[common.Address]fee_quoter.FeeQuoterTokenTransferFeeConfig)
+				for _, token := range tokens {
+					opsOutput, err := operations.ExecuteOperation(b, fqops.GetTokenTransferFeeConfig, evmChain,
+						contract.FunctionInput[fqops.GetTokenTransferFeeConfigInput]{
+							Address:       fqAddress,
+							ChainSelector: chainSelector,
+							Args: fqops.GetTokenTransferFeeConfigInput{
+								Token:             token,
+								DestChainSelector: remoteChain,
+							},
+						})
+					if err != nil {
+						return sequences.OnChainOutput{}, fmt.Errorf("failed to get token transfer fee config for "+
+							"token %s to remote chain %d from feequoter %s on chain %d: %w",
+							token.Hex(), remoteChain, fqAddress.Hex(), chainSelector, err)
+					}
+					if opsOutput.Output.IsEnabled {
+						tokenTransferFeeCfgs[token] = opsOutput.Output
+					}
+				}
+				tokenTransferFeeCfgsPerChain[remoteChain] = tokenTransferFeeCfgs
+			}
+			for remoteChain, destCfg := range destChainConfigs {
+				fqOutput[remoteChain] = FeeQuoterImportConfigSequenceOutput{
+					DestChainCfg:         destCfg,
+					TokenTransferFeeCfgs: tokenTransferFeeCfgsPerChain[remoteChain],
+				}
+			}
+			contractMetadata = []datastore.ContractMetadata{
+				{
+					Address:       fqAddress.Hex(),
+					ChainSelector: chainSelector,
+					Metadata:      fqOutput,
+				},
+			}
+			return sequences.OnChainOutput{
+				Metadata: sequences.Metadata{
+					Contracts: contractMetadata,
+				},
 			}, nil
 		})
 )
