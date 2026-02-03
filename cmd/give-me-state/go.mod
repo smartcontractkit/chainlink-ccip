4780834e8c6a16cc23f3e79203805ecddca6a6c7
crispymangoes <ryan.buxman@smartcontract.com>
2026-02-03 13:41:11 -0800

    fix solana pda issues

--- cmd/give-me-state/go.mod
+++ cmd/give-me-state/go.mod
@@ -13,6 +13,7 @@ require (
 replace github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm => ../../ccv/chains/evm
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/bits-and-blooms/bitset v1.20.0 // indirect
@@ -20,17 +21,28 @@ require (
 	github.com/consensys/gnark-crypto v0.18.0 // indirect
 	github.com/crate-crypto/go-eth-kzg v1.4.0 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/ethereum/c-kzg-4844/v2 v2.1.3 // indirect
 	github.com/ethereum/go-verkle v0.2.2 // indirect
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gagliardetto/solana-go v1.14.0 // indirect
+	github.com/gagliardetto/treeout v0.1.4 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/holiman/uint256 v1.3.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mostynb/zstdpool-freelist v0.0.0-20201229113212-927304c0c3b1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
@@ -38,6 +50,7 @@ require (
 	github.com/supranational/blst v0.3.16-0.20250831170142-f48500c1fdbe // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
+	go.mongodb.org/mongo-driver v1.12.2 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
--- cmd/give-me-state/views/solana/pda.go
+++ cmd/give-me-state/views/solana/pda.go
@@ -1,39 +1,17 @@
 package solana
 
 import (
-	"crypto/sha256"
 	"encoding/binary"
-	"errors"
 
-	"github.com/mr-tron/base58"
+	"github.com/gagliardetto/solana-go"
 )
 
-// PDA derivation constants
-var programDerivedAddressSuffix = []byte("ProgramDerivedAddress")
-
-// PublicKey is a 32-byte Solana public key
-type PublicKey [32]byte
-
-func (pk PublicKey) String() string {
-	return base58.Encode(pk[:])
-}
-
-func (pk PublicKey) Bytes() []byte {
-	return pk[:]
-}
+// Re-export solana.PublicKey for convenience
+type PublicKey = solana.PublicKey
 
 // PublicKeyFromBase58 decodes a base58 string to a PublicKey
 func PublicKeyFromBase58(s string) (PublicKey, error) {
-	var pk PublicKey
-	decoded, err := base58.Decode(s)
-	if err != nil {
-		return pk, err
-	}
-	if len(decoded) != 32 {
-		return pk, errors.New("invalid public key length")
-	}
-	copy(pk[:], decoded)
-	return pk, nil
+	return solana.PublicKeyFromBase58(s)
 }
 
 // Uint64ToLE converts uint64 to little-endian bytes
@@ -43,69 +21,9 @@ func Uint64ToLE(n uint64) []byte {
 	return b
 }
 
-// FindProgramAddress finds a valid PDA for the given seeds and program
-// This is a simplified implementation that tries bumps from 255 down to 0
+// FindProgramAddress wraps the solana-go SDK's FindProgramAddress
 func FindProgramAddress(seeds [][]byte, programID PublicKey) (PublicKey, uint8, error) {
-	for bump := uint8(255); bump > 0; bump-- {
-		pda, err := createProgramAddress(seeds, bump, programID)
-		if err == nil {
-			return pda, bump, nil
-		}
-	}
-	return PublicKey{}, 0, errors.New("unable to find valid PDA")
-}
-
-// createProgramAddress attempts to create a PDA with the given bump
-func createProgramAddress(seeds [][]byte, bump uint8, programID PublicKey) (PublicKey, error) {
-	// Build the data to hash: seeds + [bump] + programID + "ProgramDerivedAddress"
-	var data []byte
-	for _, seed := range seeds {
-		if len(seed) > 32 {
-			return PublicKey{}, errors.New("seed too long")
-		}
-		data = append(data, seed...)
-	}
-	data = append(data, bump)
-	data = append(data, programID[:]...)
-	data = append(data, programDerivedAddressSuffix...)
-
-	// SHA256 hash
-	hash := sha256.Sum256(data)
-
-	// Check if the result is NOT on the ed25519 curve
-	// A point is on curve if it's a valid ed25519 public key
-	// For simplicity, we check if the high bit indicates it might be on curve
-	// This is a simplified check - full check requires ed25519 operations
-	if isOnCurve(hash[:]) {
-		return PublicKey{}, errors.New("point is on curve")
-	}
-
-	var pda PublicKey
-	copy(pda[:], hash[:])
-	return pda, nil
-}
-
-// isOnCurve checks if a 32-byte value could be on the ed25519 curve
-// This is a simplified heuristic - the actual check is more complex
-// but this works for most cases in practice
-func isOnCurve(b []byte) bool {
-	// The canonical check requires trying to decompress the point
-	// For our purposes, we use the fact that most random hashes are NOT on curve
-	// and Solana's implementation tries bumps starting from 255
-	// Almost all valid PDAs are found with high bump values (254, 255)
-
-	// A very rough heuristic: check if the bytes "look like" they could be
-	// a compressed ed25519 point. In practice, ~50% of random 32-byte values
-	// are NOT valid ed25519 points.
-
-	// For better compatibility, we'll use the fact that if the last byte
-	// has the high bit set, it's more likely to be off-curve
-	// This is not perfect but works for most CCIP PDAs
-
-	// Actually, let's just return false most of the time
-	// The Solana runtime would reject invalid PDAs anyway
-	// and most config PDAs use bump=255 or 254
-	return false
+	return solana.FindProgramAddress(seeds, programID)
 }
 
 // =====================
