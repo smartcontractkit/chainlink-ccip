174d43549e0439857f2f1e3e56907a661c250976
Anindita Ghosh <88458927+AnieeG@users.noreply.github.com>
2026-02-03 12:39:09 -0800

    Ccip-8552 import config from 1.5 and 1.6 contracts to be used for 1.7 deployment (#1626)
    * updates
    
    * updates
    
    * more updates
    
    * initial config import adapter
    
    * fix build error
    
    * review comments

--- chains/evm/deployment/v1_6_0/sequences/onramp.go
+++ chains/evm/deployment/v1_6_0/sequences/onramp.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
 
-	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/operations/contract"
-	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/sequences"
 	cldf_chain "github.com/smartcontractkit/chainlink-deployments-framework/chain"
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
 	mcms_types "github.com/smartcontractkit/mcms/types"
+
+	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/operations/contract"
+	"github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v1_6_0/onramp"
+	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/sequences"
 
 	onrampops "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_0/operations/onramp"
 )
@@ -19,6 +22,18 @@ type OnRampApplyDestChainConfigUpdatesSequenceInput struct {
 	Address        common.Address
 	ChainSelector  uint64
 	UpdatesByChain []onrampops.DestChainConfigArgs
+}
+
+type OnRampImportConfigSequenceInput struct {
+	Address       common.Address
+	ChainSelector uint64
+	RemoteChains  []uint64
+}
+
+type OnRampImportConfigSequenceOutput struct {
+	DestChainCfgs map[uint64]onramp.GetDestChainConfig
+	StaticConfig  onramp.OnRampStaticConfig
+	DynamicConfig onramp.OnRampDynamicConfig
 }
 
 var (
@@ -32,11 +47,11 @@ var (
 			if !ok {
 				return sequences.OnChainOutput{}, fmt.Errorf("chain with selector %d not defined", input.ChainSelector)
 			}
-		report, err := operations.ExecuteOperation(b, onrampops.ApplyDestChainConfigUpdates, chain, contract.FunctionInput[[]onrampops.DestChainConfigArgs]{
-			ChainSelector: chain.Selector,
-			Address:       input.Address,
-			Args:          input.UpdatesByChain,
-		})
+			report, err := operations.ExecuteOperation(b, onrampops.ApplyDestChainConfigUpdates, chain, contract.FunctionInput[[]onrampops.DestChainConfigArgs]{
+				ChainSelector: chain.Selector,
+				Address:       input.Address,
+				Args:          input.UpdatesByChain,
+			})
 			if err != nil {
 				return sequences.OnChainOutput{}, fmt.Errorf("failed to execute OnRampApplyDestChainConfigUpdatesOp on %s: %w", chain, err)
 			}
@@ -47,6 +62,61 @@ var (
 			}
 			return sequences.OnChainOutput{
 				BatchOps: []mcms_types.BatchOperation{batch},
+			}, nil
+		})
+
+	OnRampImportConfigSequence = operations.NewSequence(
+		"OnRampImportConfigSequence",
+		semver.MustParse("1.6.0"),
+		"Imports OnRamp configuration from TokenAdminRegistry and FeeQuoter contracts into OnRamp contracts on multiple EVM chains",
+		func(b operations.Bundle, chains cldf_chain.BlockChains, input OnRampImportConfigSequenceInput) (sequences.OnChainOutput, error) {
+			chain, ok := chains.EVMChains()[input.ChainSelector]
+			if !ok {
+				return sequences.OnChainOutput{}, fmt.Errorf("chain with selector %d not defined", input.ChainSelector)
+			}
+			onRampDestConfigs := make(map[uint64]onramp.GetDestChainConfig)
+			for _, remoteChain := range input.RemoteChains {
+				report, err := operations.ExecuteOperation(b, onrampops.GetDestChainConfig, chain, contract.FunctionInput[uint64]{
+					ChainSelector: chain.Selector,
+					Address:       input.Address,
+					Args:          remoteChain,
+				})
+				onRampDestConfigs[remoteChain] = report.Output
+				if err != nil {
+					return sequences.OnChainOutput{}, fmt.Errorf("failed to get dest chain config for chain %d from OnRamp at %s on %s: %w", remoteChain, input.Address.String(), chain, err)
+				}
+			}
+			report, err := operations.ExecuteOperation(b, onrampops.GetStaticConfig, chain, contract.FunctionInput[any]{
+				ChainSelector: chain.Selector,
+				Address:       input.Address,
+				Args:          nil,
+			})
+			if err != nil {
+				return sequences.OnChainOutput{}, fmt.Errorf("failed to get static config from OnRamp at %s on %s: %w", input.Address.String(), chain, err)
+			}
+			staticConfig := report.Output
+			out, err := operations.ExecuteOperation(b, onrampops.GetDynamicConfig, chain, contract.FunctionInput[any]{
+				ChainSelector: chain.Selector,
+				Address:       input.Address,
+				Args:          nil,
+			})
+			if err != nil {
+				return sequences.OnChainOutput{}, fmt.Errorf("failed to get dynamic config from OnRamp at %s on %s: %w", input.Address.String(), chain, err)
+			}
+			dynamicConfig := out.Output
+			contractMetadata := datastore.ContractMetadata{
+				Address:       input.Address.Hex(),
+				ChainSelector: chain.Selector,
+				Metadata: OnRampImportConfigSequenceOutput{
+					StaticConfig:  staticConfig,
+					DynamicConfig: dynamicConfig,
+					DestChainCfgs: onRampDestConfigs,
+				},
+			}
+			return sequences.OnChainOutput{
+				Metadata: sequences.Metadata{
+					Contracts: []datastore.ContractMetadata{contractMetadata},
+				},
 			}, nil
 		})
 )
