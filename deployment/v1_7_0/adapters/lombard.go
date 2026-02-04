e2356413dcfaf7649c0bb756c4ebb8fb23c2dd43
Mateusz Sekara <mateusz.sekara@gmail.com>
2026-02-04 09:39:09 +0100

    CCIP-9312 Lombard lanes configuration changeset / ops (#1624)

--- deployment/v1_7_0/adapters/lombard.go
+++ deployment/v1_7_0/adapters/lombard.go
@@ -8,6 +8,7 @@ import (
 	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
 	cldf_ops "github.com/smartcontractkit/chainlink-deployments-framework/operations"
 
+	"github.com/smartcontractkit/chainlink-ccip/deployment/tokens"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/sequences"
 )
 
@@ -18,6 +19,8 @@ type DeployLombardInput struct {
 	Bridge string
 	// Token is the address of the token to be used in the LombardTokenPool.
 	Token string
+	// TokenQualifier is the qualifier for matching token with the tokenPool during deployment
+	TokenQualifier string
 	// DeployerContract is a contract that can be used to deploy other contracts.
 	// i.e. A CREATE2Factory contract on Ethereum can enable consistent deployments.
 	DeployerContract string
@@ -37,13 +40,55 @@ type DeployLombardChainDeps struct {
 	DataStore datastore.DataStore
 }
 
+// ConfigureLombardChainForLanesInput specifies the input for the ConfigureLOmbardChainForLanes sequence.
+type ConfigureLombardChainForLanesInput struct {
+	// ChainSelector is the selector for the chain being configured.
+	ChainSelector uint64
+	// Token is the address of the Token contract.
+	Token string
+	// TokenQualifier is the qualifier for matching token with the tokenPool during deployment
+	TokenQualifier string
+	// RemoteChains is the set of remote chains to configure.
+	RemoteChains map[uint64]RemoteLombardChainConfig
+}
+
+// RemoteLombardChainConfig configures a Lombard-enabled chain for a remote counterpart.
+type RemoteLombardChainConfig struct {
+	TokenTransferFeeConfig tokens.TokenTransferFeeConfig
+	LombardChainId         uint32
+	FeeUSDCents            uint16
+	GasForVerification     uint32
+	PayloadSizeBytes       uint32
+}
+
+// ConfigureLombardChainForLanesDeps are the dependencies for the ConfigureLombardChainForLanes sequence.
+type ConfigureLombardChainForLanesDeps struct {
+	// BlockChains are the chains in the environment.
+	BlockChains cldf_chain.BlockChains
+	// DataStore defines all addresses in the environment.
+	DataStore    datastore.DataStore
+	RemoteChains map[uint64]RemoteLombardChain
+}
+
 // LombardChain is a configurable Lombard chain.
 type LombardChain interface {
+	RemoteLombardChain
 	// DeployLombardChain deploys the Lombard contracts on the chain.
 	DeployLombardChain() *cldf_ops.Sequence[DeployLombardInput, sequences.OnChainOutput, DeployLombardChainDeps]
+	ConfigureLombardChainForLanes() *cldf_ops.Sequence[ConfigureLombardChainForLanesInput, sequences.OnChainOutput, ConfigureLombardChainForLanesDeps]
 	// AddressRefToBytes converts an AddressRef to a byte slice representing the address.
 	// Each chain family has their own way of serializing addresses from strings and needs to specify this logic.
 	AddressRefToBytes(ref datastore.AddressRef) ([]byte, error)
+}
+
+// RemoteLombardChain is a connectable remote Lombard chain.
+type RemoteLombardChain interface {
+	// AllowedCallerOnDest returns the address allowed to deposit tokens for burn on the remote chain.
+	AllowedCallerOnDest(ds datastore.DataStore, chains cldf_chain.BlockChains, selector uint64) ([]byte, error)
+
+	RemoteTokenAddress(bundle cldf_ops.Bundle, ds datastore.DataStore, chains cldf_chain.BlockChains, selector uint64, tokenQualifier string) ([]byte, error)
+
+	RemoteTokenPoolAddress(ds datastore.DataStore, chains cldf_chain.BlockChains, selector uint64, tokenQualifier string) ([]byte, error)
 }
 
 // LombardChainRegistry maintains a registry of Lombard chains.
