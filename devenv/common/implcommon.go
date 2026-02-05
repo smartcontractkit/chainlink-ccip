5dfc5062d6b85b093f551d8f41212834f4168b30
Terry Tata <terry.tata@smartcontract.com>
2026-02-05 14:11:17 -0800

    tests

--- devenv/common/implcommon.go
+++ devenv/common/implcommon.go
@@ -755,16 +755,11 @@ func SetupTokensAndTokenPools(
 			// Set some rate limiters for testing - one enabled and one disabled.
 			rls := []tokensapi.RateLimiterConfig{
 				{
-					IsEnabled: false,
-				},
-				{
 					Capacity:  big.NewInt(2_000_000_000),
 					Rate:      big.NewInt(200_000_000),
 					IsEnabled: true,
 				},
 				{
-					Capacity:  big.NewInt(0),
-					Rate:      big.NewInt(0),
 					IsEnabled: false,
 				},
 			}
