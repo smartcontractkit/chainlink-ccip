e8bdd0bf11333a5c1af9c6f78fb4820b4b5e2ea9
Agustina Aldasoro <agustina.aldasoro@smartcontract.com>
2026-02-18 16:13:50 -0300

    Go bindings

--- chains/solana/scripts/anchor-go-gen.sh
+++ chains/solana/scripts/anchor-go-gen.sh
@@ -2,18 +2,22 @@
 
 set -e
 
+# Delete all .go files (anchor-go v1.0.0 generates all needed types directly)
+find ./gobindings/latest -name "*.go" -type f -delete
+
 function generate_bindings() {
   local idl_path_str="$1"
   IFS='/' read -r -a idl_path <<< "${idl_path_str}"
   IFS='.' read -r -a idl_name <<< "${idl_path[3]}"
-  anchor-go -src "${idl_path_str}" -dst ./gobindings/latest/"${idl_name}" -codec borsh
+  anchor-go -idl "${idl_path_str}" -output ./gobindings/latest/"${idl_name}" -no-go-mod
 }
 
-
+# Generate bindings for all IDLs (including vendor)
 for idl_path_str in "contracts/target/idl"/*
 do
   generate_bindings "${idl_path_str}"
 done
+
 for idl_path_str in "contracts/target/vendor"/*.json
 do
   generate_bindings "${idl_path_str}"
