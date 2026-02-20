b72dac14d4fc15dcbe8de8c352f93426cd1a7eb6
AnieeG <anindita.ghosh@smartcontract.com>
2026-02-20 14:10:31 -0800

    mcms config same as main

--- deployment/utils/mcms/mcms.go
+++ deployment/utils/mcms/mcms.go
@@ -1,15 +1,11 @@
 package mcms
 
 import (
-	"errors"
 	"fmt"
 
-	datastore_utils "github.com/smartcontractkit/chainlink-ccip/deployment/utils/datastore"
-	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
 	mcms_types "github.com/smartcontractkit/mcms/types"
 )
 
-// Input is the configuration for the MCMS proposal.
 type Input struct {
 	// OverridePreviousRoot indicates whether to override the root of the MCMS contract.
 	OverridePreviousRoot bool
@@ -20,35 +16,18 @@ type Input struct {
 	TimelockDelay mcms_types.Duration
 	// TimelockAction is the action to perform on the timelock contract (schedule, bypass, or cancel).
 	TimelockAction mcms_types.TimelockAction
-	// MCMSAddressRef is a reference to the MCMS contract address in the datastore.
-	MCMSAddressRef datastore.AddressRef
-	// TimelockAddressRef is a reference to the timelock contract address in the datastore.
-	TimelockAddressRef datastore.AddressRef
 	// Qualifier is a string used to qualify the MCMS + Timelock contract addresses.
 	Qualifier string
 	// Description is a human-readable description of the proposal.
 	Description string
 }
 
-// Validate validates the MCMS input.
+// TODO : need to put more validation here
 func (c *Input) Validate() error {
 	if c.TimelockAction != mcms_types.TimelockActionSchedule &&
 		c.TimelockAction != mcms_types.TimelockActionBypass &&
 		c.TimelockAction != mcms_types.TimelockActionCancel {
 		return fmt.Errorf("invalid timelock action: %s", c.TimelockAction)
 	}
-
-	if datastore_utils.IsAddressRefEmpty(c.MCMSAddressRef) {
-		return errors.New("mcms address ref is empty")
-	}
-
-	if datastore_utils.IsAddressRefEmpty(c.TimelockAddressRef) {
-		return errors.New("timelock address ref is empty")
-	}
-
-	if c.ValidUntil == 0 {
-		return errors.New("valid until timestamp must be set")
-	}
-
 	return nil
 }
