84941c55dd87acbd9086491621dd1fc26883e459
Blaž Hrastnik <blaz@mxxn.io>
2026-02-24 11:27:34 +0900

    Split message and token transfer+message tests

--- devenv/tests/e2e/smoke_test.go
+++ devenv/tests/e2e/smoke_test.go
@@ -77,85 +77,12 @@ func TestE2ESmoke(t *testing.T) {
 	for _, tc := range matrix {
 		fromImpl := tc.fromChain
 		toImpl := tc.toChain
-
 		laneTag := fmt.Sprintf("%s->%s", fromImpl.Family(), toImpl.Family())
 
-		name := fmt.Sprintf("%s msg execution EOA receiver", laneTag)
-		_, fromSupported := supportedTokenFamilies[fromImpl.Family()]
-		_, toSupported := supportedTokenFamilies[toImpl.Family()]
-		if fromSupported && toSupported {
-			name += " with token transfer"
-		} else {
-			name += " without token transfer"
-		}
-
-		t.Run(name, func(t *testing.T) {
-			t.Logf("Testing CCIP message from chain %d to chain %d", tc.fromChain, tc.toChain)
-
+		t.Run(fmt.Sprintf("%s message", laneTag), func(t *testing.T) {
 			receiver := toImpl.CCIPReceiver()
 			extraArgs, err := toImpl.GetExtraArgs(receiver, fromImpl.Family())
 			require.NoError(t, err)
-
-			// TODO: once non-EVM tooling supports token transfers, we'll be able
-			// to remove the EVM <-> EVM filter and directly set these variables.
-			var tokenAmounts []testadapters.TokenAmount = nil
-			var balanceCheck func() bool = nil
-			// technically something like solana <> solana isn't valid, but this
-			// check is just to ensure we only run token transfer tests on supported
-			// chain families for now.
-			if fromSupported && toSupported {
-				srcChainSel, srcTokenCfg := fromImpl.ChainSelector(), fromImpl.GetTokenExpansionConfig().DeployTokenInput
-				dstChainSel, dstTokenCfg := toImpl.ChainSelector(), toImpl.GetTokenExpansionConfig().DeployTokenInput
-
-				srcTokenFilterDS := datastore.AddressRef{ChainSelector: srcChainSel, Qualifier: srcTokenCfg.Symbol, Type: datastore.ContractType(srcTokenCfg.Type)}
-				srcTokenRef, err := datastore_utils.FindAndFormatRef(e.DataStore, srcTokenFilterDS, srcChainSel, datastore_utils.FullRef)
-				require.NoError(t, err)
-
-				dstTokenFilterDS := datastore.AddressRef{ChainSelector: dstChainSel, Qualifier: dstTokenCfg.Symbol, Type: datastore.ContractType(dstTokenCfg.Type)}
-				dstTokenRef, err := datastore_utils.FindAndFormatRef(e.DataStore, dstTokenFilterDS, dstChainSel, datastore_utils.FullRef)
-				require.NoError(t, err)
-
-				// Here, we avoid using a fractional token amount to simplify the test logic. In
-				// this case, we transfer 10^src_decimals units on the *src* chain, which is the
-				// the equivalent of one whole token on the source. This results in the receiver
-				// getting the equivalent of 10^dst_decimals units which is also one whole token
-				// on the *destination* chain. If we want to test fractional amounts later, then
-				// we'd need to scale the amounts according to both the src/dst token decimals.
-				sendAmnt := new(big.Int).Exp(big.NewInt(10), new(big.Int).SetUint64(uint64(srcTokenCfg.Decimals)), nil)
-
-				// We expect the receiver to get 1 whole token on the destination chain.
-				recvAmnt := new(big.Int).Exp(big.NewInt(10), new(big.Int).SetUint64(uint64(dstTokenCfg.Decimals)), nil)
-
-				// Query the initial balance of the receiver account on the destination chain
-				initAmnt, err := toImpl.GetTokenBalance(t.Context(), dstTokenRef.Address, receiver)
-				require.NoError(t, err)
-
-				// Calculate the total balance that the receiver should have after execution
-				trgtAmnt := new(big.Int).Add(initAmnt, recvAmnt)
-
-				// This balance check function will be polled at regular intervals. It returns
-				// true when the receiver's current balance matches the expected target amount
-				balanceCheck = func() bool {
-					t.Helper()
-
-					balance, err := toImpl.GetTokenBalance(t.Context(), dstTokenRef.Address, receiver)
-					require.NoError(t, err)
-
-					t.Log(fmt.Sprintf("Fetched receiver token balance on chain %d (%s)", toImpl.ChainSelector(), toImpl.Family()),
-						"token.qualifier="+dstTokenRef.Qualifier,
-						"token.address="+dstTokenRef.Address,
-						"token.type="+dstTokenRef.Type,
-						"balance.target="+trgtAmnt.String(),
-						"balance.actual="+balance.String(),
-					)
-
-					return balance.Cmp(trgtAmnt) == 0
-				}
-
-				tokenAmounts = []testadapters.TokenAmount{
-					{Amount: sendAmnt, Token: srcTokenRef.Address},
-				}
-			}
 
 			msg, err := fromImpl.BuildMessage(testadapters.MessageComponents{
 				DestChainSelector: toImpl.ChainSelector(),
@@ -163,7 +90,7 @@ func TestE2ESmoke(t *testing.T) {
 				Data:              []byte("hello eoa"),
 				FeeToken:          "",
 				ExtraArgs:         extraArgs,
-				TokenAmounts:      tokenAmounts,
+				TokenAmounts:      nil,
 			})
 			require.NoError(t, err)
 
@@ -173,18 +100,91 @@ func TestE2ESmoke(t *testing.T) {
 			seqNumRange := ccipocr3.NewSeqNumRange(seqNr, seqNr)
 			toImpl.ValidateCommit(t, fromImpl.ChainSelector(), nil, seqNumRange)
 			toImpl.ValidateExec(t, fromImpl.ChainSelector(), nil, []uint64{seq})
+		})
 
-			// TODO: once non-EVM tooling supports token transfers we can
-			// remove this if statement and always run the balance check.
-			if balanceCheck != nil {
-				require.Eventually(t, balanceCheck, 5*time.Second, time.Second)
+		t.Run(fmt.Sprintf("%s token transfer", laneTag), func(t *testing.T) {
+			receiver := toImpl.CCIPReceiver()
+			extraArgs, err := toImpl.GetExtraArgs(receiver, fromImpl.Family())
+			require.NoError(t, err)
+
+			_, fromSupported := supportedTokenFamilies[fromImpl.Family()]
+			_, toSupported := supportedTokenFamilies[toImpl.Family()]
+			if !fromSupported || !toSupported {
+				t.Skip("Token transfers not supported on %s", laneTag)
 			}
+
+			srcChainSel, srcTokenCfg := fromImpl.ChainSelector(), fromImpl.GetTokenExpansionConfig().DeployTokenInput
+			dstChainSel, dstTokenCfg := toImpl.ChainSelector(), toImpl.GetTokenExpansionConfig().DeployTokenInput
+
+			srcTokenFilterDS := datastore.AddressRef{ChainSelector: srcChainSel, Qualifier: srcTokenCfg.Symbol, Type: datastore.ContractType(srcTokenCfg.Type)}
+			srcTokenRef, err := datastore_utils.FindAndFormatRef(e.DataStore, srcTokenFilterDS, srcChainSel, datastore_utils.FullRef)
+			require.NoError(t, err)
+
+			dstTokenFilterDS := datastore.AddressRef{ChainSelector: dstChainSel, Qualifier: dstTokenCfg.Symbol, Type: datastore.ContractType(dstTokenCfg.Type)}
+			dstTokenRef, err := datastore_utils.FindAndFormatRef(e.DataStore, dstTokenFilterDS, dstChainSel, datastore_utils.FullRef)
+			require.NoError(t, err)
+
+			// Here, we avoid using a fractional token amount to simplify the test logic. In
+			// this case, we transfer 10^src_decimals units on the *src* chain, which is the
+			// the equivalent of one whole token on the source. This results in the receiver
+			// getting the equivalent of 10^dst_decimals units which is also one whole token
+			// on the *destination* chain. If we want to test fractional amounts later, then
+			// we'd need to scale the amounts according to both the src/dst token decimals.
+			sendAmnt := new(big.Int).Exp(big.NewInt(10), new(big.Int).SetUint64(uint64(srcTokenCfg.Decimals)), nil)
+
+			// We expect the receiver to get 1 whole token on the destination chain.
+			recvAmnt := new(big.Int).Exp(big.NewInt(10), new(big.Int).SetUint64(uint64(dstTokenCfg.Decimals)), nil)
+
+			// Query the initial balance of the receiver account on the destination chain
+			initAmnt, err := toImpl.GetTokenBalance(t.Context(), dstTokenRef.Address, receiver)
+			require.NoError(t, err)
+
+			// Calculate the total balance that the receiver should have after execution
+			trgtAmnt := new(big.Int).Add(initAmnt, recvAmnt)
+
+			// This balance check function will be polled at regular intervals. It returns
+			// true when the receiver's current balance matches the expected target amount
+			balanceCheck := func() bool {
+				t.Helper()
+
+				balance, err := toImpl.GetTokenBalance(t.Context(), dstTokenRef.Address, receiver)
+				require.NoError(t, err)
+
+				t.Log(fmt.Sprintf("Fetched receiver token balance on chain %d (%s)", toImpl.ChainSelector(), toImpl.Family()),
+					"token.qualifier="+dstTokenRef.Qualifier,
+					"token.address="+dstTokenRef.Address,
+					"token.type="+dstTokenRef.Type,
+					"balance.target="+trgtAmnt.String(),
+					"balance.actual="+balance.String(),
+				)
+				return balance.Cmp(trgtAmnt) == 0
+			}
+
+			msg, err := fromImpl.BuildMessage(testadapters.MessageComponents{
+				DestChainSelector: toImpl.ChainSelector(),
+				Receiver:          receiver,
+				Data:              []byte("hello eoa"),
+				FeeToken:          "",
+				ExtraArgs:         extraArgs,
+				TokenAmounts: []testadapters.TokenAmount{
+					{Amount: sendAmnt, Token: srcTokenRef.Address},
+				},
+			})
+			require.NoError(t, err)
+
+			seq, err := fromImpl.SendMessage(t.Context(), toImpl.ChainSelector(), msg)
+			require.NoError(t, err)
+			seqNr := ccipocr3.SeqNum(seq)
+			seqNumRange := ccipocr3.NewSeqNumRange(seqNr, seqNr)
+			toImpl.ValidateCommit(t, fromImpl.ChainSelector(), nil, seqNumRange)
+			toImpl.ValidateExec(t, fromImpl.ChainSelector(), nil, []uint64{seq})
+			require.Eventually(t, balanceCheck, 5*time.Second, time.Second)
 		})
 
 		t.Run(fmt.Sprintf("%s gas limit too high", laneTag), func(t *testing.T) {
 			receiver := toImpl.CCIPReceiver()
 
-			extraArgs, err := toImpl.GetExtraArgs(receiver, fromImpl.Family(), testadapters.NewGasLimitExtraArg(big.NewInt(math.MaxInt64)))
+			extraArgs, err := toImpl.GetExtraArgs(receiver, fromImpl.Family(), testadapters.NewGasLimitExtraArg(big.NewInt(math.MaxInt32)))
 			require.NoError(t, err)
 
 			msg, err := fromImpl.BuildMessage(testadapters.MessageComponents{
@@ -213,6 +213,10 @@ func TestE2ESmoke(t *testing.T) {
 		})
 
 		t.Run(fmt.Sprintf("%s invalid receiver", laneTag), func(t *testing.T) {
+			if fromImpl.Family() == chainsel.FamilySolana {
+				t.Skip("GetExtraArgs fails with invalid pubkey receivers, we'd need to construct a raw payload to test against the contract")
+			}
+
 			invalidReceiver := []byte{99}
 
 			extraArgs, err := toImpl.GetExtraArgs(invalidReceiver, fromImpl.Family(), testadapters.NewGasLimitExtraArg(big.NewInt(math.MaxInt64)))
