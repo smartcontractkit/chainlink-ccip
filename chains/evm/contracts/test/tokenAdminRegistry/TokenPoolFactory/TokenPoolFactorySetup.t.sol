9faf29f03092574fbacaf8e65b0924b0ba538e41
Rens Rooimans <github@rensrooimans.nl>
2026-02-24 15:41:58 +0100

    cleanup factory tests

--- chains/evm/contracts/test/tokenAdminRegistry/TokenPoolFactory/TokenPoolFactorySetup.t.sol
+++ chains/evm/contracts/test/tokenAdminRegistry/TokenPoolFactory/TokenPoolFactorySetup.t.sol
@@ -6,16 +6,22 @@ import {RegistryModuleOwnerCustom} from "../../../tokenAdminRegistry/RegistryMod
 import {FactoryBurnMintERC20} from "../../../tokenAdminRegistry/TokenPoolFactory/FactoryBurnMintERC20.sol";
 import {TokenPoolFactory} from "../../../tokenAdminRegistry/TokenPoolFactory/TokenPoolFactory.sol";
 import {TokenAdminRegistrySetup} from "../TokenAdminRegistry/TokenAdminRegistrySetup.t.sol";
-import {Create2} from "@openzeppelin/contracts@5.3.0/utils/Create2.sol";
 
 contract TokenPoolFactorySetup is TokenAdminRegistrySetup {
-  using Create2 for bytes32;
-
   TokenPoolFactory internal s_tokenPoolFactory;
   RegistryModuleOwnerCustom internal s_registryModuleOwnerCustom;
+  address internal s_rmnProxy = address(0x1234);
 
-  bytes32 internal constant FAKE_SALT = keccak256(abi.encode("FAKE_SALT"));
+  bytes internal constant POOL_INIT_CODE = type(BurnMintTokenPool).creationCode;
   uint256 public constant PREMINT_AMOUNT = 100 ether;
+  bytes32 internal constant FAKE_SALT = keccak256(abi.encode("FAKE_SALT"));
+
+  bytes internal constant TOKEN_CREATION_PARAMS =
+    abi.encode("TestToken", "TT", 18, type(uint256).max, PREMINT_AMOUNT, OWNER);
+  bytes internal constant TOKEN_INIT_CODE =
+    abi.encodePacked(type(FactoryBurnMintERC20).creationCode, TOKEN_CREATION_PARAMS);
+
+  bytes internal s_poolInitArgs;
 
   address internal s_rmnProxy = address(0x1234);
 
@@ -25,12 +31,14 @@ contract TokenPoolFactorySetup is TokenAdminRegistrySetup {
   bytes internal constant POOL_INIT_CODE = type(BurnMintTokenPool).creationCode;
 
   function setUp() public virtual override {
-    TokenAdminRegistrySetup.setUp();
+    super.setUp();
 
     s_registryModuleOwnerCustom = new RegistryModuleOwnerCustom(address(s_tokenAdminRegistry));
     s_tokenAdminRegistry.addRegistryModule(address(s_registryModuleOwnerCustom));
 
     s_tokenPoolFactory =
       new TokenPoolFactory(s_tokenAdminRegistry, s_registryModuleOwnerCustom, s_rmnProxy, address(s_sourceRouter));
+
+    s_poolInitArgs = abi.encode(address(0), address(0x1234), s_sourceRouter);
   }
 }
