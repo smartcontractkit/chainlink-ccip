ec4a1ece52175a167a38eb9965fa77156cbd64ba
Terry Tata <terry.tata@smartcontract.com>
2026-02-24 16:08:09 -0800

    bubble up

--- deployment/tokens/token_expansion.go
+++ deployment/tokens/token_expansion.go
@@ -341,12 +341,13 @@ func tokenExpansionApply() func(cldf.Environment, TokenExpansionInput) (cldf.Cha
 		}
 
 		// we process the token configs for transfers, which will register the tokens and token pools on-chain and set the pool on the token if necessary
-		transferOps, transferReports, err := processTokenConfigForChain(e, allTokenConfigs, cfg.ChainAdapterVersion)
+		transferOps, transferReports, tokends, err := processTokenConfigForChain(e, allTokenConfigs, cfg.ChainAdapterVersion)
 		if err != nil {
 			return cldf.ChangesetOutput{}, fmt.Errorf("failed to process token configs for transfers: %w", err)
 		}
 		batchOps = append(batchOps, transferOps...)
 		reports = append(reports, transferReports...)
+		ds.Merge(tokends.Seal())
 
 		// finally, we update the authorities on the tokens if necessary
 		for selector, tokenConfig := range allTokenConfigs {
