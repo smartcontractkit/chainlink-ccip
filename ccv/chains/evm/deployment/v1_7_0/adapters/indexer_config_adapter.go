6f16a1cce719553f19ce509524e69e7be507320f
Rens Rooimans <github@rensrooimans.nl>
2026-03-09 17:08:36 +0100

    CCTP verifier

--- ccv/chains/evm/deployment/v1_7_0/adapters/indexer_config_adapter.go
+++ ccv/chains/evm/deployment/v1_7_0/adapters/indexer_config_adapter.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Masterminds/semver/v3"
 
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/latest/operations/committee_verifier"
-	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/cctp_verifier"
+	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/latest/operations/cctp_verifier"
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/lombard_verifier"
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/versioned_verifier_resolver"
 	ccvadapters "github.com/smartcontractkit/chainlink-ccip/deployment/v1_7_0/adapters"
@@ -50,7 +50,7 @@ func resolveContractMeta(kind ccvadapters.VerifierKind) (deployment.ContractType
 	case ccvadapters.CommitteeVerifierKind:
 		return versioned_verifier_resolver.CommitteeVerifierResolverType, committee_verifier.Version, nil
 	case ccvadapters.CCTPVerifierKind:
-		return cctp_verifier.ResolverType, cctp_verifier.Version, nil
+		return versioned_verifier_resolver.CCTPVerifierResolverType, cctp_verifier.Version, nil
 	case ccvadapters.LombardVerifierKind:
 		return lombard_verifier.ResolverType, lombard_verifier.Version, nil
 	default:
