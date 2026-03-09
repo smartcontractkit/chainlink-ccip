bf46f596372cf10cb61c02ad3465d686a2977943
Terry Tata <terry.tata@smartcontract.com>
2026-03-09 15:38:08 -0700

    tests

--- ccv/chains/evm/deployment/v1_7_0/sequences/deploy_chain_contracts_test.go
+++ ccv/chains/evm/deployment/v1_7_0/sequences/deploy_chain_contracts_test.go
@@ -6,15 +6,15 @@ import (
 	"github.com/Masterminds/semver/v3"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/v2"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/latest/operations/offramp"
+	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/latest/operations/onramp"
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/committee_verifier"
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/create2_factory"
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/executor"
-	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v2_0_0/operations/fee_quoter"
 	mock_receiver "github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/operations/mock_receiver"
-	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/latest/operations/offramp"
-	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/latest/operations/onramp"
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/sequences"
 	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v1_7_0/testsetup"
+	"github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/deployment/v2_0_0/operations/fee_quoter"
 	mock_recv_bindings "github.com/smartcontractkit/chainlink-ccip/ccv/chains/evm/gobindings/generated/latest/mock_receiver_v2"
 	evm_datastore_utils "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/datastore"
 	contract_utils "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/operations/contract"
@@ -343,18 +343,21 @@ func TestDeployChainContracts_MultipleCommitteeVerifiersAndMultipleMockReceiverC
 	q1Receiver, err := mock_recv_bindings.NewMockReceiverV2(q1ReceiverRef, e.BlockChains.EVMChains()[chainSelector].Client)
 	require.NoError(t, err)
 
-	required, optional, threshold, err := q1Receiver.GetCCVs(&bind.CallOpts{Context: e.OperationsBundle.GetContext()}, chainSelector)
+	sender := e.BlockChains.EVMChains()[chainSelector].DeployerKey.From
+	required, optional, threshold, minBlockDepth, err := q1Receiver.GetCCVsAndMinBlockDepth(&bind.CallOpts{Context: e.OperationsBundle.GetContext()}, chainSelector, sender.Bytes())
 	require.NoError(t, err)
 	require.Len(t, required, 2)
 	require.Len(t, optional, 0)
 	require.Equal(t, uint8(0), threshold)
+	require.Equal(t, uint16(0), minBlockDepth)
 
 	q2Receiver, err := mock_recv_bindings.NewMockReceiverV2(q2ReceiverRef, e.BlockChains.EVMChains()[chainSelector].Client)
 	require.NoError(t, err)
 
-	required, optional, threshold, err = q2Receiver.GetCCVs(&bind.CallOpts{Context: e.OperationsBundle.GetContext()}, chainSelector)
+	required, optional, threshold, minBlockDepth, err = q2Receiver.GetCCVsAndMinBlockDepth(&bind.CallOpts{Context: e.OperationsBundle.GetContext()}, chainSelector, sender.Bytes())
 	require.NoError(t, err)
 	require.Len(t, required, 1)
 	require.Len(t, optional, 1)
 	require.Equal(t, uint8(1), threshold)
+	require.Equal(t, uint16(0), minBlockDepth)
 }
