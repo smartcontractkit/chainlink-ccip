4230ea8a71e407aac647005f5d07dab3b8d2c660
Alkhara <cawley22@gmail.com>
2026-05-06 12:20:28 -0400

    remove ink

--- chains/evm/contracts/test/fork/USDCForkTest.t.sol
+++ chains/evm/contracts/test/fork/USDCForkTest.t.sol
@@ -19,11 +19,8 @@ contract USDCForkTest is MCMSForkTest {
 
   address private s_lockbox_address_jovay_testnet;
 
-  address private s_lockbox_address_ink_testnet;
-
   uint256 private s_adi_withdraw_amount;
   uint256 private s_jovay_withdraw_amount;
-  uint256 private s_ink_withdraw_amount;
 
   uint256 private s_forkId;
   bytes[] private s_payloads;
@@ -47,10 +44,8 @@ contract USDCForkTest is MCMSForkTest {
     s_timelock_address_sepolia = vm.envAddress("TIMELOCK_ADDRESS_SEPOLIA");
     s_lockbox_address_adi_testnet = vm.envAddress("LOCKBOX_ADDRESS_ADI_TESTNET");
     s_lockbox_address_jovay_testnet = vm.envAddress("LOCKBOX_ADDRESS_JOVAY_TESTNET");
-    s_lockbox_address_ink_testnet = vm.envAddress("LOCKBOX_ADDRESS_INK_TESTNET");
     s_adi_withdraw_amount = vm.envUint("ADI_WITHDRAW_AMOUNT");
     s_jovay_withdraw_amount = vm.envUint("JOVAY_WITHDRAW_AMOUNT");
-    s_ink_withdraw_amount = vm.envUint("INK_WITHDRAW_AMOUNT");
   }
 
   function testFork_Migration() public {
@@ -67,33 +62,28 @@ contract USDCForkTest is MCMSForkTest {
 
     uint64 remoteChainSelectorADI = 9418205736192840573; // ADI Testnet
     uint64 remoteChainSelectorJovay = 945045181441419236; // Jovay Testnet
-    uint64 remoteChainSelectorInk = 9763904284804119144; // Ink Testnet
 
     uint256 adiTestnetLockedTokens = HybridLockReleaseUSDCTokenPool(s_usdc_token_pool_address_sepolia).getLockedTokensForChain(remoteChainSelectorADI);
     uint256 jovayTestnetLockedTokens = HybridLockReleaseUSDCTokenPool(s_usdc_token_pool_address_sepolia).getLockedTokensForChain(remoteChainSelectorJovay);
-    uint256 inkTestnetLockedTokens = HybridLockReleaseUSDCTokenPool(s_usdc_token_pool_address_sepolia).getLockedTokensForChain(remoteChainSelectorInk);
     
     uint256 timelockBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_timelock_address_sepolia);
     
     uint256 adiTestnetLockBoxBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_lockbox_address_adi_testnet);
     uint256 jovayTestnetLockBoxBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_lockbox_address_jovay_testnet);
-    uint256 inkTestnetLockBoxBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_lockbox_address_ink_testnet);
 
     // Locked Tokens should be >= withdraw amounts
     assertEq(adiTestnetLockedTokens, s_adi_withdraw_amount, "ADI Testnet locked tokens should be >= withdraw amount");
     assertEq(jovayTestnetLockedTokens, s_jovay_withdraw_amount, "Jovay Testnet locked tokens should be >= withdraw amount");
-    assertEq(inkTestnetLockedTokens, s_ink_withdraw_amount, "Ink Testnet locked tokens should be >= withdraw amount");
 
     // Apply first payload (withdraw liquidity)
     _applyPayload(s_timelock_address_sepolia, s_payloads[0]);
 
     // Timelock should have the withdraw amounts
-    assertEq(timelockBalance, s_adi_withdraw_amount + s_jovay_withdraw_amount + s_ink_withdraw_amount, "Timelock balance should be >= withdraw amounts");
+    assertEq(timelockBalance, s_adi_withdraw_amount + s_jovay_withdraw_amount, "Timelock balance should be >= withdraw amounts");
 
     // LockBoxes should have 0 balance still (no deposit yet)
     assertEq(adiTestnetLockBoxBalance, 0, "ADI Testnet lock box balance should be 0");
     assertEq(jovayTestnetLockBoxBalance, 0, "Jovay Testnet lock box balance should be 0");
-    assertEq(inkTestnetLockBoxBalance, 0, "Ink Testnet lock box balance should be 0");
 
     //Apply the second payload (approve lockbox to spend timelock's USDC)
     _applyPayload(s_timelock_address_sepolia, s_payloads[1]);
@@ -104,14 +94,11 @@ contract USDCForkTest is MCMSForkTest {
     // Re-check Locked Tokens
     uint256 newadiTestnetLockedTokens = HybridLockReleaseUSDCTokenPool(s_usdc_token_pool_address_sepolia).getLockedTokensForChain(remoteChainSelectorADI);
     uint256 newjovayTestnetLockedTokens = HybridLockReleaseUSDCTokenPool(s_usdc_token_pool_address_sepolia).getLockedTokensForChain(remoteChainSelectorJovay);
-    uint256 newinkTestnetLockedTokens = HybridLockReleaseUSDCTokenPool(s_usdc_token_pool_address_sepolia).getLockedTokensForChain(remoteChainSelectorInk);
 
     // Locked Tokens should be adiTestnetLockedTokens - s_adi_withdraw_amount
     // Jovay Testnet locked tokens should be jovayTestnetLockedTokens - s_jovay_withdraw_amount
-    // Ink Testnet locked tokens should be inkTestnetLockedTokens - s_ink_withdraw_amount
     assertEq(newadiTestnetLockedTokens, adiTestnetLockedTokens - s_adi_withdraw_amount, "ADI Testnet locked tokens should be adiTestnetLockedTokens - s_adi_withdraw_amount");
     assertEq(newjovayTestnetLockedTokens, jovayTestnetLockedTokens - s_jovay_withdraw_amount, "Jovay Testnet locked tokens should be jovayTestnetLockedTokens - s_jovay_withdraw_amount");
-    assertEq(newinkTestnetLockedTokens, inkTestnetLockedTokens - s_ink_withdraw_amount, "Ink Testnet locked tokens should be inkTestnetLockedTokens - s_ink_withdraw_amount");
 
     // Re-check Timelock balance
     uint256 newtimelockBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_timelock_address_sepolia);
@@ -122,11 +109,9 @@ contract USDCForkTest is MCMSForkTest {
     // Re-check LockBox balances
     uint256 newadiTestnetLockBoxBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_lockbox_address_adi_testnet);
     uint256 newjovayTestnetLockBoxBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_lockbox_address_jovay_testnet);
-    uint256 newinkTestnetLockBoxBalance = IERC20(s_usdc_token_address_sepolia).balanceOf(s_lockbox_address_ink_testnet);
 
     // LockBox balances should be s_adi_withdraw_amount
     assertEq(newadiTestnetLockBoxBalance, s_adi_withdraw_amount, "ADI Testnet lock box balance should be s_adi_withdraw_amount");
     assertEq(newjovayTestnetLockBoxBalance, s_jovay_withdraw_amount, "Jovay Testnet lock box balance should be s_jovay_withdraw_amount");
-    assertEq(newinkTestnetLockBoxBalance, s_ink_withdraw_amount, "Ink Testnet lock box balance should be s_ink_withdraw_amount");
   }
 }
