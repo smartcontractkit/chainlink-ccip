28b39187272130fc030e5ad6e1949071e31e14bf
chris-de-leon-cll <147140544+chris-de-leon-cll@users.noreply.github.com>
2026-05-11 17:24:29 -0700

    chore: address copilot feedback

--- chains/evm/deployment/v1_6_2/adapters/init.go
+++ chains/evm/deployment/v1_6_2/adapters/init.go
@@ -3,15 +3,14 @@ package adapters
 import (
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
 
-	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_1/adapters"
+	adaptersV1_6_1 "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_1/adapters"
 	tokensapi "github.com/smartcontractkit/chainlink-ccip/deployment/tokens"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/utils"
 )
 
-// NOTE: v1.6.2 token pool contracts still use the base v1.6.1 token pool bindings
-
 func init() {
-	adapter := adapters.NewTokenAdapter()
 	version := utils.Version_1_6_2
-	tokensapi.GetTokenAdapterRegistry().RegisterTokenAdapter(chain_selectors.FamilyEVM, version, adapter)
+
+	// NOTE: v1.6.2 token pool contracts still use the base v1.6.1 token pool bindings
+	tokensapi.GetTokenAdapterRegistry().RegisterTokenAdapter(chain_selectors.FamilyEVM, version, adaptersV1_6_1.NewTokenAdapter())
 }
