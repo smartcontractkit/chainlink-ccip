b1a36a0a591306d87ef840b067a604d77f2c44dc
chris-de-leon-cll <147140544+chris-de-leon-cll@users.noreply.github.com>
2026-05-11 17:08:25 -0700

    fix: import v1.6.1 adapter

--- chains/evm/deployment/v1_6_2/adapters/init.go
+++ chains/evm/deployment/v1_6_2/adapters/init.go
@@ -3,6 +3,7 @@ package adapters
 import (
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
 
+	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_6_1/adapters"
 	tokensapi "github.com/smartcontractkit/chainlink-ccip/deployment/tokens"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/utils"
 )
@@ -10,8 +11,8 @@ import (
 // NOTE: v1.6.2 token pool contracts still use the base v1.6.1 token pool bindings
 
 func init() {
+	adapter := adapters.NewTokenAdapter()
 	version := utils.Version_1_6_2
-	adapter := NewTokenAdapter()
 	tokensapi.GetTokenAdapterRegistry().RegisterTokenAdapter(
 		chain_selectors.FamilyEVM,
 		version,
