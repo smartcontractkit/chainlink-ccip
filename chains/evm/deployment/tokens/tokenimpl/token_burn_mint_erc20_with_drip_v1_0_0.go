054dbc170dc5b749f17f5e3918c78dbcd85a1eda
Matt Yang <matt.yang@smartcontract.com>
2026-05-12 15:21:35 -0700

    add 2 hasAdminRole token types

--- chains/evm/deployment/tokens/tokenimpl/token_burn_mint_erc20_with_drip_v1_0_0.go
+++ chains/evm/deployment/tokens/tokenimpl/token_burn_mint_erc20_with_drip_v1_0_0.go
@@ -1,6 +1,7 @@
 package tokenimpl
 
 import (
+	"context"
 	"fmt"
 	"math/big"
 
@@ -36,6 +37,10 @@ func (tokenBurnMintERC20WithDripV1_0_0) Capabilities() CapabilitySet {
 
 func (tokenBurnMintERC20WithDripV1_0_0) RevokeAdminRole(b operations.Bundle, chain evm.Chain, token, externalAdmin common.Address) ([]contract.WriteOutput, error) {
 	return revokeDefaultAdminRoleBurnMintERC20(b, chain, token, externalAdmin)
+}
+
+func (tokenBurnMintERC20WithDripV1_0_0) HasAdminRole(ctx context.Context, chain evm.Chain, token, user common.Address) (bool, error) {
+	return hasDefaultAdminRoleBurnMintERC20(ctx, chain, token, user)
 }
 
 func (tokenBurnMintERC20WithDripV1_0_0) GrantAdminRole(b operations.Bundle, chain evm.Chain, token, externalAdmin common.Address) ([]contract.WriteOutput, error) {
