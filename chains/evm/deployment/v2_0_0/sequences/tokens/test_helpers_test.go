35b0877c5482cbdc2f196df2bc9fb25e6aa28232
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2026-09-18 11:59:45 +0530

    add check for CCV x AdvancePoolHook + fix test

--- chains/evm/deployment/v2_0_0/sequences/tokens/test_helpers_test.go
+++ chains/evm/deployment/v2_0_0/sequences/tokens/test_helpers_test.go
@@ -9,10 +9,12 @@ import (
 	"github.com/stretchr/testify/require"
 
 	evm_datastore_utils "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/datastore"
+	contract_utils "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/utils/operations/contract"
 	bnm_drip_v1_0 "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v1_0_0/operations/burn_mint_erc20_with_drip"
 	_ "github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v2_0_0/adapters"
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v2_0_0/operations/advanced_pool_hooks"
 	"github.com/smartcontractkit/chainlink-ccip/chains/evm/deployment/v2_0_0/operations/burn_mint_token_pool"
+	latest_token_pool "github.com/smartcontractkit/chainlink-ccip/chains/evm/gobindings/generated/v2_0_0/token_pool"
 	tokenscore "github.com/smartcontractkit/chainlink-ccip/deployment/tokens"
 	datastore_utils "github.com/smartcontractkit/chainlink-ccip/deployment/utils/datastore"
 	"github.com/smartcontractkit/chainlink-ccip/deployment/utils/mcms"
@@ -102,13 +104,35 @@ func deployTokenAndPoolViaExpansionWithDecimals(
 	}, chainSel, evm_datastore_utils.ToEVMAddress)
 	require.NoError(t, err, "Token pool should exist in datastore after TokenExpansion")
 
-	hooksAddr, err := datastore_utils.FindAndFormatRef(e.DataStore, datastore.AddressRef{
-		ChainSelector: chainSel,
-		Type:          datastore.ContractType(advanced_pool_hooks.ContractType),
-		Version:       advanced_pool_hooks.Version,
-		Qualifier:     "TEST",
-	}, chainSel, evm_datastore_utils.ToEVMAddress)
-	require.NoError(t, err, "Advanced pool hooks should exist in datastore after TokenExpansion")
+	// TokenExpansion no longer deploys AdvancedPoolHooks. Deploy one separately and wire it into
+	// the pool so tests can continue to exercise hook-dependent configuration.
+	chain := e.BlockChains.EVMChains()[chainSel]
+	hooksQualifier := "TEST"
+	hooksRef, err := contract_utils.MaybeDeployContract(e.OperationsBundle, advanced_pool_hooks.Deploy, chain, contract_utils.DeployInput[advanced_pool_hooks.ConstructorArgs]{
+		ChainSelector:  chainSel,
+		TypeAndVersion: deployment.NewTypeAndVersion(advanced_pool_hooks.ContractType, *advanced_pool_hooks.Version),
+		Args: advanced_pool_hooks.ConstructorArgs{
+			ThresholdAmountForAdditionalCCVs: thresholdAmountForAdditionalCCVs,
+		},
+		Qualifier: &hooksQualifier,
+	}, nil)
+	require.NoError(t, err, "Failed to deploy AdvancedPoolHooks")
+
+	hooksAddr := common.HexToAddress(hooksRef.Address)
+
+	postDS := datastore.NewMemoryDataStore()
+	postDS.Merge(e.DataStore)
+	require.NoError(t, postDS.Addresses().Add(hooksRef), "Failed to add AdvancedPoolHooks to datastore")
+	e.DataStore = postDS.Seal()
+
+	poolContract, err := latest_token_pool.NewTokenPool(poolAddr, chain.Client)
+	require.NoError(t, err, "Failed to bind token pool contract")
+	auth := *chain.DeployerKey
+	auth.Nonce = nil
+	tx, err := poolContract.UpdateAdvancedPoolHooks(&auth, hooksAddr)
+	require.NoError(t, err, "Failed to wire AdvancedPoolHooks into the pool")
+	_, err = chain.Confirm(tx)
+	require.NoError(t, err, "Failed to confirm AdvancedPoolHooks wiring")
 
 	return tokenExpansionResult{
 		TokenAddress:         tokenAddr,
